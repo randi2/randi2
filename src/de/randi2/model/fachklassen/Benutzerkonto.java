04e498277e4016b496717832ed6f29d598eb2f08
lplotni <lplotni@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-01-28 16:28:11 +0000

    Benutzerkonto Klasse wurde erstellet.
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@282 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/model/fachklassen/Benutzerkonto.java
@@ -0,0 +1,121 @@
+package de.randi2.model.fachklassen;
+
+import java.util.Vector;
+
+import de.randi2.model.exceptions.BenutzerNichtVorhandenException;
+import de.randi2.model.fachklassen.beans.BenutzerkontoBean;
+
+/**
+ * Diese Klasse bietet Methoden zur Verwaltung eines bestimmten Benutzerkontos
+ * an. Beinhaltet auch es.
+ * 
+ * @version 0.1
+ * @author Lukasz Plotnicki <lplotni@stud.hs-heilbronn.de>
+ * 
+ */
+public class Benutzerkonto {
+
+	/**
+	 * Das zugehörige BenutzerkontoBean-Objekt.
+	 */
+	private BenutzerkontoBean aBenutzerkonto;
+
+	/**
+	 * Ein Konstruktor dieser Klasse.
+	 * 
+	 * @param aBenutzerkonto
+	 *            das zugeöhrige BenutzerkontoBean
+	 */
+	public Benutzerkonto(BenutzerkontoBean aBenutzerkonto) {
+		this.aBenutzerkonto = aBenutzerkonto;
+	}
+
+	/**
+	 * Diese statische Methode sucht die gewünschten Objekte und falls sie
+	 * vorhanden sind, liefert sie zurück.
+	 * 
+	 * @param sBenutzerkonto
+	 *            ein BenutzerkontoBean mit gesuchten Eigenschaften (alle
+	 *            irrelevante Felder entsprechen den Null-Werten aus der
+	 *            de.randi2.utility.NullKonstanten Klasse)
+	 * @return ein Vector mit gefundenen Objekten
+	 */
+	public static Vector<BenutzerkontoBean> suchenBenutzer(
+			BenutzerkontoBean sBenutzerkonto) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Mit Hilfe dieser Methode, kann ein Benutzerkonto angelegt werden.
+	 * Darunter ist zu verstehen, dass das Benutzerkonto gespeichert wird und
+	 * die Aktivierungsnachricht an den entsprechenden Benutzer gesendet wird.
+	 * 
+	 * @param aBenutzerkonto
+	 *            das Bentuzerkonto das angelegt werden soll.
+	 * @return
+	 */
+	public static Benutzerkonto anlegenBenutzer(BenutzerkontoBean aBenutzerkonto) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Diese Methode generiert die Aktivierungsnachricht und schickt dem
+	 * Benutzer zu.
+	 */
+	private void sendenAktivierungsMail() {
+		// TODO
+	}
+
+	/**
+	 * Diese statische Methode liefert das Bentutzerkonto Objekt zu dem
+	 * eingegebenenem Benutzername.
+	 * 
+	 * @param benutzername
+	 *            String, der dem Benutzername entspricht.
+	 * @return Ein Benutzerkonto Objekt zu diesem Benutzername
+	 * @throws BenutzerNichtVorhandenException
+	 *             wenn kein Benutzer mit diesem Banutzername vorhanden ist
+	 */
+	public static Benutzerkonto getBenutzer(String benutzername)
+			throws BenutzerNichtVorhandenException {
+		// TODO
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Diese Methode dient zum Verlgeich von 2 Objekten dieser Klasse.
+	 * 
+	 * @param zuvergleichendesObjekt
+	 *            Objekt mit dem verglichen werden soll.
+	 * @return true - wenn die beiden Objekte identisch sind, false wenn das
+	 *         nicht der Fall ist.
+	 */
+	public boolean equals(Benutzerkonto zuvergleichendesObjekt) {
+		// TODO
+		return false;
+	}
+
+	/**
+	 * Diese Methode prüft, ob das übergebene Passwort richtig ist.
+	 * 
+	 * @param passwort
+	 *            Das Passwort, das überprüft werden soll.
+	 * @return true, wenn das Passwort richtig ist. False, bei falchem Passwort.
+	 */
+	public boolean pruefenPasswort(String passwort) {
+		// TODO
+		return false;
+	}
+}
