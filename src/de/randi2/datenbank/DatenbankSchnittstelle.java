d47de9d01334cc76aeaa51895f2568e447a15cc8
dhaehn <dhaehn@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-01-28 16:09:59 +0000

    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@279 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/datenbank/DatenbankSchnittstelle.java
@@ -0,0 +1,55 @@
+package de.randi2.datenbank;
+
+import java.util.Vector;
+
+import de.randi2.datenbank.exceptions.DatenbankFehlerException;
+
+/**
+ * Diese Klasse repraesentiert eine Schnittstelle fuer den Datenbankzugriff.
+ * Methoden zum Suchen von Daten sowie zum Schreiben von Daten sind vorhanden.
+ * 
+ * @version 0.1
+ * @author Daniel Haehn <dhaehn@stud.hs-heilbronn.de>
+ */
+public interface DatenbankSchnittstelle {
+
+	/**
+	 * <p>
+	 * Diese Methode sucht alle in der Datenbank gespeicherten Objekte, welche
+	 * der Filterung, die durch das uebergebene Objekt definiert wurde,
+	 * entsprechen. Dabei werden alle Attribute welche != null bzw. !=
+	 * null-Konstanten (siehe de.randi2.utility.NullKonstanten) sind, als Filter
+	 * verwendet.
+	 * </p>
+	 * <p>
+	 * Falls keine Objekte der Filterung entsprechen, wird ein leerer Vektor
+	 * zurueckgegeben.
+	 * </p>
+	 * 
+	 * @param zuSuchendesObjekt
+	 *            das zusuchende Objekt mit Attributen als Filter
+	 * @return ein Vektor von auf die Filterung zutreffenden Objekten
+	 * @throws DatenbankFehlerException
+	 *             falls ein Fehler beim Datenbankzugriff aufgetreten ist
+	 */
+	public Vector<Object> suchenObjekt(Object zuSuchendesObjekt)
+			throws DatenbankFehlerException;
+
+	/**
+	 * <p>
+	 * Diese Methode schreibt das uebergebene Objekt mit allen Attributen in die
+	 * Datenbank und liefert bei Erfolg das geschriebene Objekt zurueck, bei
+	 * welchem dann das Feld id gesetzt wurde.
+	 * </p>
+	 * 
+	 * 
+	 * @param zuSchreibendesObjekt
+	 *            das zuschreibende Objekt
+	 * @return das geschriebene Objekt, bei welchem das Feld id gesetzt wurde
+	 * @throws DatenbankFehlerException
+	 *             falls ein Fehler beim Datenbankzugriff aufgetreten ist
+	 */
+	public Object schreibenObjekt(Object zuSchreibendesObjekt)
+			throws DatenbankFehlerException;
+
+}
---
+++ src/de/randi2/datenbank/exceptions/DatenbankFehlerException.java
@@ -0,0 +1,14 @@
+package de.randi2.datenbank.exceptions;
+
+/**
+ * Diese Exception zeigt einen generellen Fehler beim Datenbankzugriff auf.
+ * 
+ * @version 0.1
+ * @author Daniel Haehn <dhaehn@stud.hs-heilbronn.de>
+ */
+public class DatenbankFehlerException extends Exception {
+
+	// TODO
+	
+}
+
