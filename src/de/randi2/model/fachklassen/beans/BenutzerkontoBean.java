d4ac93452161c3f4396f49ab3c4f6b2df1a5216a
btheel <btheel@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-01-30 14:51:22 +0000

    Benutzerbean kommentiert, heraufsetzung der 14-Zeichengrenze fuer den Benutzernamen vorgeschlagen
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@299 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/model/fachklassen/beans/BenutzerkontoBean.java
+++ src/de/randi2/model/fachklassen/beans/BenutzerkontoBean.java
@@ -14,323 +14,324 @@ import de.randi2.model.fachklassen.Rolle;
  */
 public class BenutzerkontoBean {
 
-	// TODO Anbindung an de.randi2.utility.NullAttribute noch zu realisieren
+    // TODO Anbindung an de.randi2.utility.NullAttribute noch zu realisieren
 
-	/*
-	 * Change Log 29.01.2007 Thomas Willert
-	 * 
-	 * toString und equals sind noch nicht implementiert. Passwort Sonderzeichen
-	 * muessen noch gecheckt werden. Pruefung der Rolle fraglich, da Verwendung
-	 * der enum aus der Klasse Rolle nicht moeglich. Vielleicht sollte man die
-	 * Konstanten dort public machen.
-	 * 
-	 */
-
-	/**
-	 * Benutzername
-	 */
-	private String benutzername;
-
-	/**
-	 * Passwort (md5)
-	 */
-	private String passwort;
-
-	/**
-	 * Rolle des Benutzerkontos
-	 */
-	private Rolle rolle;
+    /*
+     * Change Log 29.01.2007 Thomas Willert
+     * 
+     * toString und equals sind noch nicht implementiert. Passwort Sonderzeichen
+     * muessen noch gecheckt werden. Pruefung der Rolle fraglich, da Verwendung
+     * der enum aus der Klasse Rolle nicht moeglich. Vielleicht sollte man die
+     * Konstanten dort public machen.
+     * 
+     */
 
     /**
-     * ID des Kontos
-     *TODO Datentyp checken, long korrekt?
+     * Benutzername
+     */
+    private String benutzername;
+
+    /**
+     * Passwort (md5)
+     */
+    private String passwort;
+
+    /**
+     * Rolle des Benutzerkontos
+     */
+    private Rolle rolle;
+
+    /**
+     * ID des Kontos TODO Datentyp checken, long korrekt?
      */
     private long id = NullKonstanten.DUMMY_ID;
-    
-	/**
-	 * Zugehoeriges PersonBean zu diesem Benutzerkonto.
-	 */
-	private PersonBean benutzer;
 
-	/**
-	 * Zugehoeriger Ansprechparter.
-	 */
-	private PersonBean ansprechpartner;
+    /**
+     * Zugehoeriges PersonBean zu diesem Benutzerkonto.
+     */
+    private PersonBean benutzer;
 
-	/**
-	 * Ein boolescher Wert, der dem Status gesperrt/entsperrt entspricht.
-	 */
-	private boolean gesperrt;
+    /**
+     * Zugehoeriger Ansprechparter.
+     */
+    private PersonBean ansprechpartner;
 
-	/**
-	 * Zeitpunkt des ersten Logins
-	 */
-	private GregorianCalendar ersterLogin;
+    /**
+     * Ein boolescher Wert, der dem Status gesperrt/entsperrt entspricht.
+     */
+    private boolean gesperrt;
 
-	/**
-	 * Zeitpunkt des letzten Logins
-	 */
-	private GregorianCalendar letzterLogin;
+    /**
+     * Zeitpunkt des ersten Logins
+     */
+    private GregorianCalendar ersterLogin;
 
-	/**
-	 * Der Standardkonstruktor
-	 * 
-	 */
-	public BenutzerkontoBean() throws IllegalArgumentException {
+    /**
+     * Zeitpunkt des letzten Logins
+     */
+    private GregorianCalendar letzterLogin;
 
-	}
+    /**
+     * Der Standardkonstruktor
+     * 
+     */
+    public BenutzerkontoBean() throws IllegalArgumentException {
 
-	/**
-	 * Der Konstruktor mit allen Attributen.
-	 * 
-	 * @param benutzername
-	 *            der Benutzername des Benutzers
-	 * @param passwort
-	 *            das Passwort des Benutzers
-	 * @param rolle
-	 *            die Rolle des Benutzerkontos
-	 * @param benutzer
-	 *            das PersonBean zu diesem Benutzer
-	 * @param ansprechpartner
-	 *            das PersonBean das dem Ansprechpartner des Benutzers
-	 *            entspricht
-	 * @param gesperrt
-	 *            ob der Benutzer gesperrt ist
-	 * @param ersterLogin
-	 *            Zeitpunkt des ersten Logins als GregorianCalendar
-	 * @param letzterLogin
-	 *            Zeitpunkt des letzten Logins als GregorianCalendar
-	 */
-	public BenutzerkontoBean(String benutzername, String passwort, Rolle rolle,
-			PersonBean benutzer, PersonBean ansprechpartner, boolean gesperrt,
-			GregorianCalendar ersterLogin, GregorianCalendar letzterLogin)
-			throws IllegalArgumentException {
-		this.setBenutzername(benutzername);
-		this.setPasswort(passwort);
-		this.setRolle(rolle);
-		this.setBenutzer(benutzer);
-		this.setAnsprechpartner(ansprechpartner);
-		this.setGesperrt(gesperrt);
-		this.setErsterLogin(ersterLogin);
-		this.setLetzterLogin(letzterLogin);
-	}
+    }
 
-	/**
-	 * Reduzierter Konstruktor, der die Attribute ersterLogin und letzterLogin
-	 * automatisch setzt.
-	 * 
-	 * @param benutzername
-	 *            der Benutzername des Benutzers
-	 * @param passwort
-	 *            das Passwort des Benutzers
-	 * @param benutzer
-	 *            das PersonBean zu diesem Benutzer
-	 */
-	public BenutzerkontoBean(String benutzername, String passwort,
-			PersonBean benutzer) throws IllegalArgumentException {
-		super();
-		this.setBenutzername(benutzername);
-		this.setPasswort(passwort);
-		this.setBenutzer(benutzer);
-	}
+    /**
+     * Der Konstruktor mit allen Attributen.
+     * 
+     * @param benutzername
+     *            der Benutzername des Benutzers
+     * @param passwort
+     *            das Passwort des Benutzers
+     * @param rolle
+     *            die Rolle des Benutzerkontos
+     * @param benutzer
+     *            das PersonBean zu diesem Benutzer
+     * @param ansprechpartner
+     *            das PersonBean das dem Ansprechpartner des Benutzers
+     *            entspricht
+     * @param gesperrt
+     *            ob der Benutzer gesperrt ist
+     * @param ersterLogin
+     *            Zeitpunkt des ersten Logins als GregorianCalendar
+     * @param letzterLogin
+     *            Zeitpunkt des letzten Logins als GregorianCalendar
+     */
+    public BenutzerkontoBean(String benutzername, String passwort, Rolle rolle,
+            PersonBean benutzer, PersonBean ansprechpartner, boolean gesperrt,
+            GregorianCalendar ersterLogin, GregorianCalendar letzterLogin)
+            throws IllegalArgumentException {
+        this.setBenutzername(benutzername);
+        this.setPasswort(passwort);
+        this.setRolle(rolle);
+        this.setBenutzer(benutzer);
+        this.setAnsprechpartner(ansprechpartner);
+        this.setGesperrt(gesperrt);
+        this.setErsterLogin(ersterLogin);
+        this.setLetzterLogin(letzterLogin);
+    }
 
-	/**
-	 * @return the benutzer
-	 */
-	public PersonBean getBenutzer() {
-		return benutzer;
-	}
+    /**
+     * Reduzierter Konstruktor, der die Attribute ersterLogin und letzterLogin
+     * automatisch setzt.
+     * 
+     * @param benutzername
+     *            der Benutzername des Benutzers
+     * @param passwort
+     *            das Passwort des Benutzers
+     * @param benutzer
+     *            das PersonBean zu diesem Benutzer
+     */
+    public BenutzerkontoBean(String benutzername, String passwort,
+            PersonBean benutzer) throws IllegalArgumentException {
+        super();
+        this.setBenutzername(benutzername);
+        this.setPasswort(passwort);
+        this.setBenutzer(benutzer);
+    }
 
-	/**
-	 * @param benutzer
-	 *            the benutzer to set
-	 */
-	public void setBenutzer(PersonBean benutzer) {
-		// keine Pruefung, da bei der Erzeugung der PersonBean schon alles
-		// geprueft wird
-		this.benutzer = benutzer;
-	}
+    /**
+     * @return the benutzer
+     */
+    public PersonBean getBenutzer() {
+        return benutzer;
+    }
 
-	/**
-	 * @return the benutzername
-	 */
-	public String getBenutzername() {
-		return benutzername;
-	}
+    /**
+     * @param benutzer
+     *            the benutzer to set
+     */
+    public void setBenutzer(PersonBean benutzer) {
+        // keine Pruefung, da bei der Erzeugung der PersonBean schon alles
+        // geprueft wird
+        this.benutzer = benutzer;
+    }
 
-	/**
-	 * @param benutzername
-	 *            the benutzername to set
-	 */
-	public void setBenutzername(String benutzername)
-			throws IllegalArgumentException {
-		if (benutzername == null) {
-			throw new IllegalArgumentException(
-					"Bitte geben Sie einen Benutzernamen ein.");
-		}
-		benutzername = benutzername.trim();
-		if (benutzername.length() == 0) {
-			throw new IllegalArgumentException(
-					"Bitte geben Sie einen Benutzernamen ein.");
-		}
-		if (!(benutzername.matches("(\\w|\\d|[.-]|\\@){4,14}"))) {
-			throw new IllegalArgumentException(
-					"Nur 4-14 Zeichen. Bitte geben Sie den Benutzernamen erneut ein.");
-		}
-		this.benutzername = benutzername;
-	}
+    /**
+     * @return the benutzername
+     */
+    public String getBenutzername() {
+        return benutzername;
+    }
 
-	/**
-	 * @return the ersterLogin
-	 */
-	public GregorianCalendar getErsterLogin() {
-		return ersterLogin;
-	}
+    /**
+     * @param benutzername
+     *            the benutzername to set
+     */
+    public void setBenutzername(String benutzername)
+            throws IllegalArgumentException {
+        if (benutzername == null) {
+            throw new IllegalArgumentException(
+                    "Bitte geben Sie einen Benutzernamen ein.");
+        }
+        benutzername = benutzername.trim();
+        if (benutzername.length() == 0) {
+            throw new IllegalArgumentException(
+                    "Bitte geben Sie einen Benutzernamen ein.");
+        }
+        if (!(benutzername.matches("(\\w|\\d|[.-]|\\@){4,14}"))) {
+            // FIXME 14 Zeichen sind IMO zu wenig, alleine
+            // "@med.uni-heidelberg.de" sind ja schon 23 Zeichen! BTheel
+            throw new IllegalArgumentException(
+                    "Nur 4-14 Zeichen. Bitte geben Sie den Benutzernamen erneut ein.");
+        }
+        this.benutzername = benutzername;
+    }
 
-	/**
-	 * @param ersterLogin
-	 *            the ersterLogin to set
-	 */
-	public void setErsterLogin(GregorianCalendar ersterLogin)
-			throws IllegalArgumentException {
-		// Testen, ob sich das Datum in der Zukunft befindet
-		if ((new GregorianCalendar(Locale.GERMANY)).before(ersterLogin)) {
-			throw new IllegalArgumentException(
-					"Zeit des ersten Logins ist fehlerhaft.");
-		}
-		this.ersterLogin = ersterLogin;
-	}
+    /**
+     * @return the ersterLogin
+     */
+    public GregorianCalendar getErsterLogin() {
+        return ersterLogin;
+    }
 
-	/**
-	 * @return the gesperrt
-	 */
-	public boolean isGesperrt() {
-		return gesperrt;
-	}
+    /**
+     * @param ersterLogin
+     *            the ersterLogin to set
+     */
+    public void setErsterLogin(GregorianCalendar ersterLogin)
+            throws IllegalArgumentException {
+        // Testen, ob sich das Datum in der Zukunft befindet
+        if ((new GregorianCalendar(Locale.GERMANY)).before(ersterLogin)) {
+            throw new IllegalArgumentException(
+                    "Zeit des ersten Logins ist fehlerhaft.");
+        }
+        this.ersterLogin = ersterLogin;
+    }
 
-	/**
-	 * @param gesperrt
-	 *            the gesperrt to set
-	 */
-	public void setGesperrt(boolean gesperrt) {
-		this.gesperrt = gesperrt;
-	}
+    /**
+     * @return the gesperrt
+     */
+    public boolean isGesperrt() {
+        return gesperrt;
+    }
 
-	/**
-	 * @return the letzterLogin
-	 */
-	public GregorianCalendar getLetzterLogin() {
-		return letzterLogin;
-	}
+    /**
+     * @param gesperrt
+     *            the gesperrt to set
+     */
+    public void setGesperrt(boolean gesperrt) {
+        this.gesperrt = gesperrt;
+    }
 
-	/**
-	 * @param letzterLogin
-	 *            the letzterLogin to set
-	 */
-	public void setLetzterLogin(GregorianCalendar letzterLogin)
-			throws IllegalArgumentException {
-		// Testen, ob sich das Datum in der Zukunft befindet
-		if ((new GregorianCalendar(Locale.GERMANY)).before(letzterLogin)) {
-			throw new IllegalArgumentException(
-					"Zeit des letzten Logins ist fehlerhaft.");
-		}
-		this.letzterLogin = letzterLogin;
-	}
+    /**
+     * @return the letzterLogin
+     */
+    public GregorianCalendar getLetzterLogin() {
+        return letzterLogin;
+    }
 
-	/**
-	 * @return the passwort
-	 */
-	public String getPasswort() {
-		return passwort;
-	}
+    /**
+     * @param letzterLogin
+     *            the letzterLogin to set
+     */
+    public void setLetzterLogin(GregorianCalendar letzterLogin)
+            throws IllegalArgumentException {
+        // Testen, ob sich das Datum in der Zukunft befindet
+        if ((new GregorianCalendar(Locale.GERMANY)).before(letzterLogin)) {
+            throw new IllegalArgumentException(
+                    "Zeit des letzten Logins ist fehlerhaft.");
+        }
+        this.letzterLogin = letzterLogin;
+    }
 
-	/**
-	 * @param passwort
-	 *            the passwort to set
-	 */
-	public void setPasswort(String passwort) throws IllegalArgumentException {
-		if (passwort == null) {
-			throw new IllegalArgumentException(
-					"Bitte geben Sie ein Passwort ein.");
-		}
-		passwort = passwort.trim();
-		if (passwort.length() == 0) {
-			throw new IllegalArgumentException(
-					"Bitte geben Sie ein Passwort ein.");
-		}
-		if (passwort.length() < 6) {
-			throw new IllegalArgumentException(
-					"Mindestens 6 Zeichen. Bitte geben Sei ein anderes Passwort ein.");
-		}
-		if (!(passwort.matches(".*[A-Za-z].*") || passwort.matches(".*[0-9].*") /*
-																				 * ||
-																				 * passwort.matches(".*[\^,.-#+;:_'*!\"�$%&/()=?|<>].*")
-																				 */)) {
-			throw new IllegalArgumentException(
-					"Beachten Sie die Vorschriften! Bitte geben Sie ein anderes Passwort ein.");
-		}
-		this.passwort = passwort;
-	}
+    /**
+     * @return the passwort
+     */
+    public String getPasswort() {
+        return passwort;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString() {
+    /**
+     * @param passwort
+     *            the passwort to set
+     */
+    public void setPasswort(String passwort) throws IllegalArgumentException {
+        if (passwort == null) {
+            throw new IllegalArgumentException(
+                    "Bitte geben Sie ein Passwort ein.");
+        }
+        passwort = passwort.trim();
+        if (passwort.length() == 0) {
+            throw new IllegalArgumentException(
+                    "Bitte geben Sie ein Passwort ein.");
+        }
+        if (passwort.length() < 6) {
+            throw new IllegalArgumentException(
+                    "Mindestens 6 Zeichen. Bitte geben Sei ein anderes Passwort ein.");
+        }
+        if (!(passwort.matches(".*[A-Za-z].*") || passwort.matches(".*[0-9].*") /*
+                                                                                 * ||
+                                                                                 * passwort.matches(".*[\^,.-#+;:_'*!\"�$%&/()=?|<>].*")
+                                                                                 */)) {
+            throw new IllegalArgumentException(
+                    "Beachten Sie die Vorschriften! Bitte geben Sie ein anderes Passwort ein.");
+        }
+        this.passwort = passwort;
+    }
 
-		// TODO
-		return null;
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
 
-	}
+        // TODO
+        return null;
 
-	/**
-	 * Diese Methode prueft ob die aktuelle Instanz dem uebergebenen Objekt
-	 * entspricht.
-	 * 
-	 * @param zuvergleichendesObjekt
-	 *            das zu vergleichende Objekt vom selben Typ
-	 * @return TRUE oder FALSE, je nach Ergebnis des Vergleichs
-	 */
-	public boolean equals(BenutzerkontoBean zuvergleichendesObjekt) {
+    }
 
-		// TODO
-		return false;
+    /**
+     * Diese Methode prueft ob die aktuelle Instanz dem uebergebenen Objekt
+     * entspricht.
+     * 
+     * @param zuvergleichendesObjekt
+     *            das zu vergleichende Objekt vom selben Typ
+     * @return TRUE oder FALSE, je nach Ergebnis des Vergleichs
+     */
+    public boolean equals(BenutzerkontoBean zuvergleichendesObjekt) {
 
-	}
+        // TODO
+        return false;
 
-	/**
-	 * @return the ansprechpartner
-	 */
-	public PersonBean getAnsprechpartner() {
-		return ansprechpartner;
-	}
+    }
 
-	/**
-	 * @param ansprechpartner
-	 *            the ansprechpartner to set
-	 */
-	public void setAnsprechpartner(PersonBean ansprechpartner) {
-		// keine Pruefung, da bei der Erzeugung der PersonBean schon alles
-		// geprueft wird
-		this.ansprechpartner = ansprechpartner;
-	}
+    /**
+     * @return the ansprechpartner
+     */
+    public PersonBean getAnsprechpartner() {
+        return ansprechpartner;
+    }
 
-	public void setRolle(Rolle rolle) throws IllegalArgumentException {
-		if (rolle == null) {
-			throw new IllegalArgumentException("Keine Rolle!");
-		}
-		if (!(rolle == Rolle.getAdmin() || rolle == Rolle.getStatistiker()
-				|| rolle == Rolle.getStudienleiter()
-				|| rolle == Rolle.getStudienarzt() || rolle == Rolle.getSysop())) {
-			throw new IllegalArgumentException("Falsche Rolle!");
-		}
-		this.rolle = rolle;
-	}
+    /**
+     * @param ansprechpartner
+     *            the ansprechpartner to set
+     */
+    public void setAnsprechpartner(PersonBean ansprechpartner) {
+        // keine Pruefung, da bei der Erzeugung der PersonBean schon alles
+        // geprueft wird
+        this.ansprechpartner = ansprechpartner;
+    }
 
-	public Rolle getRolle() {
-		return this.rolle;
-	}
+    public void setRolle(Rolle rolle) throws IllegalArgumentException {
+        if (rolle == null) {
+            throw new IllegalArgumentException("Keine Rolle!");
+        }
+        if (!(rolle == Rolle.getAdmin() || rolle == Rolle.getStatistiker()
+                || rolle == Rolle.getStudienleiter()
+                || rolle == Rolle.getStudienarzt() || rolle == Rolle.getSysop())) {
+            throw new IllegalArgumentException("Falsche Rolle!");
+        }
+        this.rolle = rolle;
+    }
+
+    public Rolle getRolle() {
+        return this.rolle;
+    }
 
     /**
      * @return the id
@@ -340,7 +341,8 @@ public class BenutzerkontoBean {
     }
 
     /**
-     * @param id the id to set
+     * @param id
+     *            the id to set
      */
     public void setId(long id) {
         this.id = id;
