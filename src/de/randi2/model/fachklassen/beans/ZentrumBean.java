20f94f43647e2adc9793e76461c1c118b1aabade
lplotni <lplotni@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-02-01 12:59:49 +0000

    ZentrumBean wurde in rudimaenterer Form eingerichtet.
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@341 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/model/fachklassen/beans/ZentrumBean.java
@@ -0,0 +1,22 @@
+package de.randi2.model.fachklassen.beans;
+
+/**
+ * Diese Klasse repraesentiert ein Zentrum.
+ * 
+ * @author Lukasz Plotnicki <lplotni@stud.hs-heilbronn.de>
+ * @version $Id$
+ * 
+ */
+public class ZentrumBean {
+
+	/*
+	 * Die Attribute dieser Klasse muessen noch hinzugefuegt werden.
+	 */
+
+	/**
+	 * Einfacher Konstruktor von dieser Klasse.
+	 */
+	public ZentrumBean() {
+
+	}
+}
