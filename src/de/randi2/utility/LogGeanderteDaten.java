30fd7a412ce2632d53901ef6e944f6f40ac4c3d7
jthoenes <jthoenes@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-02-01 11:24:40 +0000

    Log-Klassen entwickelt
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@327 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/utility/LogGeanderteDaten.java
@@ -0,0 +1,90 @@
+package de.randi2.utility;
+
+import java.util.HashMap;
+
+/**
+ * Die Klasse LogGeanderteDaten kapselt die geanderten Daten eines
+ * Datenbankobjekts und stelt get-Methoden zur Verfuegung.
+ * 
+ * @author Johannes Th&ouml;nes <jthoenes@stud.hs-heilbronn.de>
+ * @version $Id$
+ * 
+ */
+public class LogGeanderteDaten {
+
+	/**
+	 * Die Id des Datenobjekts.
+	 */
+	private long id = Long.MAX_VALUE;
+
+	/**
+	 * Der Typ des Datenobjekts.
+	 */
+	private String typ = null;
+
+	/**
+	 * Die geanderten Daten.
+	 */
+	private HashMap<String, String> daten = null;
+
+	/**
+	 * Konstruktor fuer ein geandertes Datenobjekt.
+	 * 
+	 * @param id
+	 *            Die id des Datenobjekts in der Datenbank. Muss groesser als 0
+	 *            sein.
+	 * @param typ
+	 *            Der Typ des Datenobjekts. Darf nicht leer sein.
+	 * @param daten
+	 *            Die geanderten Daten. Darf nicht leer sein.
+	 * @throws IllegalArgumentException
+	 *             Falls leere Objekte uebergeben wurden.
+	 */
+	public LogGeanderteDaten(long id, String typ, HashMap<String, String> daten)
+			throws IllegalArgumentException {
+		super();
+
+		if (id <= 0) {
+			throw new IllegalArgumentException("Id darf nicht 0 sein");
+		}
+		this.id = id;
+		if (typ == null || typ.equals("")) {
+			throw new IllegalArgumentException("Typ darf nicht leer sein");
+		}
+		this.typ = typ;
+
+		if (this.daten == null || this.daten.isEmpty()) {
+			throw new IllegalArgumentException(
+					"Die geanderten Daten duerfen nicht leer sein");
+		}
+		this.daten = daten;
+	}
+
+	/**
+	 * Gibt die geanderten Daten zurueck.
+	 * 
+	 * @return Die geanderten Daten.
+	 */
+	public HashMap<String, String> getDaten() {
+		return daten;
+	}
+
+	/**
+	 * Gibt die Id des geanderten Objekts zurueck.
+	 * 
+	 * @return Die Id.
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * Der Typ des geanderten Objekts.
+	 * 
+	 * @return Der Typ.
+	 */
+	public String getTyp() {
+		return typ;
+	}
+
+}
