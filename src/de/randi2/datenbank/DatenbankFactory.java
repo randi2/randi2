25ae76e2a00a87861ad0a72185b6613256d7ab2a
lplotni <lplotni@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-02-01 11:55:07 +0000

    DatenbankFactory Klasse wurde eingerichtet. Das loest das Singelton vs. Interface Problem.
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@331 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/datenbank/DatenbankFactory.java
@@ -0,0 +1,55 @@
+package de.randi2.datenbank;
+
+/**
+ * <p>
+ * <b>FACTORY PATTERN</b><br>
+ * Diese Klasse kuemmert sich um die Erzeugung der Objekte von der
+ * Datenbankklasse. Sie ist immer anzusprechen, wenn eine Datenbank Instanz
+ * gebraucht wird.
+ * </p>
+ * (FACTORY PATTERN)
+ * 
+ * @author Lukasz Plotnicki <lplotni@stud.hs-heilbronn.de>
+ * @version $Id $
+ */
+public class DatenbankFactory {
+
+	/**
+	 * Das ist eine akutelle Instanz von der DatenbankFactory Klasse. ( <b>SINGELTON PATTERN</b> )
+	 */
+	private static DatenbankFactory aDBFactory = null;
+
+	/**
+	 * Die Instanz von der Datenbank Klasse ( <b>SINGELTON PATTERN</b> )
+	 */
+	private static DatenbankSchnittstelle aDB = null;
+
+	/**
+	 * Diese Methode ist aufzurufen, wenn ein Objekt von dieser Klasse (DatenbankFactory) benoetigt wird. 
+	 * @return DatenbankFactory Es wird immer die aktuellste Instanz dieser Klasse zurueckgeliefert.
+	 */
+	public static DatenbankFactory getInstanz() {
+		if (aDBFactory == null) {
+			aDBFactory = new DatenbankFactory();
+		}
+		return aDBFactory;
+	}
+
+	/**
+	 * Konstruktor von dieser Klasse.
+	 */
+	private DatenbankFactory() {
+
+	}
+
+	/**
+	 * Diese Methode ist aufzurufen, wenn eine Datenbank Instanz benoetigt wird.
+	 * @return DatenbankSchnittstelle  Es wird immer die aktuellste Instanz zureuckgeliefert.
+	 */
+	public static DatenbankSchnittstelle getAktuelleDBInstanz() {
+		if (aDB == null) {
+			aDB = new DatenbankDummy();
+		}
+		return aDB;
+	}
+}
