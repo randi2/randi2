713b4c9407db34c49564cd8f949755e295b898b6
kchruscz <kchruscz@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-02-02 09:00:02 +0000

    JunitTest-Ordner hinzugefügt
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@350 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/junittests/BenutzerkontoTest.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package junittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author user
+ *
+ */
+public class BenutzerkontoTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#Benutzerkonto(de.randi2.model.fachklassen.beans.BenutzerkontoBean)}.
+	 */
+	@Test
+	public void testBenutzerkonto() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#suchenBenutzer(de.randi2.model.fachklassen.beans.BenutzerkontoBean)}.
+	 */
+	@Test
+	public void testSuchenBenutzer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#anlegenBenutzer(de.randi2.model.fachklassen.beans.BenutzerkontoBean)}.
+	 */
+	@Test
+	public void testAnlegenBenutzer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#getBenutzer(java.lang.String)}.
+	 */
+	@Test
+	public void testGetBenutzer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#equals(de.randi2.model.fachklassen.Benutzerkonto)}.
+	 */
+	@Test
+	public void testEqualsBenutzerkonto() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#getBenutzerkontobean()}.
+	 */
+	@Test
+	public void testGetBenutzerkontobean() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#pruefenPasswort(java.lang.String)}.
+	 */
+	@Test
+	public void testPruefenPasswort() {
+		fail("Not yet implemented");
+	}
+
+}
