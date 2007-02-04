75e2dbc0a2a896f4416a7b25a0ef3184d5022a59
kchruscz <kchruscz@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-02-04 09:55:03 +0000

    BenutzerkontoTest angefangen... noch nicht fertig!
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@366 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/junittests/BenutzerkontoTest.java
+++ src/junittests/BenutzerkontoTest.java
@@ -1,9 +1,8 @@
-/**
- * 
- */
 package junittests;
 
 import static org.junit.Assert.*;
+
+import java.util.GregorianCalendar;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,31 +10,63 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.randi2.model.fachklassen.beans.BenutzerkontoBean;
+import de.randi2.model.fachklassen.beans.PersonBean;
+import de.randi2.model.fachklassen.beans.ZentrumBean;
+import de.randi2.model.fachklassen.Benutzerkonto;
+import de.randi2.model.fachklassen.Rolle;
+
 /**
- * @author user
- *
+ * @author Katharina Chruscz <kchruscz@stud.hs-heilbronn.de>
+ * @version 1.0 (2007-02-03)
+ * 
  */
 public class BenutzerkontoTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+	private static final char m = 0;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	private char geschlecht = m;
+
+	private Benutzerkonto bKonto;
+
+	private BenutzerkontoBean bKontoBean;
+
+	private String benutzername, passwort;
+
+	private Rolle rolle;
+
+	private PersonBean benutzer, ansprechpartner;
+
+	private boolean gesperrt;
+
+	private ZentrumBean zentrum;
+
+	private GregorianCalendar ersterLogin, letzterLogin;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+
+		benutzername = "studienleiter";
+		passwort = "1$studienleiter";
+		rolle = Rolle.getStudienleiter();
+		benutzer = new PersonBean("Mueller", "Hans", "Dr.", geschlecht,
+				"blabla@bla.de", "032452342", "973423", "12345");
+		ansprechpartner = new PersonBean("Schmidt", "Juergen", "Dr.",
+				geschlecht, "blabla@bla.de", "032452342", "973423", "12345");
+		gesperrt = false;
+
+		zentrum = new ZentrumBean("institution", "abteilung", "ort", "plz",
+				"strasse", "hausnr", ansprechpartner, "passwort");
+		ersterLogin = new GregorianCalendar();
+		letzterLogin = new GregorianCalendar();
+
+		bKontoBean = new BenutzerkontoBean(benutzername, passwort, rolle,
+				benutzer, ansprechpartner, gesperrt, zentrum, ersterLogin,
+				letzterLogin);
+		bKonto = new Benutzerkonto(bKontoBean);
 	}
 
 	/**
@@ -43,50 +74,85 @@ public class BenutzerkontoTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		bKonto = null;
 	}
 
 	/**
-	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#Benutzerkonto(de.randi2.model.fachklassen.beans.BenutzerkontoBean)}.
+	 * Test method for
+	 * {@link de.randi2.model.fachklassen.Benutzerkonto#Benutzerkonto(de.randi2.model.fachklassen.beans.BenutzerkontoBean)}.
 	 */
 	@Test
 	public void testBenutzerkonto() {
 		fail("Not yet implemented");
+
 	}
 
 	/**
-	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#suchenBenutzer(de.randi2.model.fachklassen.beans.BenutzerkontoBean)}.
+	 * Test method for
+	 * {@link de.randi2.model.fachklassen.Benutzerkonto#suchenBenutzer(de.randi2.model.fachklassen.beans.BenutzerkontoBean)}.
 	 */
 	@Test
 	public void testSuchenBenutzer() {
-		fail("Not yet implemented");
+		try {
+
+		} catch (Exception e) {
+			fail("Not yet implemented");
+		}
 	}
 
 	/**
-	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#anlegenBenutzer(de.randi2.model.fachklassen.beans.BenutzerkontoBean)}.
+	 * Test method for
+	 * {@link de.randi2.model.fachklassen.Benutzerkonto#anlegenBenutzer(de.randi2.model.fachklassen.beans.BenutzerkontoBean)}.
 	 */
 	@Test
 	public void testAnlegenBenutzer() {
-		fail("Not yet implemented");
+		try {
+			bKonto.anlegenBenutzer(bKontoBean);
+		} catch (Exception e) {
+			fail("Fehler aufgetreten bei testAnlegenBenutzer");
+		}
 	}
 
 	/**
-	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#getBenutzer(java.lang.String)}.
+	 * Test method for
+	 * {@link de.randi2.model.fachklassen.Benutzerkonto#getBenutzer(java.lang.String)}.
 	 */
 	@Test
 	public void testGetBenutzer() {
-		fail("Not yet implemented");
+		try {
+			String benutzerSa = "sa@randi2.de";
+			String benutzerSl = "studienleider";
+			String benutzerSt = "statistiker";
+			String benutzerAd = "administrator";
+			String benutzerSy = "systemoperator";
+			bKonto.getBenutzer(benutzerSa);
+			bKonto.getBenutzer(benutzerSl);
+			bKonto.getBenutzer(benutzerSt);
+			bKonto.getBenutzer(benutzerAd);
+			bKonto.getBenutzer(benutzerSy);
+		} catch (Exception e) {
+			fail("Fehler aufgetreten bei testGetBenutzer");
+		}
+
 	}
 
 	/**
-	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#toString()}.
+	 * Test method for
+	 * {@link de.randi2.model.fachklassen.Benutzerkonto#toString()}.
 	 */
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		try {
+			bKonto.toString();
+
+		} catch (Exception e) {
+			fail("Fehler aufgetreten bei testToString");
+		}
 	}
 
 	/**
-	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#equals(de.randi2.model.fachklassen.Benutzerkonto)}.
+	 * Test method for
+	 * {@link de.randi2.model.fachklassen.Benutzerkonto#equals(de.randi2.model.fachklassen.Benutzerkonto)}.
 	 */
 	@Test
 	public void testEqualsBenutzerkonto() {
@@ -94,7 +160,8 @@ public class BenutzerkontoTest {
 	}
 
 	/**
-	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#getBenutzerkontobean()}.
+	 * Test method for
+	 * {@link de.randi2.model.fachklassen.Benutzerkonto#getBenutzerkontobean()}.
 	 */
 	@Test
 	public void testGetBenutzerkontobean() {
@@ -102,11 +169,32 @@ public class BenutzerkontoTest {
 	}
 
 	/**
-	 * Test method for {@link de.randi2.model.fachklassen.Benutzerkonto#pruefenPasswort(java.lang.String)}.
+	 * Test method for
+	 * {@link de.randi2.model.fachklassen.Benutzerkonto#pruefenPasswort(java.lang.String)}.
 	 */
 	@Test
-	public void testPruefenPasswort() {
+	public void testPruefenPasswortFalsch() {
 		fail("Not yet implemented");
+	}
+
+	public void testPruefenPasswortRichtig() {
+
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		// wird erstmal nicht benötigt
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		// wird erstmal nicht benötigt
 	}
 
 }
