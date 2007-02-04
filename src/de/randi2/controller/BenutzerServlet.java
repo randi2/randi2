5d2ffe557ade212b63d91ccaf2dd4d23896eb2a2
dhaehn <dhaehn@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-02-04 16:10:59 +0000

    illegalargumentexception von BenutzerkontoBean abgefangen
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@393 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/controller/BenutzerServlet.java
+++ src/de/randi2/controller/BenutzerServlet.java
@@ -60,147 +60,157 @@ public class BenutzerServlet extends javax.servlet.http.HttpServlet implements
 		// Login
 		if (id.equals("CLASS_DISPATCHERSERVLET_LOGIN1")) {
 			BenutzerkontoBean sBenutzer = new BenutzerkontoBean();
-			sBenutzer
-					.setBenutzername((String) request.getParameter("username"));
-			// Zukünftig sollte hier gleich das Passwort überprüft werden
-			sBenutzer.setPasswort(PasswortUtil.getInstance().hashPasswort(
-					(String) request.getParameter("password")));
-			Vector<BenutzerkontoBean> gBenutzer = Benutzerkonto
-					.suchenBenutzer(sBenutzer);
-			System.out.println("achtung"+gBenutzer.size());
-			if (gBenutzer.size() == 1) {
-				if (!gBenutzer.get(0).isGesperrt()
-						&& new Benutzerkonto(gBenutzer.firstElement())
-								.pruefenPasswort((String) request
-										.getParameter("password"))) {
-					request.getSession(true).setAttribute("aBenutzer",
-							gBenutzer.firstElement());
-					request.setAttribute("anfrage_id",
-							"CLASS_BENUTZERSERVLET_LOGIN_OK");
-					LogAktion a = new LogAktion(
-							"Benutzer hat sich erfolgreich eingeloggt",
-							gBenutzer.firstElement());
-					Logger.getLogger(LogLayout.LOGIN_LOGOUT).info(a);
+			try {
+				sBenutzer.setBenutzername((String) request
+						.getParameter("username"));
+				// Zukünftig sollte hier gleich das Passwort überprüft werden
+				sBenutzer.setPasswort(PasswortUtil.getInstance().hashPasswort(
+						(String) request.getParameter("password")));
+
+				Vector<BenutzerkontoBean> gBenutzer = Benutzerkonto
+						.suchenBenutzer(sBenutzer);
+				System.out.println("achtung" + gBenutzer.size());
+				if (gBenutzer.size() == 1) {
+					if (!gBenutzer.get(0).isGesperrt()
+							&& new Benutzerkonto(gBenutzer.firstElement())
+									.pruefenPasswort((String) request
+											.getParameter("password"))) {
+						request.getSession(true).setAttribute("aBenutzer",
+								gBenutzer.firstElement());
+						request.setAttribute("anfrage_id",
+								"CLASS_BENUTZERSERVLET_LOGIN_OK");
+						LogAktion a = new LogAktion(
+								"Benutzer hat sich erfolgreich eingeloggt",
+								gBenutzer.firstElement());
+						Logger.getLogger(LogLayout.LOGIN_LOGOUT).info(a);
+					}// if
+					else {
+						request.setAttribute("fehlernachricht", "Loginfehler");
+						request.setAttribute("anfrage_id",
+								"CLASS_BENUTZERSERVLET_LOGIN_ERROR");
+						LogAktion a = new LogAktion(
+								"Falsches Passwort eingegeben.", sBenutzer);
+						Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
+					}
 				}// if
 				else {
 					request.setAttribute("fehlernachricht", "Loginfehler");
 					request.setAttribute("anfrage_id",
 							"CLASS_BENUTZERSERVLET_LOGIN_ERROR");
-					LogAktion a = new LogAktion(
-							"Falsches Passwort eingegeben.", sBenutzer);
-					Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
-				}
-			}// if
-			else {
+					if (gBenutzer.size() == 0) {
+						LogAktion a = new LogAktion(
+								"Falscher Benutzernamen eingegeben.", sBenutzer);
+						Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
+					} else {
+						Logger.getLogger(this.getClass()).fatal(
+								"Mehr als einen Benutzer fuer '"
+										+ sBenutzer.getBenutzername()
+										+ "' in der Datenbank gefunden!");
+					}
+				}// else
+
+			} catch (IllegalArgumentException e) {
+				
+				// Ungueltiger Benutzername/Passwort
+				
 				request.setAttribute("fehlernachricht", "Loginfehler");
 				request.setAttribute("anfrage_id",
 						"CLASS_BENUTZERSERVLET_LOGIN_ERROR");
-				if (gBenutzer.size() == 0) {
-					LogAktion a = new LogAktion(
-							"Falscher Benutzernamen eingegeben.", sBenutzer);
-					Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
-				} else {
-					Logger.getLogger(this.getClass()).fatal(
-							"Mehr als einen Benutzer fuer '"
-									+ sBenutzer.getBenutzername()
-									+ "' in der Datenbank gefunden!");
-				}
-			}// else
+				LogAktion a = new LogAktion(
+						"Ungueltige Benutzername/Passwort Kombination eingegeben.",
+						sBenutzer);
+				Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
+			}
+
 			request.getRequestDispatcher("DispatcherServlet").forward(request,
 					response);
 		}// if
 
-//		// Benutzer registrieren
-//		// Schritt 1:
-//		else if (id
-//				.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_ZWEI")) {
-//
-//			request.setAttribute("anfrage_id",
-//					"CLASS_BENUTZERSERVLET_BENUTZER_REGISTRIEREN_ZWEI");
-//			// Hier noch jede Menge Logik
-//			request.getRequestDispatcher("DispatcherServlet").forward(request,
-//					response);
-//		}
-//		// Schritt 2:
-//		else if (id
-//				.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_DREI")) {
-//			request.setAttribute("anfrage_id",
-//					"CLASS_BENUTZERSERVLET_BENUTZER_REGISTRIEREN_DREI");
-//			// Hier noch jede Menge Logik
-//			request.getRequestDispatcher("DispatcherServlet").forward(request,
-//					response);
-//			request.getRequestDispatcher("/benutzer_anlegen_drei.jsp").forward(request, response);
-//
-//		}
-//		// Schritt 3:
-//		else if (id
-//				.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_VIER")) {
-//			request.setAttribute("anfrage_id",
-//					"CLASS_BENUTZERSERVLET_BENUTZER_REGISTRIEREN_VIER");
-//			// Hier noch jede Menge Logik
-//			request.getRequestDispatcher("DispatcherServlet").forward(request,
-//					response);
-//
-//		}
-		//Letzter Schritt Benutzer registrieren
-		if (id.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_VIER"))
-		{
-			
-			String fehlernachricht="";
-			try{
-			String titel=request.getParameter("Titel");
-			if(titel!=null&&titel.equals("kein Titel"))
-			{
-				titel=null;
-			}
-			String vorname=request.getParameter("Vorname");
-			String nachname=request.getParameter("Nachname");
-			char geschlecht=NullKonstanten.NULL_CHAR;
-			String passwort=null;
-			String email=request.getParameter("Email");
-			String telefon=request.getParameter("Telefon");
-			String fax=request.getParameter("fax");
-			//TODO dirty fix
-			String handynummer="123123123123";
-			String institut=request.getParameter("Institut");
-			//Geschlecht abfragen
-			if(request.getParameter("maennlich")!=null)
-			{
-				geschlecht='m';
-			}
-			else if(request.getParameter("weiblich")!=null)
-			{
-				geschlecht='w';
-			}
-			//Wiederholte Passworteingabe prüfen
-			if(request.getParameter("Passwort")!=null&&request.getParameter("Passwort_wh")!=null)
-			{
-				if(request.getParameter("Passwort").equals(request.getParameter("Passwort_wh")))
-				{
-					passwort=request.getParameter("Passwort");
+		// // Benutzer registrieren
+		// // Schritt 1:
+		// else if (id
+		// .equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_ZWEI")) {
+		//
+		// request.setAttribute("anfrage_id",
+		// "CLASS_BENUTZERSERVLET_BENUTZER_REGISTRIEREN_ZWEI");
+		// // Hier noch jede Menge Logik
+		// request.getRequestDispatcher("DispatcherServlet").forward(request,
+		// response);
+		// }
+		// // Schritt 2:
+		// else if (id
+		// .equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_DREI")) {
+		// request.setAttribute("anfrage_id",
+		// "CLASS_BENUTZERSERVLET_BENUTZER_REGISTRIEREN_DREI");
+		// // Hier noch jede Menge Logik
+		// request.getRequestDispatcher("DispatcherServlet").forward(request,
+		// response);
+		// request.getRequestDispatcher("/benutzer_anlegen_drei.jsp").forward(request,
+		// response);
+		//
+		// }
+		// // Schritt 3:
+		// else if (id
+		// .equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_VIER")) {
+		// request.setAttribute("anfrage_id",
+		// "CLASS_BENUTZERSERVLET_BENUTZER_REGISTRIEREN_VIER");
+		// // Hier noch jede Menge Logik
+		// request.getRequestDispatcher("DispatcherServlet").forward(request,
+		// response);
+		//
+		// }
+		// Letzter Schritt Benutzer registrieren
+		if (id.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_VIER")) {
+
+			String fehlernachricht = "";
+			try {
+				String titel = request.getParameter("Titel");
+				if (titel != null && titel.equals("kein Titel")) {
+					titel = null;
 				}
-				else
-				{
-					fehlernachricht+="Passwort und wiederholtes Passwort sind nicht gleich";
+				String vorname = request.getParameter("Vorname");
+				String nachname = request.getParameter("Nachname");
+				char geschlecht = NullKonstanten.NULL_CHAR;
+				String passwort = null;
+				String email = request.getParameter("Email");
+				String telefon = request.getParameter("Telefon");
+				String fax = request.getParameter("fax");
+				// TODO dirty fix
+				String handynummer = "123123123123";
+				String institut = request.getParameter("Institut");
+				// Geschlecht abfragen
+				if (request.getParameter("maennlich") != null) {
+					geschlecht = 'm';
+				} else if (request.getParameter("weiblich") != null) {
+					geschlecht = 'w';
 				}
+				// Wiederholte Passworteingabe prüfen
+				if (request.getParameter("Passwort") != null
+						&& request.getParameter("Passwort_wh") != null) {
+					if (request.getParameter("Passwort").equals(
+							request.getParameter("Passwort_wh"))) {
+						passwort = request.getParameter("Passwort");
+					} else {
+						fehlernachricht += "Passwort und wiederholtes Passwort sind nicht gleich";
+					}
+				}
+				PersonBean aPerson = new PersonBean(nachname, vorname, titel,
+						geschlecht, email, telefon, handynummer, fax);
+				BenutzerkontoBean aBenutzerkonto = new BenutzerkontoBean(email,
+						passwort, aPerson);
+				Benutzerkonto.anlegenBenutzer(aBenutzerkonto);
+
+			} catch (IllegalArgumentException e) {
+				fehlernachricht += e.getMessage();
 			}
-			PersonBean aPerson=new PersonBean(nachname,vorname,titel,geschlecht,email,telefon,handynummer,fax);
-			BenutzerkontoBean aBenutzerkonto=new BenutzerkontoBean(email,passwort,aPerson);
-			Benutzerkonto.anlegenBenutzer(aBenutzerkonto);
-			
-			
-			}
-			catch(IllegalArgumentException e)
-			{fehlernachricht+=e.getMessage();}
-			//Daten waren nicht fehlerfrei
-			if(fehlernachricht!="")
-			{
+			// Daten waren nicht fehlerfrei
+			if (fehlernachricht != "") {
 				request.setAttribute("fehlernachricht", fehlernachricht);
-				request.getRequestDispatcher("/benutzer_anlegen_drei.jsp").forward(request, response);
-			}
-			else
-			{
-				request.getRequestDispatcher("/benutzer_anlegen_vier.jsp").forward(request, response);
+				request.getRequestDispatcher("/benutzer_anlegen_drei.jsp")
+						.forward(request, response);
+			} else {
+				request.getRequestDispatcher("/benutzer_anlegen_vier.jsp")
+						.forward(request, response);
 			}
 		}
 
