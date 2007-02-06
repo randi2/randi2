5aeb092fcbafd9e037d53df3b992eab40a55d90e
nzwink <nzwink@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-02-06 13:55:14 +0000

    bei BenutzerkotnoBeanTest noch eine Kleinigkeit verbessert
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@460 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/junittests/BenutzerkontoBeanTest.java
+++ src/de/randi2/junittests/BenutzerkontoBeanTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
-import org.apache.log4j.PropertyConfigurator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +13,6 @@ import de.randi2.datenbank.Filter;
 import de.randi2.model.exceptions.BenutzerkontoException;
 import de.randi2.model.exceptions.PersonException;
 import de.randi2.model.fachklassen.Rolle;
-import de.randi2.model.fachklassen.Zentrum;
 import de.randi2.model.fachklassen.Rolle.Rollen;
 import de.randi2.model.fachklassen.beans.BenutzerkontoBean;
 import de.randi2.model.fachklassen.beans.PersonBean;
@@ -38,7 +36,6 @@ public class BenutzerkontoBeanTest extends Filter{
     
     @Before
     public void setUp() throws Exception {
-    	PropertyConfigurator.configure("/RANDI2/WebContent/WEB-INF/log4j.lcf");
         aKonto = new BenutzerkontoBean();
     }
 
@@ -219,18 +216,13 @@ public class BenutzerkontoBeanTest extends Filter{
 		letzterLogin = new GregorianCalendar(jahrLetzterLogin, monatLetzterLogin - 1, tagLetzterLogin);
 		ersterLogin=new GregorianCalendar(jahrLetzterLogin, monatLetzterLogin - 1, tagLetzterLogin);
 		
-    	//try {
-			aKonto = new BenutzerkontoBean(benutzername, passwort, rolle,
-					 benutzer, ansprechpartner, gesperrt,zentrum, 
-					 ersterLogin,letzterLogin);
-			cKonto = new BenutzerkontoBean("", "", null,null, null, false, null,
-	    			 null, null);
+    	aKonto = new BenutzerkontoBean(benutzername, passwort, rolle,
+				 benutzer, ansprechpartner, gesperrt,zentrum, 
+				 ersterLogin,letzterLogin);
+		cKonto = new BenutzerkontoBean("", "", null,null, null, false, null,
+	    		 null, null);
 	    
-			assertFalse(aKonto.equals(cKonto));
-			
-	/**	} catch (BenutzerkontoException e) {
-			e.printStackTrace();
-		}*/
+		assertFalse(aKonto.equals(cKonto));
     }
 
     
