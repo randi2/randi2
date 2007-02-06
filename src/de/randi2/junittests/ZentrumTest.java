820da1514013e02a00b6a85f527fef6fa1fc2c54
tnoack <tnoack@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-02-06 18:44:36 +0000

    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@476 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/junittests/ZentrumTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package de.randi2.junittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Tino Noack [tino.noack@web.de]
+ * @version $Id
+ */
+public class ZentrumTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Zentrum#Zentrum(de.randi2.model.fachklassen.beans.ZentrumBean)}.
+	 */
+	@Test
+	public void testZentrum() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Zentrum#suchenZentrum(de.randi2.model.fachklassen.beans.ZentrumBean)}.
+	 */
+	@Test
+	public void testSuchenZentrum() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Zentrum#pruefenPasswort(java.lang.String)}.
+	 */
+	@Test
+	public void testPruefenPasswort() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.randi2.model.fachklassen.Zentrum#getZentrumBean()}.
+	 */
+	@Test
+	public void testGetZentrumBean() {
+		fail("Not yet implemented");
+	}
+
+}
