a3500d9ee72cf127ae63e908f770f4b0cd3ab3d0
tnoack <tnoack@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-02-06 20:21:31 +0000

    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@486 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/junittests/ZentrumTest.java
+++ src/de/randi2/junittests/ZentrumTest.java
@@ -9,13 +9,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.randi2.datenbank.DatenbankFactory;
+import de.randi2.model.exceptions.ZentrumException;
+import de.randi2.datenbank.exceptions.DatenbankFehlerException;
 import de.randi2.model.fachklassen.Zentrum;
 import de.randi2.model.fachklassen.beans.PersonBean;
 import de.randi2.model.fachklassen.beans.ZentrumBean;
 
 /**
- * @author Tino Noack [tino.noack@web.de]
- * @version $Id
+ * @author Tino Noack <tnoack@stud.hs-heilbronn.de>
+ * @version $Id$
  */
 public class ZentrumTest {
 	
@@ -41,21 +44,40 @@ public class ZentrumTest {
 	}
 
 	/**
-	 * Test method for {@link de.randi2.model.fachklassen.Zentrum#Zentrum(de.randi2.model.fachklassen.beans.ZentrumBean)}.
-	 */
-	@Test
-	public void testZentrum() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link de.randi2.model.fachklassen.Zentrum#suchenZentrum(de.randi2.model.fachklassen.beans.ZentrumBean)}.
 	 */
 	@Test
-	public void testSuchenZentrum() {
-		fail("Not yet implemented");
+	public void testSuchenZentrumKorrekt() {
+		
+		try{
+		assertEquals(Zentrum.suchenZentrum(testZB),
+				DatenbankFactory.getAktuelleDBInstanz().suchenObjekt(testZB));
+		}catch(Exception e){
+			e.printStackTrace();
+			fail("Sollte keine Exception werfen");
+		}
 	}
-
+	
+//	/**
+//	 * Test method for {@link de.randi2.model.fachklassen.Zentrum#suchenZentrum(de.randi2.model.fachklassen.beans.ZentrumBean)}.
+//	 */
+//	@Test (expected= IllegalArgumentException.class)
+//	public void testSuchenZentrumIllegalArgument() {
+//		
+//			Zentrum.suchenZentrum(); //hier illegal argument einfuegen
+//			
+//	}
+//	
+//	/**
+//	 * Test method for {@link de.randi2.model.fachklassen.Zentrum#suchenZentrum(de.randi2.model.fachklassen.beans.ZentrumBean)}.
+//	 */
+//	@Test (expected= DatenbankFehlerException.class)
+//	public void testSuchenZentrumDatenbankException() {
+//		
+//			Zentrum.suchenZentrum(); //hier datenbankexception provozieren
+//			
+//	}
+	
 	/**
 	 * Test method for {@link de.randi2.model.fachklassen.Zentrum#pruefenPasswort(java.lang.String)}.
 	 */
