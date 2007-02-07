758d156453666b68970c7786ad8309e576d250a2
lplotni <lplotni@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-02-07 20:23:57 +0000

    Auf Grun der Aenderungen in der Zentrum Klasse mussten diese Servlets angepasst werden. Entsprechende Kommentare sind in dem Code zu finden
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@533 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/controller/BenutzerServlet.java
+++ src/de/randi2/controller/BenutzerServlet.java
@@ -28,225 +28,244 @@ import de.randi2.model.exceptions.*;
  * @author Andreas Freudling <afreudling@stud.hs-heilbronn.de>
  * 
  */
-public class BenutzerServlet extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet
-{
+public class BenutzerServlet extends javax.servlet.http.HttpServlet implements
+		javax.servlet.Servlet {
 
-    /*
-         * (non-Java-doc)
-         * 
-         * @see javax.servlet.http.HttpServlet#HttpServlet()
-         */
-    public BenutzerServlet()
-    {
-	super();
-    }
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public BenutzerServlet() {
+		super();
+	}
 
-    /*
-         * (non-Java-doc)
-         * 
-         * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request,
-         *      HttpServletResponse response)
-         */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-	// TODO Bisher noch kein doGet gemacht, damit Nutzer keine Details sieht
-    }
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request,
+	 *      HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Bisher noch kein doGet gemacht, damit Nutzer keine Details sieht
+	}
 
-    /*
-         * (non-Java-doc)
-         * 
-         * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest
-         *      request, HttpServletResponse response)
-         */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-	String id = (String) request.getAttribute("anfrage_id");
-	Logger.getLogger(this.getClass()).debug(id);
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request,
+	 *      HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String id = (String) request.getAttribute("anfrage_id");
+		Logger.getLogger(this.getClass()).debug(id);
 
-	// Login
-	if (id.equals("CLASS_DISPATCHERSERVLET_LOGIN1"))
-	{
-	    BenutzerkontoBean sBenutzer = null;
-	    try
-	    {
+		// Login
+		if (id.equals("CLASS_DISPATCHERSERVLET_LOGIN1")) {
+			BenutzerkontoBean sBenutzer = null;
+			try {
 
-		sBenutzer = new BenutzerkontoBean();
-		// Filter setzen
-		sBenutzer.setFilter(true);
-		sBenutzer.setBenutzername((String) request.getParameter("username"));
-		// TODO Zukünftig sollte hier gleich das Passwort überprüft
-		// werden
-		sBenutzer.setPasswort(PasswortUtil.getInstance().hashPasswort((String) request.getParameter("password")));
-		Vector<BenutzerkontoBean> gBenutzer = null;
+				sBenutzer = new BenutzerkontoBean();
+				// Filter setzen
+				sBenutzer.setFilter(true);
+				sBenutzer.setBenutzername((String) request
+						.getParameter("username"));
+				// TODO Zukünftig sollte hier gleich das Passwort überprüft
+				// werden
+				sBenutzer.setPasswort(PasswortUtil.getInstance().hashPasswort(
+						(String) request.getParameter("password")));
+				Vector<BenutzerkontoBean> gBenutzer = null;
 
-		try
-		{
-		    gBenutzer = Benutzerkonto.suchenBenutzer(sBenutzer);
-		} catch (DatenbankFehlerException e)
-		{
-		    // Interner Fehler, wird nicht an Benutzer weitergegeben
-		    Logger.getLogger(this.getClass()).fatal("Fehler bei Benutzerfilterung", e);
-		}
-		if (gBenutzer.size() == 1)
-		{
-		    if (!gBenutzer.get(0).isGesperrt() && new Benutzerkonto(gBenutzer.firstElement()).pruefenPasswort((String) request.getParameter("password")))
-		    {
-			request.getSession(true).setAttribute("aBenutzer", gBenutzer.firstElement());
-			request.setAttribute("anfrage_id", "CLASS_BENUTZERSERVLET_LOGIN_OK");
-			LogAktion a = new LogAktion("Benutzer hat sich erfolgreich eingeloggt", gBenutzer.firstElement());
-			Logger.getLogger(LogLayout.LOGIN_LOGOUT).info(a);
-		    }// if
-		    else
-		    {
-			request.setAttribute("fehlernachricht", "Loginfehler:<br> Bitte Benutzername und Passwort &uuml;berpr&uuml;fen");
-			request.setAttribute("anfrage_id", "CLASS_BENUTZERSERVLET_LOGIN_ERROR");
-			LogAktion a = new LogAktion("Falsches Passwort eingegeben.", sBenutzer);
-			Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
-		    }
+				try {
+					gBenutzer = Benutzerkonto.suchenBenutzer(sBenutzer);
+				} catch (DatenbankFehlerException e) {
+					// Interner Fehler, wird nicht an Benutzer weitergegeben
+					Logger.getLogger(this.getClass()).fatal(
+							"Fehler bei Benutzerfilterung", e);
+				}
+				if (gBenutzer.size() == 1) {
+					if (!gBenutzer.get(0).isGesperrt()
+							&& new Benutzerkonto(gBenutzer.firstElement())
+									.pruefenPasswort((String) request
+											.getParameter("password"))) {
+						request.getSession(true).setAttribute("aBenutzer",
+								gBenutzer.firstElement());
+						request.setAttribute("anfrage_id",
+								"CLASS_BENUTZERSERVLET_LOGIN_OK");
+						LogAktion a = new LogAktion(
+								"Benutzer hat sich erfolgreich eingeloggt",
+								gBenutzer.firstElement());
+						Logger.getLogger(LogLayout.LOGIN_LOGOUT).info(a);
+					}// if
+					else {
+						request
+								.setAttribute("fehlernachricht",
+										"Loginfehler:<br> Bitte Benutzername und Passwort &uuml;berpr&uuml;fen");
+						request.setAttribute("anfrage_id",
+								"CLASS_BENUTZERSERVLET_LOGIN_ERROR");
+						LogAktion a = new LogAktion(
+								"Falsches Passwort eingegeben.", sBenutzer);
+						Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
+					}
+				}// if
+				else {
+					request
+							.setAttribute("fehlernachricht",
+									"Loginfehler:<br> Bitte Benutzername und Passwort &uuml;berpr&uuml;fen");
+					request.setAttribute("anfrage_id",
+							"CLASS_BENUTZERSERVLET_LOGIN_ERROR");
+					if (gBenutzer.size() == 0) {
+						LogAktion a = new LogAktion(
+								"Falscher Benutzernamen eingegeben.", sBenutzer);
+						Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
+					} else {
+						Logger.getLogger(this.getClass()).fatal(
+								"Mehr als einen Benutzer fuer '"
+										+ sBenutzer.getBenutzername()
+										+ "' in der Datenbank gefunden!");
+					}
+				}// else
+
+			} catch (BenutzerkontoException e) {
+
+				// Ungueltiger Benutzername/Passwort
+
+				request.setAttribute("fehlernachricht", "Loginfehler");
+				request.setAttribute("anfrage_id",
+						"CLASS_BENUTZERSERVLET_LOGIN_ERROR");
+				LogAktion a = new LogAktion(
+						"Ungueltige Benutzername/Passwort Kombination eingegeben.",
+						sBenutzer);
+				Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
+			}
+
+			request.getRequestDispatcher("DispatcherServlet").forward(request,
+					response);
 		}// if
-		else
-		{
-		    request.setAttribute("fehlernachricht", "Loginfehler:<br> Bitte Benutzername und Passwort &uuml;berpr&uuml;fen");
-		    request.setAttribute("anfrage_id", "CLASS_BENUTZERSERVLET_LOGIN_ERROR");
-		    if (gBenutzer.size() == 0)
-		    {
-			LogAktion a = new LogAktion("Falscher Benutzernamen eingegeben.", sBenutzer);
-			Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
-		    } else
-		    {
-			Logger.getLogger(this.getClass()).fatal("Mehr als einen Benutzer fuer '" + sBenutzer.getBenutzername() + "' in der Datenbank gefunden!");
-		    }
-		}// else
 
-	    } catch (BenutzerkontoException e)
-	    {
+		// Letzter Schritt Benutzer registrieren
+		if (id.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_VIER")) {
+			// Alle Attribute des request inititalisieren
+			String fehlernachricht = "";
+			String vorname = request.getParameter("Vorname");
+			String nachname = request.getParameter("Nachname");
+			char geschlecht = NullKonstanten.NULL_CHAR;
+			String passwort = null;
+			String email = request.getParameter("Email");
+			String telefon = request.getParameter("Telefon");
+			String fax = request.getParameter("Fax");
+			String handynummer = request.getParameter("Handy");
+			String institut = request.getParameter("Institut");
+			String zent = request.getParameter("aZentrum");
+			int zentrumID = Integer.parseInt(zent);
+			ZentrumBean zentrum = null;
+			String titel = request.getParameter("Titel");
 
-		// Ungueltiger Benutzername/Passwort
+			if (titel != null && titel.equals("kein Titel")) {
+				titel = null;
+			}
 
-		request.setAttribute("fehlernachricht", "Loginfehler");
-		request.setAttribute("anfrage_id", "CLASS_BENUTZERSERVLET_LOGIN_ERROR");
-		LogAktion a = new LogAktion("Ungueltige Benutzername/Passwort Kombination eingegeben.", sBenutzer);
-		Logger.getLogger(LogLayout.LOGIN_LOGOUT).warn(a);
-	    }
+			// TODO Dirty Fix: Da noch keine Suche nach Zentrumbeans möglich
+			// @Andy: man kann doch ein ZentrumBean einfach erzeugen, dazu
+			// brauchen
+			// wir keine konstante in der Zentrum Klasse. Oder steckt noch was
+			// dahinter? (Lukasz)
+			ZentrumBean sZentrum = new ZentrumBean();
+			sZentrum.setFilter(true);
+			Vector<ZentrumBean> gZentrum = null;
+			try {
+				gZentrum = Zentrum.suchenZentrum(sZentrum);
+			} catch (DatenbankFehlerException e2) {
+				// TODO Wieder muss an dieser Stelle die Ueberlegung gemacht werden, was für eine Nachricht dem Benutzer angezeigt werden soll.
+				e2.printStackTrace();
+			}
+			Iterator<ZentrumBean> itgZentrum = gZentrum.iterator();
+			while (itgZentrum.hasNext()) {
+				ZentrumBean aZentrumBean = itgZentrum.next();
+				if (aZentrumBean.getId() == zentrumID) {
+					zentrum = aZentrumBean;
+				}
+			}
+			// Ende Dirty Fix
 
-	    request.getRequestDispatcher("DispatcherServlet").forward(request, response);
-	}// if
+			// Geschlecht abfragen
+			if (request.getParameter("maennlich") != null) {
+				geschlecht = 'm';
+			} else if (request.getParameter("weiblich") != null) {
+				geschlecht = 'w';
+			}
+			// Wiederholte Passworteingabe prüfen
+			if (request.getParameter("Passwort") != null
+					&& request.getParameter("Passwort_wh") != null) {
+				if (request.getParameter("Passwort").equals(
+						request.getParameter("Passwort_wh"))) {
+					passwort = request.getParameter("Passwort");
+				} else {
+					fehlernachricht += "Passwort und wiederholtes Passwort sind nicht gleich";
+				}
+			}
+			// Benutzer anlegen
+			// Hier findet die Überprüfung der Daten auf Serverseite statt,
+			// Fehler wird an Benutzer weiter gegeben
+			PersonBean aPerson = null;
+			try {
+				aPerson = new PersonBean(nachname, vorname, titel, geschlecht,
+						email, telefon, handynummer, fax);
+				BenutzerkontoBean aBenutzerkonto;
+				aBenutzerkonto = new BenutzerkontoBean(email, passwort, aPerson);
+				aBenutzerkonto.setZentrum(zentrum);
+				Benutzerkonto.anlegenBenutzer(aBenutzerkonto);
 
-	// Letzter Schritt Benutzer registrieren
-	if (id.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_VIER"))
-	{
-	    //Alle Attribute des request inititalisieren
-	    String fehlernachricht = "";
-	    String vorname = request.getParameter("Vorname");
-	    String nachname = request.getParameter("Nachname");
-	    char geschlecht = NullKonstanten.NULL_CHAR;
-	    String passwort = null;
-	    String email = request.getParameter("Email");
-	    String telefon = request.getParameter("Telefon");
-	    String fax = request.getParameter("Fax");
-	    String handynummer = request.getParameter("Handy");
-	    String institut = request.getParameter("Institut");
-	    String zent = request.getParameter("aZentrum");
-	    int zentrumID = Integer.parseInt(zent);
-	    ZentrumBean zentrum = null;
-	    String titel = request.getParameter("Titel");
+				// Hier startet das Abfangen aller Fehlerprüfungen
+			} catch (DatenbankFehlerException e) {
+				Logger
+						.getLogger(this.getClass())
+						.warn(
+								"Nutzer konnte nicht in die Datenbank aufgenommen werden",
+								e);
+				fehlernachricht += e.getMessage() + "\n";
+			} catch (BenutzerkontoException e1) {
+				Logger.getLogger(this.getClass()).info(
+						"Fehler mit Passwort oder Email", e1);
+				fehlernachricht += e1.getMessage() + "\n";
+			} catch (PersonException e) {
+				Logger.getLogger(this.getClass()).info("Persondaten ungültig",
+						e);
+				fehlernachricht += e.getMessage() + "\n";
+				e.printStackTrace();
+			}
+			// Falls ein Fehler aufgetreten ist, request wieder auffüllen
+			if (fehlernachricht != "") {
 
-	    if (titel != null && titel.equals("kein Titel"))
-	    {
-		titel = null;
-	    }
+				request.setAttribute("Vorname", vorname);
+				request.setAttribute("Nachname", nachname);
+				if (geschlecht == 'm') {
+					request.setAttribute("maennlich", "maennlich");
+				} else if (geschlecht == 'w') {
+					request.setAttribute("weiblich", "weiblich");
+				}
+				request.setAttribute("Titel", titel);
+				request.setAttribute("Passwort", request
+						.getParameter("Passwort"));
+				request.setAttribute("Passwort_wh", request
+						.getParameter("Passwort_wh"));
+				request.setAttribute("Email", email);
+				request.setAttribute("Telefon", telefon);
+				request.setAttribute("Fax", fax);
+				request.setAttribute("aZentrum", zentrum.getId());
+				request.setAttribute("Handy", handynummer);
+				request.setAttribute("Institut", institut);
 
-	    // TODO Dirty Fix: Da noch keine Suche nach Zentrumbeans möglich
-	    ZentrumBean sZentrum = Zentrum.NULL_ZENTRUM;
-	    sZentrum.setFilter(true);
-	    Vector<ZentrumBean> gZentrum = Zentrum.suchenZentrum(sZentrum);
-	    Iterator<ZentrumBean> itgZentrum = gZentrum.iterator();
-	    while (itgZentrum.hasNext())
-	    {
-		ZentrumBean aZentrumBean = itgZentrum.next();
-		if (aZentrumBean.getId() == zentrumID)
-		{
-		    zentrum = aZentrumBean;
-		}
-	    }
-	    // Ende Dirty Fix
-
-	    // Geschlecht abfragen
-	    if (request.getParameter("maennlich") != null)
-	    {
-		geschlecht = 'm';
-	    } else if (request.getParameter("weiblich") != null)
-	    {
-		geschlecht = 'w';
-	    }
-	    // Wiederholte Passworteingabe prüfen
-	    if (request.getParameter("Passwort") != null && request.getParameter("Passwort_wh") != null)
-	    {
-		if (request.getParameter("Passwort").equals(request.getParameter("Passwort_wh")))
-		{
-		    passwort = request.getParameter("Passwort");
-		} else
-		{
-		    fehlernachricht += "Passwort und wiederholtes Passwort sind nicht gleich";
-		}
-	    }
-	    // Benutzer anlegen
-	    // Hier findet die Überprüfung der Daten auf Serverseite statt,
-	    // Fehler wird an Benutzer weiter gegeben
-	    PersonBean aPerson = null;
-	    try
-	    {
-		aPerson = new PersonBean(nachname, vorname, titel, geschlecht, email, telefon, handynummer, fax);
-		BenutzerkontoBean aBenutzerkonto;
-		aBenutzerkonto = new BenutzerkontoBean(email, passwort, aPerson);
-		aBenutzerkonto.setZentrum(zentrum);
-		Benutzerkonto.anlegenBenutzer(aBenutzerkonto);
-
-		// Hier startet das Abfangen aller Fehlerprüfungen
-	    } catch (DatenbankFehlerException e)
-	    {
-		Logger.getLogger(this.getClass()).warn("Nutzer konnte nicht in die Datenbank aufgenommen werden", e);
-		fehlernachricht += e.getMessage() + "\n";
-	    } catch (BenutzerkontoException e1)
-	    {
-		Logger.getLogger(this.getClass()).info("Fehler mit Passwort oder Email", e1);
-		fehlernachricht += e1.getMessage() + "\n";
-	    } catch (PersonException e)
-	    {
-		Logger.getLogger(this.getClass()).info("Persondaten ungültig", e);
-		fehlernachricht += e.getMessage() + "\n";
-		e.printStackTrace();
-	    }
-	    // Falls ein Fehler aufgetreten ist, request wieder auffüllen
-	    if (fehlernachricht != "")
-	    {
-
-		request.setAttribute("Vorname", vorname);
-		request.setAttribute("Nachname", nachname);
-		if (geschlecht == 'm')
-		{
-		    request.setAttribute("maennlich", "maennlich");
-		} else if (geschlecht == 'w')
-		{
-		    request.setAttribute("weiblich", "weiblich");
-		}
-		request.setAttribute("Titel", titel);
-		request.setAttribute("Passwort", request.getParameter("Passwort"));
-		request.setAttribute("Passwort_wh", request.getParameter("Passwort_wh"));
-		request.setAttribute("Email", email);
-		request.setAttribute("Telefon", telefon);
-		request.setAttribute("Fax", fax);
-		request.setAttribute("aZentrum", zentrum.getId());
-		request.setAttribute("Handy", handynummer);
-		request.setAttribute("Institut", institut);
-
-		// Ausgeben der Fehlermeldung
-		request.setAttribute("fehlernachricht", fehlernachricht);
-		request.getRequestDispatcher("/benutzer_anlegen_drei.jsp").forward(request, response);
-	    } else
-	    {
-		request.getRequestDispatcher("/benutzer_anlegen_vier.jsp").forward(request, response);
-	    }//else
-	}// if letzter Schritt Benutzer registrieren
-    }// doPost
+				// Ausgeben der Fehlermeldung
+				request.setAttribute("fehlernachricht", fehlernachricht);
+				request.getRequestDispatcher("/benutzer_anlegen_drei.jsp")
+						.forward(request, response);
+			} else {
+				request.getRequestDispatcher("/benutzer_anlegen_vier.jsp")
+						.forward(request, response);
+			}// else
+		}// if letzter Schritt Benutzer registrieren
+	}// doPost
 }
--- src/de/randi2/controller/ZentrumServlet.java
+++ src/de/randi2/controller/ZentrumServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import de.randi2.datenbank.exceptions.DatenbankFehlerException;
 import de.randi2.model.exceptions.ZentrumException;
 import de.randi2.model.fachklassen.*;
 import de.randi2.model.fachklassen.beans.*;
@@ -20,124 +21,171 @@ import org.apache.log4j.Logger;
  * @author Andreas Freudling <afreudling@hs-heilbronn.de>
  * 
  */
- public class ZentrumServlet extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
-    /* (non-Java-doc)
+public class ZentrumServlet extends javax.servlet.http.HttpServlet implements
+		javax.servlet.Servlet {
+	/*
+	 * (non-Java-doc)
+	 * 
 	 * @see javax.servlet.http.HttpServlet#HttpServlet()
 	 */
 	public ZentrumServlet() {
 		super();
-	}   	
-	
-	/* (non-Java-doc)
-	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	}
+
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request,
+	 *      HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//	  TODO Bisher noch kein doGet gemacht, damit Nutzer keine Details sieht
-	}  	
-	
-	/* (non-Java-doc)
-	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Bisher noch kein doGet gemacht, damit Nutzer keine Details sieht
+	}
+
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request,
+	 *      HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id=(String)request.getParameter("anfrage_id");
-		String idAttribute=(String)request.getAttribute("anfrage_id");
-		if(idAttribute!=null)
-		{
-			id=idAttribute;
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String id = (String) request.getParameter("anfrage_id");
+		String idAttribute = (String) request.getAttribute("anfrage_id");
+		if (idAttribute != null) {
+			id = idAttribute;
 		}
 		Logger.getLogger(this.getClass()).debug(id);
-		
-//		 Benutzer registrieren
+
+		// Benutzer registrieren
 		// Schritt 2.1
 		if (id.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_ZWEI")) {
-			//Nach allen vorhandenen Zentren suchen
-			ZentrumBean sZentrum=Zentrum.NULL_ZENTRUM;
+			// Nach allen vorhandenen Zentren suchen
+			// @Andy - wieder die gleiche Geschichte, mit der Konstante (siehe
+			// mein Kommentar in dem BenutzerServlet Zeile: 171)
+			ZentrumBean sZentrum = new ZentrumBean();
 			sZentrum.setFilter(true);
-			Vector<ZentrumBean> gZentrum=Zentrum.suchenZentrum(Zentrum.NULL_ZENTRUM);
-			request.setAttribute("listeZentren",gZentrum);			
-			//Schritt 2.1.3
-			request.getRequestDispatcher("/benutzer_anlegen_zwei.jsp").forward(request,response);
+			Vector<ZentrumBean> gZentrum = null;
+			try {
+				gZentrum = Zentrum.suchenZentrum(sZentrum);
+			} catch (DatenbankFehlerException e) {
+				// TODO Wieder muss an dieser Stelle die Ueberlegung gemacht
+				// werden, was für eine Nachricht dem Benutzer angezeigt werden
+				// soll.
+				e.printStackTrace();
+			}
+			request.setAttribute("listeZentren", gZentrum);
+			// Schritt 2.1.3
+			request.getRequestDispatcher("/benutzer_anlegen_zwei.jsp").forward(
+					request, response);
 		}
-		//Schritt 3.1: ZENTRUMAUSWAHL: Filterung
-//		Schritt 3.2 ZENTRUMAUSWAHL->BENUTZERDATEN_EINGEBEN
-		else if (id.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_DREI"))
-		{
-			//Filterung
-			if(((String)request.getParameter("Filtern"))!=null)
-			{
-				Vector<ZentrumBean> gZentrum=null;
-				if(((String)request.getParameter("name_institution"))!=""&&((String)request.getParameter("name_abteilung"))!="")
-				{
-					ZentrumBean sZentrum= new ZentrumBean();
-					
-					//Filter setzen
-						sZentrum.setFilter(true);
-					try {
-						sZentrum.setInstitution(request.getParameter("name_institution"));
-						sZentrum.setAbteilung(request.getParameter("name_abteilung"));
-					} catch (ZentrumException e) {
-						//Interner Fehler, wird nicht an Benutzer weitergegeben
-						Logger.getLogger(this.getClass()).fatal("Fehler bei Zentrumsfilterung",e);
-					}
-					gZentrum=Zentrum.suchenZentrum(sZentrum);
-				}
-				else
-				{
-					ZentrumBean sZentrum=Zentrum.NULL_ZENTRUM;
+		// Schritt 3.1: ZENTRUMAUSWAHL: Filterung
+		// Schritt 3.2 ZENTRUMAUSWAHL->BENUTZERDATEN_EINGEBEN
+		else if (id
+				.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_DREI")) {
+			// Filterung
+			if (((String) request.getParameter("Filtern")) != null) {
+				Vector<ZentrumBean> gZentrum = null;
+				if (((String) request.getParameter("name_institution")) != ""
+						&& ((String) request.getParameter("name_abteilung")) != "") {
+					ZentrumBean sZentrum = new ZentrumBean();
+
+					// Filter setzen
 					sZentrum.setFilter(true);
-					gZentrum=Zentrum.suchenZentrum(sZentrum);
+					try {
+						sZentrum.setInstitution(request
+								.getParameter("name_institution"));
+						sZentrum.setAbteilung(request
+								.getParameter("name_abteilung"));
+					} catch (ZentrumException e) {
+						// Interner Fehler, wird nicht an Benutzer weitergegeben
+						Logger.getLogger(this.getClass()).fatal(
+								"Fehler bei Zentrumsfilterung", e);
+					}
+					try {
+						gZentrum = Zentrum.suchenZentrum(sZentrum);
+					} catch (DatenbankFehlerException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} else {
+					ZentrumBean sZentrum = new ZentrumBean();
+					sZentrum.setFilter(true);
+					try {
+						gZentrum = Zentrum.suchenZentrum(sZentrum);
+					} catch (DatenbankFehlerException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 
-			
-			request.setAttribute("listeZentren",gZentrum);
-			request.getRequestDispatcher("/benutzer_anlegen_zwei.jsp").forward(request, response);
-			}
-			else
-			{
-				Vector<ZentrumBean> gZentrum=Zentrum.suchenZentrum(Zentrum.NULL_ZENTRUM);
-			Iterator<ZentrumBean> itgZentrum=gZentrum.iterator();
-			while(itgZentrum.hasNext())
-			{
-				ZentrumBean aZentrumBean=itgZentrum.next();
-				String suche="bestaetigen"+aZentrumBean.getId();
-				if(request.getParameter(suche)!=null)
-				{
-					Zentrum aZentrum=new Zentrum(aZentrumBean);
-					//Zentrum Passwort richtig
-					if(aZentrum.pruefenPasswort(request.getParameter("zentrum_passwort"+aZentrumBean.getId())))
-					{
-						//ID des Zentrum reinschreiben!
-						request.setAttribute("aZentrum", aZentrum.getZentrumBean().getId());
-						request.getRequestDispatcher("/benutzer_anlegen_drei.jsp").forward(request, response);
-					}
-					//Zentrum Passwort falsch
-					else
-					{
-						request.setAttribute("listeZentren",gZentrum);
-						request.setAttribute("fehlernachricht", "Falsches Zentrumpasswort");
-						request.getRequestDispatcher("/benutzer_anlegen_zwei.jsp").forward(request, response);
+				request.setAttribute("listeZentren", gZentrum);
+				request.getRequestDispatcher("/benutzer_anlegen_zwei.jsp")
+						.forward(request, response);
+			} else {
+				ZentrumBean sZentrum = new ZentrumBean();
+				sZentrum.setFilter(true);
+				Vector<ZentrumBean> gZentrum = null;
+				try {
+					gZentrum = Zentrum
+							.suchenZentrum(sZentrum);
+				} catch (DatenbankFehlerException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Iterator<ZentrumBean> itgZentrum = gZentrum.iterator();
+				while (itgZentrum.hasNext()) {
+					ZentrumBean aZentrumBean = itgZentrum.next();
+					String suche = "bestaetigen" + aZentrumBean.getId();
+					if (request.getParameter(suche) != null) {
+						Zentrum aZentrum = new Zentrum(aZentrumBean);
+						// Zentrum Passwort richtig
+						if (aZentrum.pruefenPasswort(request
+								.getParameter("zentrum_passwort"
+										+ aZentrumBean.getId()))) {
+							// ID des Zentrum reinschreiben!
+							request.setAttribute("aZentrum", aZentrum
+									.getZentrumBean().getId());
+							request.getRequestDispatcher(
+									"/benutzer_anlegen_drei.jsp").forward(
+									request, response);
+						}
+						// Zentrum Passwort falsch
+						else {
+							request.setAttribute("listeZentren", gZentrum);
+							request.setAttribute("fehlernachricht",
+									"Falsches Zentrumpasswort");
+							request.getRequestDispatcher(
+									"/benutzer_anlegen_zwei.jsp").forward(
+									request, response);
+						}
 					}
 				}
 			}
-			}
-			
-			//keine Filterung
-			
-		
+
+			// keine Filterung
+
 		}
 		// Schritt 2:
-		else if (id.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_DREI")) {
-			request.setAttribute("anfrage_id","CLASS_BENUTZERSERVLET_BENUTZER_REGISTRIEREN_DREI");
+		else if (id
+				.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_DREI")) {
+			request.setAttribute("anfrage_id",
+					"CLASS_BENUTZERSERVLET_BENUTZER_REGISTRIEREN_DREI");
 			// Hier noch jede Menge Logik
-			request.getRequestDispatcher("DispatcherServlet").forward(request,response);
+			request.getRequestDispatcher("DispatcherServlet").forward(request,
+					response);
 
 		}
 		// Schritt 3:
-		else if (id.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_VIER")) {
-			request.setAttribute("anfrage_id","CLASS_BENUTZERSERVLET_BENUTZER_REGISTRIEREN_VIER");
+		else if (id
+				.equals("CLASS_DISPATCHERSERVLET_BENUTZER_REGISTRIEREN_VIER")) {
+			request.setAttribute("anfrage_id",
+					"CLASS_BENUTZERSERVLET_BENUTZER_REGISTRIEREN_VIER");
 			// Hier noch jede Menge Logik
-			request.getRequestDispatcher("DispatcherServlet").forward(request,response);
+			request.getRequestDispatcher("DispatcherServlet").forward(request,
+					response);
 
 		}
-	}   	  	    
+	}
 }
