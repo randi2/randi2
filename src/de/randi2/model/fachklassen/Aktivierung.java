8b792a897ae61dabecbbccb93d800592ae1d30ec
afreudli <afreudli@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-04-23 17:44:00 +0000

    Aktivierung eingesetzt
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@881 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/model/fachklassen/Aktivierung.java
@@ -0,0 +1,14 @@
+package de.randi2.model.fachklassen;
+
+import de.randi2.model.fachklassen.beans.AktivierungBean;
+
+public class Aktivierung {
+
+    
+    
+    public static boolean aktivierung(AktivierungBean konto)
+    {
+	return false;
+    }
+
+}
---
+++ src/de/randi2/model/fachklassen/beans/AktivierungBean.java
@@ -0,0 +1,90 @@
+package de.randi2.model.fachklassen.beans;
+
+import java.util.GregorianCalendar;
+
+import de.randi2.utility.NullKonstanten;
+
+public class AktivierungBean {
+    private long aktivierungsId=NullKonstanten.NULL_LONG;
+    private GregorianCalendar versanddatum=null;
+    private BenutzerkontoBean benutzerkonto=null;
+    private long benutzerkontoID=NullKonstanten.NULL_LONG;
+    private String aktivierungsLink=null;
+    /**
+     * @return the aktivierungsId
+     */
+    public long getAktivierungsId() {
+        return aktivierungsId;
+    }
+    /**
+     * @param aktivierungsId the aktivierungsId to set
+     */
+    public void setAktivierungsId(long aktivierungsId) {
+        this.aktivierungsId = aktivierungsId;
+    }
+    /**
+     * @return the aktivierungsLink
+     */
+    public String getAktivierungsLink() {
+        return aktivierungsLink;
+    }
+    /**
+     * @param aktivierungsLink the aktivierungsLink to set
+     */
+    public void setAktivierungsLink(String aktivierungsLink) {
+        this.aktivierungsLink = aktivierungsLink;
+    }
+    /**
+     * @return the benutzerkonto
+     */
+    public BenutzerkontoBean getBenutzerkonto() {
+        return benutzerkonto;
+    }
+    /**
+     * @param benutzerkonto the benutzerkonto to set
+     */
+    public void setBenutzerkonto(BenutzerkontoBean benutzerkonto) {
+        this.benutzerkonto = benutzerkonto;
+    }
+    /**
+     * @return the benutzerkontoID
+     */
+    public long getBenutzerkontoID() {
+        return benutzerkontoID;
+    }
+    /**
+     * @param benutzerkontoID the benutzerkontoID to set
+     */
+    public void setBenutzerkontoID(long benutzerkontoID) {
+        this.benutzerkontoID = benutzerkontoID;
+    }
+    /**
+     * @return the versanddatum
+     */
+    public GregorianCalendar getVersanddatum() {
+        return versanddatum;
+    }
+    /**
+     * @param versanddatum the versanddatum to set
+     */
+    public void setVersanddatum(GregorianCalendar versanddatum) {
+        this.versanddatum = versanddatum;
+    }
+    /**
+     * @param aktivierungsId
+     * @param versanddatum
+     * @param benutzerkonto
+     * @param benutzerkontoID
+     * @param aktivierungsLink
+     */
+    public AktivierungBean(long aktivierungsId, GregorianCalendar versanddatum, BenutzerkontoBean benutzerkonto, long benutzerkontoID, String aktivierungsLink) {
+	super();
+	this.aktivierungsId = aktivierungsId;
+	this.versanddatum = versanddatum;
+	this.benutzerkonto = benutzerkonto;
+	this.benutzerkontoID = benutzerkontoID;
+	this.aktivierungsLink = aktivierungsLink;
+    }
+    
+
+}
