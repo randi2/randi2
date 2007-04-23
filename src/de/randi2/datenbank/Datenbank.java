e616d1f2dc689e70f2e58b08faf24cf76a8ce1f1
freifsch <freifsch@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-04-23 15:09:51 +0000

    Methoden zum verbindungsaufbau hinzugefuegt
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@860 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/datenbank/Datenbank.java
+++ src/de/randi2/datenbank/Datenbank.java
@@ -1,16 +1,33 @@
 package de.randi2.datenbank;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Vector;
-
+import de.randi2.utility.Config;
 import de.randi2.datenbank.exceptions.DatenbankFehlerException;
 
 /**
  * <p>Datenbankklasse</p>
  * @version $Id$
  * @author Frederik Reifschneider [Reifschneider@stud.uni-heidelberg.de]
- *
  */
 public class Datenbank implements DatenbankSchnittstelle{
+	
+	/**
+	 * Konstruktor der Datenbankklasse.
+	 * Laedt den SQL Treiber
+	 */
+	public Datenbank() {
+		try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (InstantiationException e) {
+        	System.out.println(e.getLocalizedMessage());
+        } catch (IllegalAccessException e) {
+        	System.out.println(e.getLocalizedMessage());
+        } catch (ClassNotFoundException e) {
+        	System.out.println(e.getLocalizedMessage());
+        }
+	}
 
 	public <T extends Filter> void loeschenObjekt(T zuLoeschendesObjekt) throws DatenbankFehlerException {
 		// TODO Auto-generated method stub
@@ -36,5 +53,27 @@ public class Datenbank implements DatenbankSchnittstelle{
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	/**
+     * Baut Verbindung zur Datenbank auf
+     * @return
+     * 			Connectionobjekt welches Zugriff auf die Datenbankermoeglicht.
+     * @throws SQLException
+     */
+    private Connection getConnection() throws SQLException {
+    	Connection con = DriverManager.getConnection("jdbc:mysql://"+Config.getProperty(Config.Felder.RELEASE_DB_HOST)+":"+Config.getProperty(Config.Felder.RELEASE_DB_PORT)+"/neuroguard",Config.getProperty(Config.Felder.RELEASE_DB_NUTZERNAME),Config.getProperty(Config.Felder.RELEASE_DB_PASSWORT));    	
+    	return con;
+    }
+    
+    /**
+     * Trennt Verbindung zur Datenbank.
+     * @throws SQLException 
+     * @throws DBExceptions
+     */
+    private void closeConnection(Connection con) throws SQLException {
+        	if(con!=null && !con.isClosed()) {
+        		con.close();
+        	}            
+    }	
 
 }
