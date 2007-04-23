b480b38e5a1d2a68dd1e03e3d8ee78e475d00249
lplotni <lplotni@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-04-23 13:04:35 +0000

    Studienarm Klasse wurde angelegt .
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@846 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/model/fachklassen/Studienarm.java
@@ -0,0 +1,55 @@
+package de.randi2.model.fachklassen;
+
+import java.util.Vector;
+
+import de.randi2.datenbank.DatenbankFactory;
+import de.randi2.datenbank.exceptions.DatenbankFehlerException;
+import de.randi2.model.fachklassen.beans.StudienarmBean;
+
+/**
+ * <p>
+ * Studienarm Fachklasse. Diese Klasse stellt nur zwei statische Methoden (get
+ * und suchen) zur Verfügung.
+ * </p>
+ * 
+ * @author Daniel Haehn [dhaehn@stud.hs-heilbronn.de]
+ * @author Lukasz Plotnicki [lplotni@stud.hs-heilbronn.de]
+ * 
+ * @version $Id$
+ * 
+ */
+public final class Studienarm {
+
+	/**
+	 * Diese Methode liefert nur das gewuenschte Objekt zurueckt.
+	 * 
+	 * @param id
+	 *            ID des gesuchten Studiearms.
+	 * @return StudienarmBean - das gewuenschte Objekt.
+	 * @throws DatenbankFehlerException
+	 *             falls Fehler bei dem Vorgang auftraten.
+	 */
+	public static StudienarmBean get(int id) throws DatenbankFehlerException {
+		return DatenbankFactory.getAktuelleDBInstanz().suchenObjektID(id,
+				new StudienarmBean());
+
+	}
+
+	/**
+	 * Diese Methode liefert die gesuchten StudienarmeBeans, falls es Objekte
+	 * mit gesuchten Eigenschaften gibt.
+	 * 
+	 * @param gesuchtesBean -
+	 *            ein StudienarmBean, in dem die gesuchten Eigenschaften gesetzt
+	 *            sind und alle andere NULL Werte enthalten.
+	 * @return ein Vector mit gefundenen StudienarmBeans.
+	 * @throws DatenbankFehlerException
+	 *             falls Fehler bei dem Vorgang auftraten.
+	 */
+	public static Vector<StudienarmBean> suchen(StudienarmBean gesuchtesBean)
+			throws DatenbankFehlerException {
+		return DatenbankFactory.getAktuelleDBInstanz().suchenObjekt(
+				gesuchtesBean);
+	}
+
+}
