a2be4d81e838fb0714255c8a83d97d73a9b895b7
freifsch <freifsch@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-04-23 14:28:42 +0000

    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@855 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/datenbank/Datenbank.java
+++ src/de/randi2/datenbank/Datenbank.java
@@ -1,5 +1,40 @@
 package de.randi2.datenbank;
 
-public class Datenbank {
+import java.util.Vector;
+
+import de.randi2.datenbank.exceptions.DatenbankFehlerException;
+
+/**
+ * Datenbankklasse
+ * @version $Id$
+ * @author Frederik Reifschneider [Reifschneider@stud.uni-heidelberg.de]
+ *
+ */
+public class Datenbank implements DatenbankSchnittstelle{
+
+	public <T extends Filter> void loeschenObjekt(T zuLoeschendesObjekt) throws DatenbankFehlerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public <T extends Filter> T schreibenObjekt(T zuSchreibendesObjekt) throws DatenbankFehlerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public <T extends Filter> Vector<T> suchenObjekt(T zuSuchendesObjekt) throws DatenbankFehlerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public <T extends Filter> T suchenObjektID(long id, T nullObjekt) throws DatenbankFehlerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public <T extends Filter> T suchenObjektKomplett(long id, T nullObjekt) throws DatenbankFehlerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
