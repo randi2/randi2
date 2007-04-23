086347ce8dc329117c935034bb9369b5ac6e7644
lplotni <lplotni@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-04-23 13:09:41 +0000

    siehe Kommentar zum PatientBean
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@850 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/model/fachklassen/beans/StudienarmBean.java
+++ src/de/randi2/model/fachklassen/beans/StudienarmBean.java
@@ -3,12 +3,126 @@
  */
 package de.randi2.model.fachklassen.beans;
 
+import java.util.Vector;
+
+import de.randi2.datenbank.Filter;
+import de.randi2.utility.NullKonstanten;
+
 /**
+ * TODO ProxyPattern noch nich angewandt!
+ * 
  * @author hype
  *
  */
-public class StudienarmBean {
+public class StudienarmBean extends Filter {
+	
+	private int id = NullKonstanten.NULL_INT;
 
+	private int aAktiv = NullKonstanten.NULL_INT;
+	
+	private String aBezeichnung = null;
+	
+	private String aBeschreibung = null;
+	
+	private StudieBean aStudie = null;
+	
+	private Vector<PatientBean> aPatienten = null;
+	
+	private Vector<Integer> aPatientenIDs = null;
+
+	public StudienarmBean() {
+		super();
+		// TODO Auto-generated constructor stub
+	}	
+	
+	public StudienarmBean(int id, StudieBean studie, int aktiv, String bezeichnung, String beschreibung, Vector<PatientBean> patienten) {
+		
+		super();
+		this.setId(id);
+		this.setStudie(studie);
+		this.setAktiv(aktiv);
+		this.setBezeichnung(bezeichnung);
+		this.setBeschreibung(beschreibung);
+		this.setPatienten(patienten);
+		
+		// IDs zu den Patienten holen und speichern
+		for (int i=0; i<this.getPatienten().size();i++) {
+			
+			Vector<Integer> aPatientenIDs = new Vector<Integer>();
+			
+			aPatientenIDs.add(this.getPatienten().elementAt(i).getId());
+			
+		}
+		
+	}
+	
+
+	
+
+	public int getAktiv() {
+		return aAktiv;
+	}
+
+	public void setAktiv(int aktiv) {
+		aAktiv = aktiv;
+	}
+
+	public String getBeschreibung() {
+		return aBeschreibung;
+	}
+
+	public void setBeschreibung(String beschreibung) {
+		aBeschreibung = beschreibung;
+	}
+
+	public String getBezeichnung() {
+		return aBezeichnung;
+	}
+
+	public void setBezeichnung(String bezeichnung) {
+		aBezeichnung = bezeichnung;
+	}
+
+	public StudieBean getStudie() {
+		return aStudie;
+	}
+
+	public void setStudie(StudieBean studie) {
+		aStudie = studie;
+	}
+	
+	
+	
+	public Vector<PatientBean> getPatienten() {
+		return aPatienten;
+	}
+
+	public void setPatienten(Vector<PatientBean> patienten) {
+		aPatienten = patienten;
+	}
+
+	public Vector getPatientenIDs() {
+		return aPatientenIDs;
+	}
+
+	public void setPatientenIDs(Vector patientenIDs) {
+		aPatientenIDs = patientenIDs;
+	}
+
+	public int getPatAnzahl() {
+		// TODO
+		return NullKonstanten.NULL_INT;
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	
 	
 }
