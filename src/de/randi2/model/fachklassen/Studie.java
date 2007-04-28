4f835a56d6f0da75cfe4ca954a15a77a2c1b18e8
sufriedr <sufriedr@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-04-28 09:24:44 +0000

    getStudienprotokoll eingefuegt
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@929 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/model/fachklassen/Studie.java
+++ src/de/randi2/model/fachklassen/Studie.java
@@ -1,8 +1,10 @@
 package de.randi2.model.fachklassen;
+
 import de.randi2.model.fachklassen.beans.RandomisationBean;
 import de.randi2.model.fachklassen.beans.StatistikBean;
 import de.randi2.model.fachklassen.beans.StudieBean;
 import de.randi2.model.fachklassen.beans.ZentrumBean;
+
 /**
  * @author Susanne Friedrich [sufriedr@stud.hs-heilbronn.de]
  * @author Nadine Zwink [nzwink@stud.hs-heilbronn.de]
@@ -10,12 +12,25 @@ import de.randi2.model.fachklassen.beans.ZentrumBean;
  */
 public class Studie {
 
+	/**
+	 * Das zugehörige StudieBean-Objekt.
+	 */
 	private StudieBean aStudieBean = null;
 
+	/**
+	 * Das zugehörige RandomistationBean-Objekt.
+	 */
 	private RandomisationBean aRandomisationBean = null;
-
+	/**
+	 * Das zugehörige ZentrumBean-Objekt.
+	 */
 	private ZentrumBean aZentrum;
 
+	/**
+	 * Die Methode zeigt eine Statistik nach bestimmten Kriterien an.
+	 * @param kriterium Kriterien zum erstellen der Statistik.
+	 * @return StatistikBean, das aktuelle StudieBean.
+	 */
 	public StatistikBean anzeigenStatistik(int kriterium) {
 
 		return null;
@@ -24,28 +39,42 @@ public class Studie {
 
 	// TODO zu klaeren, wie sollen die einzelnen Patienten repraesentiert werden
 
-	// private PatientBean randomisierePatient(PatientBean patient) {
+	// privat PatientBean randomisierePatient(PatientBean patient) {
 
 	// return null;
 
 	// }
 	// Url zurückgeben studienprotokoll
-	
-	//getStudienarme Vector
+
+	// getStudienarme Vector
 
 	// public PatientBean hinzufuegenPatient(PatientBean patient) {
 
 	// return null;
 
 	// }
-
+	
+	//TODO (Speicherung?)
+	/**
+	 * Liefert die Url des Studienprotokolls zurück.
+	 */
+	public String getStudienprotokoll(){
+		return null;
+		
+		
+		
+	}
+/**
+ * Diese Methode weist ein Zentrum einer Studie hinzu.
+ * 
+ * @param aZentrum
+ */
 	public void zuweisenZentrum(ZentrumBean aZentrum) {
 		this.aZentrum = aZentrum;
-		
-		
 
 	}
-	//TODO Array?
+
+	// TODO Array
 	public void konfiguriereRandomisation() {
 	}
 
