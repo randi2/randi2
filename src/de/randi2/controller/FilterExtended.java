c7da260ade59df3774065ef78b643ead111ff579
afreudli <afreudli@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-05-02 10:46:30 +0000

    Trimmen eingefuegt. Kommentare folgen ;)
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@964 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/controller/FilterExtended.java
@@ -0,0 +1,32 @@
+package de.randi2.controller;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+public class FilterExtended implements Filter {
+  private FilterConfig filterConfig;
+
+  public void init(FilterConfig filterConfig) throws
+    ServletException {
+    Logger.getLogger(this.getClass()).info("Filter gestartet");
+    this.filterConfig = filterConfig;
+  }
+
+  public void destroy() {
+    System.out.println("Filter destroyed");
+    this.filterConfig = null;
+  }
+
+  public void doFilter(ServletRequest request, ServletResponse response,
+    FilterChain chain)
+    throws IOException, ServletException {
+    chain.doFilter( new HttpServletRequestExtended((HttpServletRequest) request), response);
+  }
+}
---
+++ src/de/randi2/controller/HttpServletRequestExtended.java
@@ -0,0 +1,40 @@
+package de.randi2.controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.log4j.Logger;
+
+public final class HttpServletRequestExtended extends
+  HttpServletRequestWrapper {
+  public HttpServletRequestExtended(HttpServletRequest servletRequest) {
+    super(servletRequest);
+  }
+
+  public String[] getParameterValues(String parameter) {
+    String[] results = super.getParameterValues(parameter);
+    if (results==null)
+      return null;
+    int count = results.length;
+
+    String[] trimResults = new String[count];
+    for (int i=0; i<count; i++) {
+      trimResults[i] = results[i].trim();
+    }
+    return trimResults;
+  }
+  @Override
+  public String getParameter(String param){
+      Logger.getLogger(this.getClass()).info("Get Parameter wurde benutzt");
+      String rueckgabewert=super.getParameter(param);
+      if(rueckgabewert==null)
+      {
+	  return null;
+      }
+      rueckgabewert.trim();
+      if(rueckgabewert.equals(""))
+      {
+	  rueckgabewert=null;
+      }
+      return  rueckgabewert;
+  }
+}
