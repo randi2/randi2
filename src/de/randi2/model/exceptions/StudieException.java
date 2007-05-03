3d8e0eff8ee8b9ed84617fe6812915e2f36e1b0e
sufriedr <sufriedr@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-05-03 20:31:01 +0000

    Fehler bereinigt
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@977 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/model/exceptions/StudieException.java
+++ src/de/randi2/model/exceptions/StudieException.java
@@ -1,21 +1,41 @@
-package de.randi2.model.exceptions;
+ package de.randi2.model.exceptions;
 /**
- * 
+ * Diese Klasse beinhaltet die Fehler, die innerhalb der Klasse Studie auftreten koennen.
  * @author Susanne Friedrich [sufriedr@stud.hs-heilbronn.de]
  * @author Nadine Zwink [nzwink@stud.hs-heilbronn.de]
  * @version $Id$
  *
  */
 public class StudieException extends Exception {
-	//TODO Kommentare
+	
 	/**
 	 * Diese Konstante wird uebergeben, wenn das Datum nicht in der Zukunft liegt.
 	 */
 	public static final String DATUM_FEHLER="Datum liegt nicht in der Zukunft.";
+	/**
+	 * Diese Konstante wird uebergeben, wenn der Studienname nicht eingegeben wurde.
+	 */
 	public static final String STUDIENNAME_FEHLT = "Bitte Studienname eingeben.";
-	public static final String STUDIENNAME_UNGUELTIG = "Studienname ungültig";
+	/**
+	 * Diese Konstante wird uebergeben, wenn der Studienname zu kurz oder zu lang ist.
+	 */
+	public static final String STUDIENNAME_UNGUELTIG = "Studienname zu kurz oder zu lang";
+	/**
+	 * Diese Konstante wird uebergeben,wenn die Studienarme nicht eingetragen wurden.
+	 */
 	public static final String STUDIENARM_FEHLT = "Studiennarme nicht eingetragen.";
+	/**
+	 * Diese Konstante wird uebergeben, wenn der Studienarm ungueltig.
+	 */
 	public static final String STUDIENARM_UNGUELTIG = "Studienarm ungültig.";
+	/**
+	 * Diese Konstante wird uebergeben, wenn der Status nicht angegeben wurde.
+	 */
+	public static final String STATUSFEHLER ="Statusfehler.";
+	/**
+	 * Diese Konstante wird uebergeben, wenn der Status falsch gesetzt wurde.
+	 */
+	public static final String STATUS_UNGUELTIG = "Status falsch gesetzt.";
 	
 	
 	/**
