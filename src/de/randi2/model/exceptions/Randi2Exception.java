8ca1a5a2fb3b84731702e3b7fd06c1cfaadd8bea
jthoenes <jthoenes@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-05-04 14:23:56 +0000

    ValidierungsUtil & Test Randi2Exception programmiert
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1009 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/model/exceptions/Randi2Exception.java
+++ src/de/randi2/model/exceptions/Randi2Exception.java
@@ -1,18 +1,54 @@
 package de.randi2.model.exceptions;
 
+import org.apache.log4j.Logger;
+
 /**
- * @author Andreas Freudling [afreudling@hs-heilbronn.de]
+ * Die Randi2Exception ist eine Exception in der Systemfehler gekapselt werden
+ * koennen. Fehler die von ihr gekapselt werden, werden automatisch geloggt und
+ * dem Benutzer wird eine verstaendliche Warnung ausgegeben, die auf der
+ * Oberflaeche angezeigt werden kann.
+ * 
+ * @author Johannes Thoenes [jthoenes@stud.hs-heilbronn.de]
  * @version $Id$
- *
+ * 
  */
-public class Randi2Exception extends Exception {
+public abstract class Randi2Exception extends Exception {
 
-    /**
-     * @param fehlermeldung
-     */
-    public Randi2Exception(String fehlermeldung) {
-	super(fehlermeldung);
+	/**
+	 * Die nicht technische Fehlermeldung die dem Benutzer angezeigt werden
+	 * soll.
+	 */
+	private static final String FEHLERMELDUNG = "Es ist ein nicht behebbarer Fehler im System aufgetreten. Bitte verst&auml;ndigen Sie den Systemadministrator.";
 
-    }
+	/**
+	 * Loggt den Error in die Log-Datei.
+	 * 
+	 */
+	private void reportError() {
+		Logger log = Logger.getLogger(this.getClass());
+		log.error("Nicht behebbarer Systemfehler.", this);
+	}
+
+	/**
+	 * Konstruktor mit der Angabe eine spezialisierten Fehlermeldung.
+	 * 
+	 * @param fehlermeldung
+	 *            Nicht-technische Fehlermeldung die dem Benutzer angezeigt
+	 *            werden soll.
+	 */
+	public Randi2Exception(String fehlermeldung) {
+		super(fehlermeldung);
+		this.reportError();
+	}
+
+	/**
+	 * Konstruktor falls dem Benutzer die Standartfehlermeldung angezeigt werden
+	 * soll.
+	 * 
+	 */
+	public Randi2Exception() {
+		super(FEHLERMELDUNG);
+		this.reportError();
+	}
 
 }
