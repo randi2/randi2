04355e5bed09ff63665d51f1737901fe663becf6
afreudli <afreudli@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-05-04 07:26:15 +0000

    Checkstyle Fehler Wegmach-Aktion
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@981 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/utility/Config.java
+++ src/de/randi2/utility/Config.java
@@ -2,7 +2,6 @@ package de.randi2.utility;
 
 import java.util.Properties;
 import org.apache.log4j.Logger;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,25 +9,54 @@ import java.io.IOException;
 import de.randi2.controller.DispatcherServlet;
 
 /**
+ * Alle Daten aus den Konfigurationsdateien in conf/debug und conf/release werden ueber diese Klasse angesprochen.
+ * Konfigurationen die die Datenbank betreffen finden sich in conf/release/proxool_cfg.xml
  * @author Andreas Freudling afreudling@stud.hs-heilbronn.de
  * @version $Id$
  * 
  */
-public class Config {
+public final class Config {
+   
+	/**
+	 * Varible fuer den Singleton
+	 */
 	private static Config singleton = null;
 
+	/**
+	 * Properties Objekt fuer die Debug-Konfigurationsdatei
+	 */
 	private Properties debugConf = null;
 
+	/**
+	 * Properties Objekt fuer die Release-Konfigurationsdatei
+	 */
 	private Properties releaseConf = null;
 
+	/**
+	 * Properties Objekt fuer die Systemsperrung-Konfigurationsdatei
+	 */
 	private Properties systemsperrungConf = null;
 
+	/**
+	 * Dateiname der Release-Konfigurationsdatei
+	 */
 	private String releaseDateiname = "";
 
+	/**
+	 * Dateiname der Debug-Konfigurationsdatei
+	 */
 	private String debugDateiname = "";
 
+	/**
+	 * 
+	 * Dateiname der Konfigurationsdatei für die Systemsperrung
+	 */
 	private String systemsperrungDateiname = "";
 
+	/**
+	 * Privater Konstruktor für den Singleton
+	 * Liest Konfigurationsdateien
+	 */
 	private Config() {
 		releaseDateiname = "conf/release/release.conf";
 		debugDateiname = "conf/debug/debug.conf";
@@ -63,8 +91,9 @@ public class Config {
 	}
 
 	/**
-	 * @param feld
-	 * @return
+	 * Um Property-Informationen egal welcher Konfigurationsdatei auszulesen.
+	 * @param feld Name der Property
+	 * @return Daten des gewuenschten Properties
 	 */
 	public static synchronized String getProperty(Felder feld) {
 		if (singleton == null) {
@@ -74,8 +103,9 @@ public class Config {
 	}
 
 	/**
-	 * @param fehlermeldung
-	 * @return
+	 * Methode speichert die Sperrung des Systems persistent in einer Konfigurationsdatei
+	 * @param fehlermeldung Fehlermeldung die gespeichert wird, wenn das System gesperrt werden soll
+	 * @return true falls Sperrung erfolgreich, sonst false
 	 */
 	public synchronized boolean sperreSystem(String fehlermeldung) {
 		if (singleton == null) {
@@ -97,6 +127,7 @@ public class Config {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//TODO @Benny muss das jetzt weg oder nicht
 		DispatcherServlet aDispatcherServlet = new DispatcherServlet();
 		aDispatcherServlet.setSystemGesperrt(true); //angepasst an Aendungen in Servlet 
 		aDispatcherServlet.setMeldungSystemGesperrt(fehlermeldung);
@@ -104,7 +135,8 @@ public class Config {
 	}
 
 	/**
-	 * @return
+	 * Methode speichert die Entsperrung des Systems persistent ind einer Konfigurationsdatei.
+	 * @return true, falls Speicherung erfolgreich war, sonst false
 	 */
 	public synchronized boolean entsperreSystem() {
 		if (singleton == null) {
@@ -132,83 +164,76 @@ public class Config {
 	}
 
 	/**
-	 * 
-	 * 
+	 * Alle Konfigurationsfelder aus den Konfigurationsdateien muesen hier hinterlegt werden.
+	 * Dabei ist folgende Namenskonvention zu beachten: [Konfigurationstyp]_[Konfigurationsuntersektion]_[Exakte_Konfiguration].
+	 * Der Name der Enum muss exakt gleich dem Namen in der Konfigurationsdatei sein.
 	 */
 	public enum Felder {
 		/**
-		 * 
+		 * Hostname des Selenium Servers
 		 */
 		DEBUG_SELENIUM_SERVER_HOST,
 		/**
-		 * 
+		 * Port des Selenium Servers
 		 */
 		DEBUG_SELENIUM_SERVER_PORT,
 		/**
-		 * 
+		 * Pfad zur Exe-Datei des Browsers
 		 */
 		DEBUG_SELENIUM_FIREFOX_LOCATION,
 		/**
-		 * 
+		 * Startseite fuer den Selenium Server
 		 */
 		DEBUG_SELENIUM_START_URL,
+		
 		/**
-		 * 
+		 * Hostname des Mail Servers
 		 */
 		RELEASE_MAIL_SERVER,
+		
 		/**
-		 * 
+		 * Benutzername fuer den Mailaccount
 		 */
 		RELEASE_MAIL_ACCOUNT,
+		
 		/**
-		 * 
+		 * Passwort fuer den Mailaccount
 		 */
 		RELEASE_MAIL_PASSWORD,
+		
 		/**
-		 * 
+		 * Absenderadresse fuer den Mailaccount
 		 */
 		RELEASE_MAIL_RANDI2MAILADRESSE,
+		
 		/**
-		 * 
+		 *Absender-Name des Mailaccounts
 		 */
 		RELEASE_MAIL_RANDI2NAME,
 		/**
-		 * 
+		 * Email-Adresse bei denen die Emails "bouncen"
 		 */
 		RELEASE_MAIL_RANDI2BOUNCE,
 		/**
-		 * 
+		 * Debug gibt einen Log auf der Console aus
 		 */
 		RELEASE_MAIL_DEBUG,
 
 		/**
-		 * 
+		 * Speicherung ob System gesperrt ist oder nicht.
 		 */
 		SYSTEMSPERRUNG_SYSTEMSPERRUNG,
 
 		/**
-		 * 
+		 * Fehlermeldung die fuer die Systemsperrung gespeichert wird.
 		 */
 		SYSTEMSPERRUNG_FEHLERMELDUNG,
-		
-		/**
-		 * 
-		 */
-		RELEASE_DB_HOST,
-		/**
-		 * 
-		 */
-		RELEASE_DB_PORT,
-		/**
-		 * 
-		 */
-		RELEASE_DB_NUTZERNAME,
-		/**
-		 * 
-		 */
-		RELEASE_DB_PASSWORT
 	}
 
+	/**
+	 * Main-Methode zum Testen
+	 * @param args Parameter bei Programmstart
+	 */
 	public static void main(String[] args) {
 		for (Config.Felder e : Config.Felder.values()) {
 			System.out.println(Config.getProperty(e));
