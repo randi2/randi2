000a3e5463b7c0d57f4e88ae6e2f9054c88839bc
dhaehn <dhaehn@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-05-16 08:21:30 +0000

    JUnit Test fuer StudienarmBean
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1126 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/junittests/StudienarmBeanTest.java
@@ -0,0 +1,253 @@
+package de.randi2.junittests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.GregorianCalendar;
+import java.util.Random;
+import java.util.Vector;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.randi2.model.fachklassen.Studie;
+import de.randi2.model.fachklassen.beans.PatientBean;
+import de.randi2.model.fachklassen.beans.StudieBean;
+import de.randi2.model.fachklassen.beans.StudienarmBean;
+
+/**
+ * Diese Klasse stellt einen Test fuer das StudienarmBean zur Verfuegung.
+ * 
+ * 
+ * @author Daniel Haehn [dhaehn@stud.hs-heilbronn.de]
+ * @version $Id $
+ * 
+ */
+public class StudienarmBeanTest {
+
+	// das zu testende Objekt
+	private StudienarmBean aStudienarmBean = null;
+
+	/**
+	 * Method setUp() Erzeugt eine neue Instanz der Klasse StudieBean.
+	 * 
+	 * @throws Exception,
+	 *             Fehler, wenn keine Instanz der Klasse StudieBean erzeugt
+	 *             werden kann.
+	 */
+	@Before
+	public void setUp() throws Exception {
+		aStudienarmBean = new StudienarmBean();
+
+		aStudienarmBean.setId(21424);
+		aStudienarmBean.setBeschreibung("Testbeschreibung");
+		aStudienarmBean.setBezeichnung("Testbezeichnung");
+		aStudienarmBean.setStatus(Studie.Status.AKTIV);
+		aStudienarmBean.setStudieId(3434);
+		aStudienarmBean.setStudie(new StudieBean());
+	}
+
+	/**
+	 * Method tearDown() Dem StudieBean-Objekt wird der Wert "null" zugewiesen.
+	 * 
+	 * @throws Exception,
+	 *             wenn die Testklasse nicht beendet werden kann.
+	 */
+	@After
+	public void tearDown() throws Exception {
+		aStudienarmBean = null;
+	}
+
+	/**
+	 * Testet, ob die toString() Methode einen String zurueckgibt.
+	 * 
+	 * TestMethode fuer
+	 * {@link de.randi2.model.fachklassen.beans.StudienarmBean#toString()}.
+	 * 
+	 */
+	@Test
+	public void testToString() {
+		assertFalse(aStudienarmBean.toString().equals(null));
+
+	}
+
+	/**
+	 * Testet, ob zwei identische StudienarmBeans auch als identisch erkannt
+	 * werden.
+	 * 
+	 * TestMethode fuer
+	 * {@link de.randi2.model.fachklassen.beans.StudienarmBean#equals(Object)}.
+	 * 
+	 */
+	@Test
+	public void testEquals() {
+
+		StudienarmBean beanZuvergleichen = aStudienarmBean;
+		assertTrue(aStudienarmBean.equals(beanZuvergleichen));
+
+	}
+
+	/**
+	 * Testet, ob alle Werte der Status-Enum als Status des Studienarms gesetzt
+	 * werden koennen.
+	 * 
+	 * TestMethode fuer
+	 * {@link de.randi2.model.fachklassen.beans.StudienarmBean#setStatus(de.randi2.model.fachklassen.Studie.Status)}.
+	 */
+	@Test
+	public void testSetStatus() {
+
+		for (Studie.Status aStatus : Studie.Status.values()) {
+
+			aStudienarmBean.setStatus(aStatus);
+			assertTrue(aStudienarmBean.getStatus() == aStatus);
+
+		}
+
+	}
+
+	/**
+	 * Testet, ob eine Beschreibung erfolgreich fuer den Studienarm gesetzt
+	 * werden kann.
+	 * 
+	 * TestMethode fuer
+	 * {@link de.randi2.model.fachklassen.beans.StudienarmBean#setBeschreibung(String)}.
+	 */
+	@Test
+	public void testSetBeschreibung() {
+
+		// gueltige Beschreibungen
+		String[] testBeschreibungen = {
+				"",
+				"dies ist eine testbeschreibung",
+				"didsg sdg dgdsugudshg dshgudshgudsugohdshg dgh ighidhgidhgidshg dsighidhgidshgodshg dgihdighdihgidhsgihsdighdsighd sgdighdsighdishgidhihdighdsihg disghidghsiodg" };
+
+		for (int i = 0; i < testBeschreibungen.length; i++) {
+			aStudienarmBean.setBeschreibung(testBeschreibungen[i]);
+			assertTrue(aStudienarmBean.getBeschreibung().equals(
+					testBeschreibungen[i]));
+		}
+
+	}
+
+	/**
+	 * Testet, ob eine Bezeichnung erfolgreich fuer den Studienarm gesetzt
+	 * werden kann.
+	 * 
+	 * TestMethode fuer
+	 * {@link de.randi2.model.fachklassen.beans.StudienarmBean#setBezeichnung(String)}.
+	 */
+	@Test
+	public void testSetBezeichnung() {
+
+		// gueltige Beschreibungen
+		String[] testBezeichnungen = {
+				"",
+				"dies ist eine testbezeichnung",
+				"didsg sdg dgdsugudshg dshgudshgudsugohdshg dgh ighidhgidhgidshg dsighidhgidshgodshg dgihdighdihgidhsgihsdighdsighd sgdighdsighdishgidhihdighdsihg disghidghsiodg" };
+
+		for (int i = 0; i < testBezeichnungen.length; i++) {
+			aStudienarmBean.setBeschreibung(testBezeichnungen[i]);
+			assertTrue(aStudienarmBean.getBeschreibung().equals(
+					testBezeichnungen[i]));
+		}
+
+	}
+
+	/**
+	 * Testet, ob Patienten erfolgreich dem Studienarm zugeordnet werden
+	 * koennen.
+	 * 
+	 * TestMethode fuer
+	 * {@link de.randi2.model.fachklassen.beans.StudienarmBean#setPatienten(Vector)}.
+	 */
+	@Test
+	public void testSetPatienten() {
+
+		Vector<PatientBean> aPatienten = new Vector<PatientBean>();
+
+		for (int i = 0; i < 10; i++) {
+
+			try {
+
+				aPatienten.add(new PatientBean(i, "abc" + i, 'm',
+						new GregorianCalendar(1983, 3, i), 3,
+						new GregorianCalendar(1983, 3, i), 5, aStudienarmBean));
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+				fail("Patient wurde nicht zugeordnet!");
+			}
+
+		}
+
+		aStudienarmBean.setPatienten(aPatienten);
+
+		assertTrue(aStudienarmBean.getPatienten() == aPatienten);
+		assertTrue(aStudienarmBean.getPatAnzahl() == aPatienten.size());
+
+	}
+
+	/**
+	 * Testet, ob eine Studie erfolgreich im Studienarm gesetzt werden kann.
+	 * 
+	 * TestMethode fuer
+	 * {@link de.randi2.model.fachklassen.beans.StudienarmBean#setStudie(StudieBean)}.
+	 */
+	@Test
+	public void testSetStudie() {
+		try {
+			StudieBean aStudie = new StudieBean(34234, "dsfdsf",
+					new GregorianCalendar(1983, 3, 17), new GregorianCalendar(
+							1983, 3, 17), "sfsfa", 344);
+
+			aStudienarmBean.setStudie(aStudie);
+
+			assertTrue(aStudienarmBean.getStudie().equals(aStudie));
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail("Studie konnte nicht angelegt werden!");
+		}
+
+	}
+
+	/**
+	 * Testet, ob eine StudieId erfolgreich im Studienarm gesetzt werden kann.
+	 * 
+	 * TestMethode fuer
+	 * {@link de.randi2.model.fachklassen.beans.StudienarmBean#setStudieId(long)}.
+	 */
+	@Test
+	public void testSetStudieId() {
+		Random zufall = new Random();
+		long aId = zufall.nextLong();
+
+		aStudienarmBean.setStudieId(aId);
+
+		assertTrue(aStudienarmBean.getStudieId() == aId);
+
+	}
+
+	/**
+	 * Testet, ob eine Id erfolgreich im Studienarm gesetzt werden kann.
+	 * 
+	 * TestMethode fuer
+	 * {@link de.randi2.model.fachklassen.beans.StudienarmBean#setId(long)}.
+	 */
+	@Test
+	public void testSetId() {
+
+		Random zufall = new Random();
+		long aId = zufall.nextLong();
+
+		aStudienarmBean.setId(aId);
+
+		assertTrue(aStudienarmBean.getId() == aId);
+
+	}
+
+}
