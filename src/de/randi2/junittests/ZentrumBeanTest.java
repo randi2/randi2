57394d49075606edaaddeca73207343c56d32f17
twillert <twillert@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-05-22 18:58:12 +0000

    TODO noch bearbeitet, geht nach wie vor!
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1178 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/junittests/ZentrumBeanTest.java
+++ src/de/randi2/junittests/ZentrumBeanTest.java
@@ -13,16 +13,10 @@ import de.randi2.model.fachklassen.beans.ZentrumBean;
 import de.randi2.utility.Log4jInit;
 
 /**
- * TODO die get() Methoden brauchen wir nicht explizit mit eigenen Methoden zu
- * testen, da sie eigentlich bei den set() Methoden mitgetestet werden. Das ist
- * leider hier noch nicht der Fall. Man muss immer nach einem
- * set(irgendwas)Aufruf entsprechende get Methode aufrufen, und die Werte
- * vergleichen - ob das was zu setzten war, tatsaetzlich gesetzt wurde und von
- * der get methode gelifert wird.
+ * Die get() Methoden werden bei den set() Methoden mitgetestet.
  * 
  * Beispiel: zentrum.setAbteilung("Abteilung1");
- * assertTrue("Abteilung1",zentrum.getAbteilung());
- * 
+ * assertTrue(zentrum.getAbteilung().equals("Abteilung1"));
  * 
  * @author Katharina Chruscz [kchruscz@stud.hs-heilbronn.de]
  * @version $Id: ZentrumBeanTest.java 2426 2007-05-06 17:34:32Z twillert $
@@ -86,15 +80,6 @@ public class ZentrumBeanTest {
 	@Test
 	public void testZentrumBean() {
 		new ZentrumBean();
-	}
-
-	/**
-	 * Test method for
-	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#getAbteilung()}.
-	 */
-	@Test
-	public void testGetAbteilung() throws Exception {
-		abteilung = zentrum.getAbteilung();
 	}
 
 	/**
@@ -180,41 +165,6 @@ public class ZentrumBeanTest {
 
 	/**
 	 * Test method for
-	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#getAnsprechpartner()}.
-	 */
-	@Test
-	public void testGetAnsprechpartner() {
-
-		try {
-
-			PersonBean ansprechpartner = new PersonBean(1, id, "nachname",
-					"vorname", PersonBean.Titel.PROF, 'm',
-					"user@hs-heilbronn.de", "01760099334", "017600972487",
-					"01760427424");
-
-			zentrum.setAnsprechpartner(ansprechpartner);
-
-			PersonBean ansprechpartner2 = zentrum.getAnsprechpartner();
-
-			assertTrue(ansprechpartner.equals(ansprechpartner2));
-
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
-	}
-
-	/**
-	 * Test method for
-	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#getHausnr()}.
-	 */
-	@Test
-	public void testGetHausnr() {
-		hausnr = zentrum.getHausnr();
-
-	}
-
-	/**
-	 * Test method for
 	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#setHausnr(java.lang.String)}.
 	 * 
 	 * @throws ZentrumException
@@ -235,16 +185,6 @@ public class ZentrumBeanTest {
 	public void testSetHausnrFalsch() throws ZentrumException {
 		zentrum.setHausnr("{3s209>");
 		assertTrue(zentrum.getHausnr().equals("{3s209>"));
-	}
-
-	/**
-	 * Test method for
-	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#getInstitution()}.
-	 */
-	@Test
-	public void testGetInstitution() {
-		institution = zentrum.getInstitution();
-
 	}
 
 	/**
@@ -305,15 +245,6 @@ public class ZentrumBeanTest {
 
 	/**
 	 * Test method for
-	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#getOrt()}.
-	 */
-	@Test
-	public void testGetOrt() {
-		ort = zentrum.getOrt();
-	}
-
-	/**
-	 * Test method for
 	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#setOrt(java.lang.String)}.
 	 * 
 	 * @throws ZentrumException
@@ -321,6 +252,7 @@ public class ZentrumBeanTest {
 	@Test
 	public void testSetOrtNormal() throws ZentrumException {
 		zentrum.setOrt("ort");
+		assertTrue(zentrum.getOrt().equals("ort"));
 	}
 
 	/**
@@ -332,6 +264,8 @@ public class ZentrumBeanTest {
 	@Test
 	public void testSetOrtMaxLaenge() throws ZentrumException {
 		zentrum.setOrt("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		assertTrue(zentrum.getOrt().equals(
+				"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
 	}
 
 	/**
@@ -343,6 +277,7 @@ public class ZentrumBeanTest {
 	@Test(expected = ZentrumException.class)
 	public void testSetOrtZuKurz() throws ZentrumException {
 		zentrum.setOrt("a");
+		assertTrue(zentrum.getOrt().equals("a"));
 	}
 
 	/**
@@ -355,6 +290,8 @@ public class ZentrumBeanTest {
 	public void testSetOrtZuLang() throws ZentrumException {
 		zentrum
 				.setOrt("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		assertTrue(zentrum.getOrt().equals(
+				"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
 	}
 
 	/**
@@ -365,7 +302,7 @@ public class ZentrumBeanTest {
 	public void testGetPasswort() {
 		passwort = zentrum.getPasswort();
 	}
-
+	
 	/**
 	 * Test method for
 	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#setPasswort(java.lang.String)}.
@@ -385,18 +322,7 @@ public class ZentrumBeanTest {
 	 */
 	@Test(expected = ZentrumException.class)
 	public void testSetPasswortFalsch() throws ZentrumException {
-
 		zentrum.setPasswortKlartext("aaaaa(a&_67a234ikrfoawra");
-
-	}
-
-	/**
-	 * Test method for
-	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#getPlz()}.
-	 */
-	@Test
-	public void testGetPlz() {
-		plz = zentrum.getPlz();
 	}
 
 	/**
@@ -408,6 +334,7 @@ public class ZentrumBeanTest {
 	@Test
 	public void testSetPlzRichtig() throws ZentrumException {
 		zentrum.setPlz("12345");
+		assertTrue(zentrum.getPlz().equals("12345"));
 	}
 
 	/**
@@ -419,16 +346,8 @@ public class ZentrumBeanTest {
 	@Test(expected = ZentrumException.class)
 	public void testSetPlzFalsch() throws ZentrumException {
 		zentrum.setPlz("11342234111");
+		assertTrue(zentrum.getPlz().equals("11342234111"));
 
-	}
-
-	/**
-	 * Test method for
-	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#getStrasse()}.
-	 */
-	@Test
-	public void testGetStrasse() {
-		strasse = zentrum.getStrasse();
 	}
 
 	/**
@@ -440,6 +359,7 @@ public class ZentrumBeanTest {
 	@Test
 	public void testSetStrasseNormal() throws ZentrumException {
 		zentrum.setStrasse("Strasse");
+		assertTrue(zentrum.getStrasse().equals("Strasse"));
 	}
 
 	/**
@@ -452,6 +372,8 @@ public class ZentrumBeanTest {
 	public void testSetStrasseMaxLaenge() throws ZentrumException {
 		zentrum
 				.setStrasse("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		assertTrue(zentrum.getStrasse().equals(
+				"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
 	}
 
 	/**
@@ -463,6 +385,7 @@ public class ZentrumBeanTest {
 	@Test(expected = ZentrumException.class)
 	public void testSetStrasseZuKurz() throws ZentrumException {
 		zentrum.setStrasse("a");
+		assertTrue(zentrum.getStrasse().equals("a"));
 	}
 
 	/**
@@ -475,15 +398,8 @@ public class ZentrumBeanTest {
 	public void testSetStrasseZuLang() throws ZentrumException {
 		zentrum
 				.setStrasse("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-	}
-
-	/**
-	 * Test method for
-	 * {@link de.randi2.model.fachklassen.beans.ZentrumBean#getId()}.
-	 */
-	@Test
-	public void testGetId() {
-		id = zentrum.getId();
+		assertTrue(zentrum.getStrasse().equals(
+				"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
 	}
 
 	/**
@@ -493,6 +409,7 @@ public class ZentrumBeanTest {
 	@Test
 	public void testSetId() {
 		zentrum.setId(id);
+		assertTrue(zentrum.getId() == id);
 	}
 
 }
