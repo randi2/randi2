554a827a096c4b375ce261367d2a5146c21990c9
dhaehn <dhaehn@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-05-22 20:31:40 +0000

    aktualisiert
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1198 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/model/exceptions/StudienarmException.java
+++ src/de/randi2/model/exceptions/StudienarmException.java
@@ -1,11 +1,20 @@
 package de.randi2.model.exceptions;
 
 /**
+ * Diese Klasse beinhaltet die Fehler, die innerhalb der Klasse Studienarm
+ * auftreten koennen.
  * 
- * @author Lukasz Plotnicki [lplotni@stud.hs-heilbronn.de]
+ * 
+ * @author Daniel Haehn [dhaehn@stud.hs-heilbronn.de]
  * @version $Id: StudienarmException.java 2418 2007-05-04 14:37:12Z jthoenes $
  */
 public class StudienarmException extends BenutzerException {
+
+	/**
+	 * Diese Konstante wird uebergeben, wenn die Patienten nicht gefunden
+	 * wurden.
+	 */
+	public static final String PATIENTEN_NICHT_GEFUNDEN = "Die Patienten wurden nicht gefunden.";
 
 	/**
 	 * 
