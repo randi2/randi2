71f74a0cfce50603460d6fd77fa118cc90d61321
vgraeff <vgraeff@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-03 11:50:36 +0000

    Da war wohl jemand schneller als ich. Vergas die Zentrumspasswoerter zu hashen
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1286 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/utility/DBScriptUtil.java
+++ src/de/randi2/utility/DBScriptUtil.java
@@ -19,28 +19,31 @@ public final class DBScriptUtil {
 
 	/**
 	 * Aufruf der Hash Methode zur Verschluesselung der Passwoerter
+	 * sowie die Generierung zufalliger Passoerter
 	 * 
 	 * @param args
 	 *            Jetzt nicht
 	 */
 	public static void main(String[] args) {
 		Log4jInit.initDebug();
-
-		System.out.println("Passwort Frank "
-				+ KryptoUtil.getInstance().hashPasswort("1$hess80") + " Ende");
-		System.out
-				.println("Passwort Valentin "
-						+ KryptoUtil.getInstance().hashPasswort("1$graeff83")
-						+ " Ende");
-		System.out.println("Passwort Nadine "
-				+ KryptoUtil.getInstance().hashPasswort("1$zwink83") + " Ende");
-		System.out.println("Passwort Hans "
-				+ KryptoUtil.getInstance().hashPasswort("1$dampf75") + " Ende");
 		
+		System.out.println("\n\n--------------- Hash ---------------");
+		System.out.println("Passwort Frank " + KryptoUtil.getInstance().hashPasswort("1$hess80") + " Ende");
+		System.out.println("Passwort Valentin "	+ KryptoUtil.getInstance().hashPasswort("1$graeff83") + " Ende");
+		System.out.println("Passwort Nadine " + KryptoUtil.getInstance().hashPasswort("1$zwink83") + " Ende");
+		System.out.println("Passwort Hans "	+ KryptoUtil.getInstance().hashPasswort("1$dampf75") + " Ende");
+		
+		System.out.println("\n\n--------------- Generator ---------------");
 		System.out.println("Passwort1  " + KryptoUtil.getInstance().generatePasswort(12));
 		System.out.println("Passwort2  " + KryptoUtil.getInstance().generatePasswort(12));
 		System.out.println("Passwort3  " + KryptoUtil.getInstance().generatePasswort(12));
 		System.out.println("Passwort4  " + KryptoUtil.getInstance().generatePasswort(12));
+		
+		System.out.println("\n\n--------------- Hash Zentrum ---------------");
+		System.out.println(KryptoUtil.getInstance().hashPasswort(")dL*mGPeIBB+"));
+		System.out.println(KryptoUtil.getInstance().hashPasswort("6bA&vbn8EhNt"));
+		System.out.println(KryptoUtil.getInstance().hashPasswort("5Q5JRpT6NUYR"));
+		System.out.println(KryptoUtil.getInstance().hashPasswort("9$5WIPsK3n22"));
 	}
 
 }
