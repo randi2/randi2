3d053745c7b9f7a107397da24f020dcf8caf5c5e
tganszky <tganszky@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-06 08:51:32 +0000

    Selenium test auf internet explorer umgestellt
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1323 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/seleniumtest/TestBenutzerRegistrieren.java
+++ src/de/randi2/seleniumtest/TestBenutzerRegistrieren.java
@@ -1,7 +1,8 @@
 package de.randi2.seleniumtest;
 
 import com.thoughtworks.selenium.*;
-import de.randi2.utility.*;
+
+//import de.randi2.utility.*;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,11 +18,8 @@ public class TestBenutzerRegistrieren{
 	
 	@BeforeClass
 	public static void setUpBeforeClass(){
-        //Config laden
-		Log4jInit.initDebug();
-		//Selenium wird gestartet
-		 sel = new DefaultSelenium(Config.getProperty(Config.Felder.DEBUG_SELENIUM_SERVER_HOST),
-			 Integer.parseInt(Config.getProperty(Config.Felder.DEBUG_SELENIUM_SERVER_PORT)), "*firefox "+Config.getProperty(Config.Felder.DEBUG_SELENIUM_FIREFOX_LOCATION), Config.getProperty(Config.Felder.DEBUG_SELENIUM_START_URL));
+         //Selenium wird gestartet
+		 sel = new DefaultSelenium("localhost",4444, "*iexplore C:/Programme/Internet Explorer/iexplore.exe", "http://www.iap.hs-heilbronn.de:8080/randinightly/");
 		 sel.start();
 	}	
 	
@@ -33,14 +31,14 @@ public class TestBenutzerRegistrieren{
 	@Test
 	public void testBenutzerAnlegen(){
 		
-		sel.open("http://localhost:8080/RANDI2/index.jsp");
+		sel.open("http://www.iap.hs-heilbronn.de:8080/randinightly/index.jsp");
 		
 	    sel.click("//input[@value='Benutzer registrieren']");
 	    sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
 		assertTrue(sel.isTextPresent("Haftungsausschluss"));
 		
-		/**sel.click("//input[@value='Akzeptieren']");
+		sel.click("//input[@value='Akzeptieren']");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
 		assertTrue(sel.isTextPresent("Zentrum suchen"));
@@ -54,7 +52,7 @@ public class TestBenutzerRegistrieren{
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
 		assertTrue(sel.isTextPresent("Haftungsausschluss"));
-		*/
+		
 		sel.click("abbrechen");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Herzlich Willkommen"));
