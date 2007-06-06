4ae93befa98ea491b9be119b4bb6f5f5e861f6d6
tganszky <tganszky@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-06 21:35:55 +0000

    testfälle erweitert
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1342 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/seleniumtest/TestBenutzerRegistrieren.java
+++ src/de/randi2/seleniumtest/TestBenutzerRegistrieren.java
@@ -9,113 +9,127 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Release 1, SWP2:  Seleniumtest Benutzer registrieren
+ * Release 1, SWP2: Seleniumtest Benutzer registrieren
+ * 
  * @author Thomas Ganszky
  * @date 05.06.2007
  */
-public class TestBenutzerRegistrieren{
+public class TestBenutzerRegistrieren {
 	private static Selenium sel;
-	
+
 	@BeforeClass
-	public static void setUpBeforeClass(){
-         //Selenium wird gestartet
-		 sel = new DefaultSelenium("localhost",4444, "*iexplore C:/Programme/Internet Explorer/iexplore.exe", "http://www.iap.hs-heilbronn.de:8080/randinightly/");
-		 sel.start();
-	}	
-	
-	
-	
+	public static void setUpBeforeClass() {
+		// Selenium wird gestartet
+		sel = new DefaultSelenium("localhost", 4444,
+				"*iexplore C:/Programme/Internet Explorer/iexplore.exe",
+				"http://www.iap.hs-heilbronn.de:8080/randinightly/");
+		sel.start();
+	}
+
 	/**
 	 * Test Benutzer anlegen
 	 */
 	@Test
-	public void testBenutzerAnlegen(){
-		
+	public void testBenutzerAnlegen() {
+
 		sel.open("http://www.iap.hs-heilbronn.de:8080/randinightly/index.jsp");
-		
-	    sel.click("//input[@value='Benutzer registrieren']");
-	    sel.waitForPageToLoad("30000");
+
+		sel.click("//input[@value='Benutzer registrieren']");
+		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
 		assertTrue(sel.isTextPresent("Haftungsausschluss"));
-		
+
 		sel.click("//input[@value='Akzeptieren']");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
 		assertTrue(sel.isTextPresent("Zentrum suchen"));
-		
+
 		sel.click("Filtern");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
 		assertTrue(sel.isTextPresent("Zentrum suchen"));
-		
+
 		sel.click("abbrechen");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
 		assertTrue(sel.isTextPresent("Haftungsausschluss"));
-		
+
 		sel.click("abbrechen");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Herzlich Willkommen"));
 	}
-	
+
 	@Test
-	public void falschesZentrumspasswort(){
+	public void falschesZentrumspasswort() {
 		sel.open("http://www.iap.hs-heilbronn.de:8080/randinightly/index.jsp");
-		
+
 		sel.click("//input[@value='Benutzer registrieren']");
 		sel.waitForPageToLoad("30000");
-	
+
 		sel.click("//input[@value='Akzeptieren']");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
 		assertTrue(sel.isTextPresent("Zentrum suchen"));
-		
+
 		sel.type("zentrum_passwort1", "asdasd");
-	
+
 		sel.click("bestaetigen1");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Falsches Zentrumpasswort"));
-		
+
 		sel.click("abbrechen");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
 		assertTrue(sel.isTextPresent("Haftungsausschluss"));
-		
+
 		sel.click("abbrechen");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Herzlich Willkommen"));
-		
+
 	}
-	
+
 	@Test
-	public void korrektesZentrumspasswort(){
+	public void korrektesZentrumspasswort() {
 		sel.open("http://www.iap.hs-heilbronn.de:8080/randinightly/index.jsp");
-		
+
 		sel.click("//input[@value='Benutzer registrieren']");
 		sel.waitForPageToLoad("30000");
-	
+
 		sel.click("//input[@value='Akzeptieren']");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
 		assertTrue(sel.isTextPresent("Zentrum suchen"));
-		
+
 		sel.type("zentrum_passwort1", "nch1!$knochen80");
-	
+
 		sel.click("bestaetigen1");
 		sel.waitForPageToLoad("30000");
 		assertTrue(sel.isTextPresent("Benutzer anlegen"));
-		
-		
-		
+
+		sel.select("Titel", "label=Dr.");
+		sel.type("Vorname", "Willi");
+		sel.type("Nachname", "Wichtig");
+		sel.click("document.user.Geschlecht[1]");
+		sel.type("Passwort", "12!\"AAaayyyyyyyyyy)9");
+		sel.type("Passwort_wh", "12!\"AAaayyyyyyyyyy)9");
+		// sel.click("anlegen");
+		sel.type("Telefon", "012123123");
+	//	sel.click("anlegen");
+		sel.type("Email", "bla@blamail.bla");
+		sel.type("Telefon", "123123");
+		sel.click("anlegen");
+
+		// TODO Schreibfehler ist korrigiert, aber noch nicht im nightly
+		assertTrue(sel.isTextPresent("Letzer Schritt der Registrierung"));
+		assertTrue(sel
+				.isTextPresent("Sie erhalten in Kürze eine Email mit einem Aktivierungslink."));
+
 	}
-	
-	
-	
-	 @AfterClass
-	 public static void tearDownAfterClass() 
-	   {
-         //Stoppen von Selenium
-	     sel.stop();
-	   }
-	
-}//SeleniumTestLogin
+
+	@AfterClass
+	public static void tearDownAfterClass() {
+		// Stoppen von Selenium
+		sel.stop();
+	}
+
+}// SeleniumTestLogin
