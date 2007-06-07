11e0d4b0292d89cf5340c897d500bc09886886ee
nzwink <nzwink@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-07 19:05:42 +0000

    Seleniumtest Login, Logout fuer Release 2
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1345 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/seleniumtest/TestLoginLogoutRelease2.java
@@ -0,0 +1,486 @@
+package de.randi2.seleniumtest;
+
+import com.thoughtworks.selenium.*;
+import de.randi2.utility.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Release 2, SWP3:  Seleniumtest Login und Logout
+ *                   mit Aufruf der berechtigten Seite(n)
+ *                   des Benutzers
+ * @author Nadine Zwink
+ * @date 06.06.2007
+ */
+public class TestLoginLogoutRelease2{
+	
+	private static Selenium sel;
+	
+	@BeforeClass
+	public static void setUpBeforeClass(){
+        //Config laden
+		Log4jInit.initDebug();
+		//Selenium wird gestartet
+		 sel = new DefaultSelenium(Config.getProperty(Config.Felder.DEBUG_SELENIUM_SERVER_HOST),
+			 Integer.parseInt(Config.getProperty(Config.Felder.DEBUG_SELENIUM_SERVER_PORT)), "*firefox "+Config.getProperty(Config.Felder.DEBUG_SELENIUM_FIREFOX_LOCATION), Config.getProperty(Config.Felder.DEBUG_SELENIUM_START_URL));
+		 sel.start();
+	}	
+	
+	
+	/**
+	 * Pruefen von Benutzername und Passwort des Administrators.
+	 * Pruefen, ob Administrator die für ihn berechtige Seite mit 
+	 * Menuepunkte zu sehen bekommt.
+	 */
+	@Test
+    public void pruefeLoginAdmin() {
+
+		 //Seite oeffnen
+		 sel.open("http://www.iap.hs-heilbronn.de:8080/randinightly/");
+			 
+		 //Überprüfung der Eingaben von Benutzername und Passwort
+		 //für das Test-Szenarium: hans75/1$dampf75 - Kombination		 
+		 
+		 //Überprüfung der Eingabe von Benutzername und Passwort
+		 sel.type("username", "hans75");
+		 sel.type("password", "");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "");
+		 sel.type("password", "1$dampf75");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));		 
+		 
+		 sel.type("username", "xyz");
+		 sel.type("password", "1$dampf75");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "hans75");
+		 sel.type("password", "xyz");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "aaaaaaaaaaaaaaaa");
+		 sel.type("password", "1$dampf75");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "hans75");
+		 sel.type("password", "hhhhhhhhhhh");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 //Wenn Eingaben richtig
+		 sel.type("username", "hans75");
+		 sel.type("password", "1$dampf75");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Willkommen Systemadministrator"));
+		 assertTrue(sel.isTextPresent("Frau Trude Wurst (ADMIN)"));
+		 assertTrue(sel.isTextPresent("Benutzerverwaltung"));
+		 assertTrue(sel.isTextPresent("Daten ändern"));
+		 assertTrue(sel.isTextPresent("Benutzer anzeigen"));
+		 assertTrue(sel.isTextPresent("Zentrenverwaltung"));
+		 assertTrue(sel.isTextPresent("Zentren anzeigen"));
+		 assertTrue(sel.isTextPresent("Zentrum anlegen"));
+		 assertTrue(sel.isTextPresent("Studienverwaltung"));
+		 assertTrue(sel.isTextPresent("Systemadministration"));
+		 assertTrue(sel.isTextPresent("Studienleiter anlegen"));
+			
+		  
+		 //Logout
+		 sel.clickAt("logout_link", "Logout");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 
+  	}//pruefeLoginAdmin()
+	
+	
+	/**
+	 * Pruefen von Benutzername und Passwort des Studienleiters.
+	 * Pruefen, ob SL die für ihn berechtige Seite mit 
+	 * Menuepunkte zu sehen bekommt.
+	 */
+	@Test
+    public void pruefeLoginSL() {
+
+		 //Seite oeffnen
+		 sel.open("http://www.iap.hs-heilbronn.de:8080/randinightly/");
+			 
+		 //Überprüfung der Eingaben des Benutzernamens und Passwortes
+		 //für Test-Szenarium: valentin/1$graeff83 - Kombination		 
+		 
+		 //Überprüfung der Eingabe von Benutzername und Passwort
+		 sel.type("username", "valentin");
+		 sel.type("password", "");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "");
+		 sel.type("password", "1$graeff83");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));		 
+		 
+		 sel.type("username", "xyz");
+		 sel.type("password", "1$graeff83");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "valentin");
+		 sel.type("password", "xyz");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "valentinr");
+		 sel.type("password", "aaaaaaaa");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "aaaaaaaaaaaaaaaa");
+		 sel.type("password", "1$graeff83");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 //Wenn Eingaben richtig
+		 sel.type("username", "valentin");
+		 sel.type("password", "1$graeff83");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Studie auswählen"));
+		 assertTrue(sel.isTextPresent("Frau Birgit Wurst (STUDIENLEITER)"));
+		 
+		 sel.click("link=Aspirin");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Studie ansehen"));
+		 assertTrue(sel.isTextPresent("Frau Birgit Wurst (STUDIENLEITER)"));		 
+		 assertTrue(sel.isTextPresent("Benutzerverwaltung"));
+		 assertTrue(sel.isTextPresent("Daten ändern"));
+		 assertFalse(sel.isTextPresent("Benutzer anzeigen"));
+		 assertTrue(sel.isTextPresent("Studienärzte anzeigen"));
+		 assertTrue(sel.isTextPresent("Zentrenverwaltung"));
+		 assertTrue(sel.isTextPresent("Zentren anzeigen"));
+		 assertFalse(sel.isTextPresent("Zentrum anlegen"));
+		 assertTrue(sel.isTextPresent("Studienverwaltung"));
+		 assertTrue(sel.isTextPresent("Studie ansehen"));
+		 assertFalse(sel.isTextPresent("Systemadministration"));
+		 assertFalse(sel.isTextPresent("Studienleiter anlegen"));
+							 
+         //Logout
+		 sel.clickAt("logout_link", "Logout");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 	
+	}//pruefeLoginSL()
+	
+	
+	/**
+	 * Pruefen von Benutzername und Passwort des Studienarztes.
+	 * Pruefen, ob SA die für ihn berechtige Seite mit 
+	 * Menuepunkte zu sehen bekommt.
+	 */
+	@Test
+    public void pruefeLoginSA() {
+ 
+		 //Seite oeffnen
+		 sel.open("http://www.iap.hs-heilbronn.de:8080/randinightly/");
+			 
+		 //Überprüfung der Eingaben des Benutzernamens und Passwortes
+		 //für Test-Szenarium: frank80/1$hess80 - Kombination
+		 	
+		 //Überprüfung der Eingabe von Benutzername und Passwort
+		 sel.type("username", "frank80");
+		 sel.type("password", "");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "");
+		 sel.type("password", "1$hess80");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));		 
+		 
+		 sel.type("username", "xyz");
+		 sel.type("password", "1$hess80");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "frank80");
+		 sel.type("password", "xyz");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		  
+		 sel.type("username", "frank80");
+		 sel.type("password", "aaaaaaa");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "aaaaaaaaaaaaaaaa");
+		 sel.type("password", "1$hess80");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 //Wenn Eingaben richtig
+		 sel.type("username", "frank80");
+		 sel.type("password", "1$hess80");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Studie auswählen"));
+		 assertTrue(sel.isTextPresent("Herr Hans Wurst"));
+		 
+		 
+		 sel.click("link=Aspirin");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Studie ansehen"));
+		 assertTrue(sel.isTextPresent("Herr Hans Wurst"));		 
+		 assertTrue(sel.isTextPresent("Benutzerverwaltung"));
+		 assertTrue(sel.isTextPresent("Daten ändern"));
+		 assertFalse(sel.isTextPresent("Studienärzte anzeigen"));
+		 assertFalse(sel.isTextPresent("Benutzer anzeigen"));
+		 assertTrue(sel.isTextPresent("Studienverwaltung"));
+		 assertTrue(sel.isTextPresent("Studie ansehen"));
+		 assertTrue(sel.isTextPresent("Patient hinzufügen"));
+		 assertFalse(sel.isTextPresent("Systemadministration"));
+		 assertFalse(sel.isTextPresent("Studienleiter anlegen"));
+		 assertFalse(sel.isTextPresent("Zentrenverwaltung"));
+		 assertFalse(sel.isTextPresent("Zentren anzeigen"));
+		 assertFalse(sel.isTextPresent("Zentrum anlegen"));
+		 		 
+		  //Logout
+		 sel.clickAt("logout_link", "Logout");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 
+    }//pruefeLoginSA()
+	
+	
+	/**
+	 * Pruefen von Benutzername und Passwort des Systemoparators.
+	 * Pruefen, ob Sysop die für ihn berechtige Seite mit 
+	 * Menuepunkte zu sehen bekommt.
+	 */
+	@Test
+    public void pruefeLoginSysop() {
+ 
+		 //Seite oeffnen
+		sel.open("http://www.iap.hs-heilbronn.de:8080/randinightly/");
+			 
+		 //Überprüfung der Eingaben des Benutzernamens und Passwortes
+		 //für Test-Szenarium: nadine/1$zwink83 - Kombination		 
+		 
+		 //Überprüfung der Eingabe von Benutzername und Passwort
+		 sel.type("username", "nadine");
+		 sel.type("password", "");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "");
+		 sel.type("password", "1$zwink83");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));		 
+		 
+		 sel.type("username", "xyz");
+		 sel.type("password", "1$zwink83");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "nadine");
+		 sel.type("password", "xyz");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "nadine");
+		 sel.type("password", "aaaaaaa");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		 sel.type("username", "aaaaaaaaaaaaaaaa");
+		 sel.type("password", "1$zwink83");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler: Bitte Benutzername und Passwort überprüfen"));
+		 
+		//Wenn Eingaben richtig
+		 sel.type("username", "nadine");
+		 sel.type("password", "1$zwink83");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Willkommen Systemadministrator"));
+		 assertTrue(sel.isTextPresent("Herr Hans Maulwurf (SYSOP)"));	
+		 assertTrue(sel.isTextPresent("Benutzerverwaltung"));
+		 assertTrue(sel.isTextPresent("Admins anzeigen"));
+		 assertFalse(sel.isTextPresent("Daten ändern"));
+		 assertFalse(sel.isTextPresent("Studienärzte anzeigen"));
+		 assertFalse(sel.isTextPresent("Benutzer anzeigen"));
+		 assertTrue(sel.isTextPresent("Systemadministration"));
+		 assertTrue(sel.isTextPresent("System sperren"));
+		 assertTrue(sel.isTextPresent("Admin anlegen"));
+		 assertFalse(sel.isTextPresent("Studienleiter anlegen"));
+		 assertFalse(sel.isTextPresent("Zentrenverwaltung"));
+		 assertFalse(sel.isTextPresent("Zentren anzeigen"));
+		 assertFalse(sel.isTextPresent("Zentrum anlegen"));		 
+		 assertFalse(sel.isTextPresent("Studienverwaltung"));		 
+		 assertFalse(sel.isTextPresent("Studie ansehen"));
+		 assertFalse(sel.isTextPresent("Patient hinzufügen"));
+		 
+         //Logout
+		 sel.clickAt("logout_link", "Logout");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 
+	}//pruefeLoginSysop()
+
+	
+	/**
+	 * Pruefen von Benutzername und Passwort des Statistikers.
+	 * Pruefen, ob Stat die für ihn berechtige Seite mit 
+	 * Menuepunkte zu sehen bekommt.
+	 
+	@Test
+    public void pruefeLoginStat() {
+
+	     //Seite oeffnen
+		sel.open("http://www.iap.hs-heilbronn.de:8080/randinightly/");
+			 
+		 //Überprüfung der Eingaben des Benutzernamens und Passwortes
+		 //für Test-Szenarium: statistiker/1$statistiker - Kombination		 
+		 
+		 //Überprüfung der Eingabe von Benutzername und Passwort
+		 sel.type("username", "statistiker");
+		 sel.type("password", "");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler"));
+		 
+		 sel.type("username", "");
+		 sel.type("password", "1$statistiker");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler"));		 
+		 
+		 sel.type("username", "xyz");
+		 sel.type("password", "1$statistiker");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler"));
+		 
+		 sel.type("username", "statistiker");
+		 sel.type("password", "xyz");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler"));
+		 
+		 sel.type("username", "a");
+		 sel.type("password", "1$statistiker");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler"));
+		 
+		 sel.type("username", "statistiker");
+		 sel.type("password", "a");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler"));
+		 
+		 sel.type("username", "aaaaaaaaaaaaaaaa");
+		 sel.type("password", "1$statistiker");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 assertTrue(sel.isTextPresent("Loginfehler"));
+		 
+		 
+		 //Wenn Eingaben richtig
+		 sel.type("username", "statistiker");
+		 sel.type("password", "1$statistiker");
+		 sel.click("Submit");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Studie ansehen"));
+		 assertTrue(sel.isTextPresent("Frau Irene Kaese (STATISTIKER)"));
+		 assertTrue(sel.isTextPresent("Studienverwaltung"));
+		 assertTrue(sel.isTextPresent("Studie ansehen"));
+		 assertFalse(sel.isTextPresent("Patient hinzufügen"));		 
+		 assertFalse(sel.isTextPresent("Benutzerverwaltung"));
+		 assertFalse(sel.isTextPresent("Admins anzeigen"));
+		 assertFalse(sel.isTextPresent("Daten ändern"));
+		 assertFalse(sel.isTextPresent("Studienärzte anzeigen"));
+		 assertFalse(sel.isTextPresent("Benutzer anzeigen"));
+		 assertFalse(sel.isTextPresent("Systemadministration"));
+		 assertFalse(sel.isTextPresent("System sperren"));
+		 assertFalse(sel.isTextPresent("Admin anlegen"));
+		 assertFalse(sel.isTextPresent("Studienleiter anlegen"));
+		 assertFalse(sel.isTextPresent("Zentrenverwaltung"));
+		 assertFalse(sel.isTextPresent("Zentren anzeigen"));
+		 assertFalse(sel.isTextPresent("Zentrum anlegen"));		 
+		 		 
+		 //Logout
+		 sel.clickAt("logout_link", "Logout");
+		 sel.waitForPageToLoad("30000");
+		 assertTrue(sel.isTextPresent("Herzlich Willkommen"));
+		 	
+	}//pruefeLoginStat()
+	*/
+	
+	 @AfterClass
+	 public static void tearDownAfterClass() 
+	   {
+         //Stoppen von Selenium
+	     sel.stop();
+	   }
+	
+}//SeleniumTestLoginRelease2
