a12f1df90954ec91788fe451e40f2fe7885c5e40
freifsch <freifsch@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-11 09:46:17 +0000

    loginname ist jetzt unique
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1369 ccfe945e-4522-0410-b90e-900a8cb55c58

--- db/create_db.sql
+++ db/create_db.sql
@@ -49,7 +49,7 @@ CREATE TABLE Benutzerkonto (
   benutzerkontenID INT UNSIGNED NOT NULL AUTO_INCREMENT,
   Zentrum_zentrumsID INT UNSIGNED NOT NULL,
   Person_personenID INT UNSIGNED NOT NULL,
-  loginname VARCHAR(50) NOT NULL,
+  loginname VARCHAR(50) UNIQUE NOT NULL,
   passwort CHAR(64) NOT NULL,
   rolle VARCHAR(25) NOT NULL,
   erster_login DATETIME NULL,
