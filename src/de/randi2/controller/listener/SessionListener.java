ca25b137caf01c85245b73e13f5d1a893ec51639
kkrupka <kkrupka@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-13 08:51:36 +0000

    der Listener, jetzt auch mit letzterLogin-Support im BenutzerkontoBean
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1404 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/controller/listener/SessionListener.java
+++ src/de/randi2/controller/listener/SessionListener.java
@@ -1,8 +1,16 @@
 package de.randi2.controller.listener;
 
+import java.util.GregorianCalendar;
+
 import javax.servlet.http.HttpSessionAttributeListener;
 import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import de.randi2.datenbank.DatenbankFactory;
+import de.randi2.model.exceptions.BenutzerkontoException;
 import de.randi2.model.fachklassen.beans.BenutzerkontoBean;
+import de.randi2.utility.SystemException;
 
 /**
  * Der SessionListener realisiert einen BenutzerCounter.<br>
@@ -14,80 +22,111 @@ import de.randi2.model.fachklassen.beans.BenutzerkontoBean;
  * @author BTheel [BTheel@stud.hs-heilbronn.de]
  * @version $Id$
  */
-public class SessionListener implements HttpSessionAttributeListener {
+public class SessionListener implements HttpSessionAttributeListener, HttpSessionListener {
 
-    /**
-     * Anzahl der im System angemeldeten Benutzer
-     */
-    private static int counter = 0;
+	/**
+	 * Anzahl der im System angemeldeten Benutzer
+	 */
+	private static int counter = 0;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.servlet.http.HttpSessionAttributeListener#attributeAdded(javax.servlet.http.HttpSessionBindingEvent)
-     */
-    /**
-     * Wird ein {@link BenutzerkontoBean} unter dem Namen 'aBenutzer' (ein
-     * Benutzer loggt sich am system ein) an die Session gebunden, wird der
-     * Counter um 1 erhoeht.
-     * 
-     * @param be
-     *            Durch das Binding automatisch ausgeloeste Event
-     * @see javax.servlet.http.HttpSessionAttributeListener#attributeAdded(javax.servlet.http.HttpSessionBindingEvent)
-     */
-    public void attributeAdded(HttpSessionBindingEvent be) {
-        if (be.getValue() instanceof BenutzerkontoBean
-                && be.getName().equals("aBenutzer")) {
-            erhoeheCounter();
-        }
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpSessionAttributeListener#attributeAdded(javax.servlet.http.HttpSessionBindingEvent)
+	 */
+	/**
+	 * Wird ein {@link BenutzerkontoBean} unter dem Namen 'aBenutzer' (ein
+	 * Benutzer loggt sich am system ein) an die Session gebunden, wird der
+	 * Counter um 1 erhoeht.
+	 * 
+	 * @param be
+	 *            Durch das Binding automatisch ausgeloeste Event
+	 * @see javax.servlet.http.HttpSessionAttributeListener#attributeAdded(javax.servlet.http.HttpSessionBindingEvent)
+	 */
+	public void attributeAdded(HttpSessionBindingEvent be) {
+		if (be.getValue() instanceof BenutzerkontoBean && be.getName().equals("aBenutzer")) {
+			erhoeheCounter();
+		}
+	}
 
-    /**
-     * Wird das BenutzerkontoBean 'aBenutzer' aus der Session entfernt (der
-     * Benutzer wird ausgeloggt), so wird der BenutzerCounter um 1 erniedriegt.
-     * 
-     * @param be
-     *            Durch das Entfernen des Attributes automatisch ausgeloeste
-     *            Event
-     * @see javax.servlet.http.HttpSessionAttributeListener#attributeRemoved(javax.servlet.http.HttpSessionBindingEvent)
-     */
-    public void attributeRemoved(HttpSessionBindingEvent be) {
-        if (be.getValue() instanceof BenutzerkontoBean
-                && be.getName().equals("aBenutzer")) {
-            erniedrigeCounter();
-        }
+	/**
+	 * Wird das BenutzerkontoBean 'aBenutzer' aus der Session entfernt (der
+	 * Benutzer wird ausgeloggt), so wird der BenutzerCounter um 1 erniedriegt.
+	 * 
+	 * @param be
+	 *            Durch das Entfernen des Attributes automatisch ausgeloeste
+	 *            Event
+	 * @see javax.servlet.http.HttpSessionAttributeListener#attributeRemoved(javax.servlet.http.HttpSessionBindingEvent)
+	 */
+	public void attributeRemoved(HttpSessionBindingEvent be) {
+		if (be.getValue() instanceof BenutzerkontoBean && be.getName().equals("aBenutzer")) {
+			erniedrigeCounter();
+		}
 
-    }
+	}
 
-    /**
-     * Wird aufgerufen, wenn ein Attribut, welches an einer Session gebunden ist, ersetzt wird.
-     * @param be ausgeloestes Event
-     * @see javax.servlet.http.HttpSessionAttributeListener#attributeReplaced(javax.servlet.http.HttpSessionBindingEvent)
-     */
-    public void attributeReplaced(HttpSessionBindingEvent be) {
+	/**
+	 * Wird aufgerufen, wenn ein Attribut, welches an einer Session gebunden
+	 * ist, ersetzt wird.
+	 * 
+	 * @param be
+	 *            ausgeloestes Event
+	 * @see javax.servlet.http.HttpSessionAttributeListener#attributeReplaced(javax.servlet.http.HttpSessionBindingEvent)
+	 */
+	public void attributeReplaced(HttpSessionBindingEvent be) {
 
-    }
+	}
 
-    /**
-     * Erhoeht den Benutzercounter um 1
-     */
-    private synchronized void erhoeheCounter() {
-        counter++;
-    }
+	/**
+	 * Erhoeht den Benutzercounter um 1
+	 */
+	private synchronized void erhoeheCounter() {
+		counter++;
+	}
 
-    /**
-     * Reduziert den Benutzercounter um 1
-     */
-    private synchronized void erniedrigeCounter() {
-        counter--;
-    }
+	/**
+	 * Reduziert den Benutzercounter um 1
+	 */
+	private synchronized void erniedrigeCounter() {
+		counter--;
+	}
 
-    /**
-     * Liefert die Anzahl der aktuell angemeldetet Benutzer am System
-     * 
-     * @return Anzahl der angemeldeten Benutzer
-     */
-    public static synchronized int getCounter() {
-        return counter;
-    }
+	/**
+	 * Liefert die Anzahl der aktuell angemeldetet Benutzer am System
+	 * 
+	 * @return Anzahl der angemeldeten Benutzer
+	 */
+	public static synchronized int getCounter() {
+		return counter;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpSessionListener#sessionCreated(javax.servlet.http.HttpSessionEvent)
+	 */
+	public void sessionCreated(HttpSessionEvent arg0) {
+	}
+
+	/**
+	 * Wenn die Session verstoert wird und ein Benutzer an die Session gebunden
+	 * ist, so wird die
+	 * {@link BenutzerkontoBean#setLetzterLogin(GregorianCalendar)} mit der
+	 * aktuellen Zeit gespeichert und in der Db gespeichert
+	 * 
+	 * @see javax.servlet.http.HttpSessionListener#sessionDestroyed(javax.servlet.http.HttpSessionEvent)
+	 */
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+		BenutzerkontoBean aBenutzer = (BenutzerkontoBean) arg0.getSession().getAttribute("aBenutzer");
+		if (aBenutzer != null) {
+			try {
+				aBenutzer.setLetzterLogin(new GregorianCalendar());
+				DatenbankFactory.getAktuelleDBInstanz().schreibenObjekt(aBenutzer);
+				System.err.println("Konto aktualisiert");
+			} catch (Exception e) {
+				e.printStackTrace(); // FIXME Entfernen --BTheel
+				new SystemException("Setzten des letzten Logins fehlgeschlagen");
+			}
+		}
+	}
 }
