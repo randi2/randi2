75c1f5d46ee3e4aceb102ebe2f14161cac2f305f
kchruscz <kchruscz@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-13 08:38:08 +0000

    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1401 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/controller/StudienzentrumServlet.java
+++ src/de/randi2/controller/StudienzentrumServlet.java
@@ -9,20 +9,86 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
+import de.randi2.controller.StudieServlet.anfrage_id;
+import de.randi2.controller.StudieServlet.requestParameter;
 import de.randi2.datenbank.DatenbankFactory;
 import de.randi2.model.fachklassen.beans.StudieBean;
 import de.randi2.utility.Jsp;
 
 /**
+ * Zentren zu Studie verwalten: Kathy + Thomas
  * 
- * @author Thomas Ganszky
- *
+ * Diese Klasse repraesentiert das STUDIEZENTRUMSERVLET, welches Aktionen von
+ * Zentren zu Studie hinzufügt und verwaltet.
+ * 
+ * @version $Id:
+ * @author Thomas Ganszky, Katharina Chruscz
+ * 
  */
 public class StudienzentrumServlet extends javax.servlet.http.HttpServlet {
 
+
+
 	/**
-	 * Serial Version
+	 * Enhaelt die Parameternamen, die in dem Request gesetzt werden koennen
+	 */
+	public static enum requestParameter {
+		// TODO
+	}
+
+	/**
+	 * Diese Methode nimmt HTTP-POST-Request gemaess HTTP-Servlet Definition
+	 * entgegen. Hier werden Anfragen verarbeitet, die Zentren betreffen.
+	 * 
+	 * @param request
+	 *            Der Request fuer das Servlet.
+	 * @param response
+	 *            Der Response Servlet.
+	 * @throws IOException
+	 *             Falls Fehler in den E/A-Verarbeitung.
+	 * @throws ServletException
+	 *             Falls Fehler in der HTTP-Verarbeitung auftreten.
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request,
+	 *      HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO
+		/*
+		 * String id = (String) request.getParameter("anfrage_id"); String
+		 * idAttribute = (String) request.getAttribute("anfrage_id"); if
+		 * (idAttribute != null) { id = idAttribute; }
+		 * Logger.getLogger(this.getClass()).debug(id);
+		 * 
+		 * if
+		 * (request.getAttribute(DispatcherServlet.requestParameter.ANFRAGE_Id
+		 * .name()) == anfrage_id.STUDIE_AUSWAEHLEN) { // Die
+		 * studie_auswaehlen.jsp soll angezeigt werden. StudieBean leeresObjekt =
+		 * new StudieBean(); leeresObjekt.setFilter(true); Vector<StudieBean>
+		 * listeStudien = DatenbankFactory
+		 * .getAktuelleDBInstanz().suchenObjekt(leeresObjekt);
+		 * System.out.println(listeStudien.size());
+		 * System.out.println(requestParameter.LISTE_DER_STUDIEN.name());
+		 * request.setAttribute(requestParameter.LISTE_DER_STUDIEN.name(),
+		 * listeStudien);
+		 * request.getRequestDispatcher(Jsp.STUDIE_AUSWAEHLEN).forward(request,
+		 * response); }
+		 */
+	}
+	
+	/**
+	 * Default Serial
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Konstruktor.
+	 * 
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public StudienzentrumServlet() {
+		super();
+	}
 }
