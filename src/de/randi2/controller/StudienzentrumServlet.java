4d485b1beb43dafd423de3c6477c44eee994da47
tganszky <tganszky@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-13 08:00:39 +0000

    Servlet um Zentren zu einer Studie zu verwalten
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1399 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/controller/StudienzentrumServlet.java
@@ -0,0 +1,28 @@
+package de.randi2.controller;
+
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import de.randi2.datenbank.DatenbankFactory;
+import de.randi2.model.fachklassen.beans.StudieBean;
+import de.randi2.utility.Jsp;
+
+/**
+ * 
+ * @author Thomas Ganszky
+ *
+ */
+public class StudienzentrumServlet extends javax.servlet.http.HttpServlet {
+
+	/**
+	 * Serial Version
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
