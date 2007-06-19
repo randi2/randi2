cdc47847fbb4ecf8f72fb382ef217f52fbebd006
freifsch <freifsch@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-19 08:52:00 +0000

    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1465 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/datenbank/ConnectionFactory.java
+++ src/de/randi2/datenbank/ConnectionFactory.java
@@ -8,6 +8,8 @@ import org.apache.log4j.Logger;
 import org.logicalcobwebs.proxool.ProxoolException;
 import org.logicalcobwebs.proxool.configuration.JAXPConfigurator;
 
+import sun.nio.cs.ext.DoubleByteEncoder;
+
 import com.meterware.httpunit.HttpUnitUtils;
 
 import de.randi2.datenbank.exceptions.DatenbankExceptions;
@@ -60,11 +62,16 @@ public class ConnectionFactory {
 	 * Baut Verbindung zur Datenbank auf
 	 * 
 	 * @return Connectionobjekt welches Zugriff auf die Datenbank ermoeglicht.
+	 * @throws DatenbankExceptions 
 	 * @throws SQLException Falls ein Fehler beim Verbindungsaufbau auftritt.
 	 */
-	public Connection getConnection() throws SQLException {
+	public Connection getConnection() throws DatenbankExceptions {
 		if(con==null) {
-			this.con = DriverManager.getConnection("proxool.randi2");
+			try {
+				this.con = DriverManager.getConnection("proxool.randi2");
+			} catch (SQLException e) {
+				throw new DatenbankExceptions(DatenbankExceptions.CONNECTION_ERR);
+			}
 		}		
 		return con;
 	}
@@ -73,12 +80,17 @@ public class ConnectionFactory {
 	 * Trennt Verbindung zur Datenbank.
 	 * 
 	 * @param con das Connection Objekt.
+	 * @throws DatenbankExceptions 
 	 * @throws SQLException Falls ein Fehler bei der Verbindungstrennung auftritt.
 	 */
-	public void closeConnection() throws SQLException {
-		if (con != null && !con.isClosed()) {
-			con.close();
-			con=null;
+	public void closeConnection() throws DatenbankExceptions {
+		try {
+			if (con != null && !con.isClosed()) {
+				con.close();
+				con=null;
+			}
+		} catch (SQLException e) {
+			throw new DatenbankExceptions(DatenbankExceptions.CONNECTION_ERR);
 		}		
 	}
 	
