341365c0b61ee73f860ce75f2dbd8e835222e25a
afreudli <afreudli@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-27 08:01:39 +0000

    BenutzerSuchen
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1723 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/model/fachklassen/beans/BenutzerSuchenBean.java
@@ -0,0 +1,99 @@
+package de.randi2.model.fachklassen.beans;
+
+import de.randi2.datenbank.Filter;
+import de.randi2.model.exceptions.BenutzerException;
+import de.randi2.model.exceptions.PersonException;
+import de.randi2.utility.NullKonstanten;
+
+public class BenutzerSuchenBean extends Filter {
+
+	/**
+	 * Die ID des zugehoerigen Benutzers. Fremdschlueschel zum PersonBean. 1:1
+	 * Beziehung
+	 */
+	private long benutzerId = NullKonstanten.NULL_LONG;
+	
+	/**
+	 * 
+	 */
+	private long zentrumId=NullKonstanten.NULL_LONG;
+	/**
+	 * 
+	 */
+	private long personId=NullKonstanten.NULL_LONG;
+	
+	
+	private String vorname=null;
+	private String nachname=null;
+	private String email=null;
+	private String loginname=null;
+	private String institut=null;
+	
+	public BenutzerSuchenBean(long benutzerId, long zentrumId, long personId, String vorname, String nachname, String email, String loginname, String institut) {
+		super();
+		this.benutzerId = benutzerId;
+		this.zentrumId = zentrumId;
+		this.personId = personId;
+		this.vorname = vorname;
+		this.nachname = nachname;
+		this.email = email;
+		this.loginname = loginname;
+		this.institut = institut;
+	}
+	public long getBenutzerId() {
+		return benutzerId;
+	}
+	public void setBenutzerId(long benutzerId) {
+		this.benutzerId = benutzerId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getInstitut() {
+		return institut;
+	}
+	public void setInstitut(String institut) {
+		this.institut = institut;
+	}
+	public String getLoginname() {
+		return loginname;
+	}
+	public void setLoginname(String loginname) {
+		this.loginname = loginname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	public long getPersonId() {
+		return personId;
+	}
+	public void setPersonId(long personId) {
+		this.personId = personId;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public long getZentrumId() {
+		return zentrumId;
+	}
+	public void setZentrumId(long zentrumId) {
+		this.zentrumId = zentrumId;
+	}
+	@Override
+	public void validate() throws BenutzerException {
+		if(benutzerId==NullKonstanten.NULL_LONG||zentrumId==NullKonstanten.NULL_LONG||personId==NullKonstanten.NULL_LONG||vorname==null||nachname==null||email==null||loginname==null||institut==null)
+		{
+			throw new PersonException("Daten ungueltig");
+		}
+	}
+
+}
