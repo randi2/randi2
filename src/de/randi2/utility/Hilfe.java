611241c27e1684d2cba0614e3b6930ea0906d368
kchruscz <kchruscz@ccfe945e-4522-0410-b90e-900a8cb55c58>
2007-06-30 18:44:12 +0000

    Für die meisten Seiten wurde eine Hilfe eingetragen. Noch funktionieren nicht alle Seiten!
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/randi/trunk/RANDI2@1838 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/utility/Hilfe.java
+++ src/de/randi2/utility/Hilfe.java
@@ -23,33 +23,54 @@ public class Hilfe {
 	private static String[][] unterseiten = new String[][] {
 
 			{
-				JspTitel.STUDIE_AUSWAEHLEN,
-					"Hier kann die Studie ausgew&auml;hlt werden!<br><br>Unter allen zur Verf&uuml;gung stehenden Studien kann gefiltert werden." 
-			},
-			{ 
-				JspTitel.STUDIE_ANSEHEN, "Hier kann die ausgew&auml;lte Studie angesehen werden!" 
-			}, 
-			
+					JspTitel.STUDIE_AUSWAEHLEN,
+					"Hier können Sie durch Anklicken einer Studie diese ausw&auml;len. <br> Sie müssen Sie eine Studie ausw&auml;len, um weiter zu kommen. <br><br> Mit der Filterfunktion <img src=\"images\\find.png\"> k&ouml;nnen Sie unter allen verf&uuml;gbaren Studien filtern." },
+			{ JspTitel.STUDIE_ANSEHEN,
+					"Die ausgew&auml;lte Studie wird mit ihren Details angezeigt!" },
+
 			{
-				JspTitel.STUDIENARZTE_LISTE, "Hier werden alle Studien&auml;rzte, die an der aktuell ausgew&auml;lten Studie beteiligt sind. <br><br> Unter allen zur Verf&uuml;gung stehenden Studien&auml;rzten kann gefiltert werden."
-			}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+					JspTitel.DATEN_AENDERN,
+					"Hier k&ouml;nnen Sie Ihr pers&ouml;nlichen Daten &auml;ndern! <br><br> Die Felder, die mit * gekennzeichnet sind, sind Pflichtfelder, d.h. diese m&uuml;ssen ausgef&uuml;llt werden! <br> Die restlichen Felder sind freiwillige Angaben. <br> Vor- und Nachname, Geschlecht und Ihre E-Mailadresse k&ouml;nnen Sie nicht &auml;ndern!" },
+			{
+					JspTitel.PATIENT_HINZUFUEGEN,
+					"Hier kann ein neuer Patient hinzugef&uuml;gt werden. <br><br> Sie m&uuml;ssen alle Felder ausf&uuml;llen, um einen neuen Patienten hinzufügen zu können.  <br> Beim Feld \"K&ouml;rperoberfl&auml;che verwenden Sie bitte einen Punkt (.) und kein Komma (,). " },
+
+			{
+					JspTitel.STUDIENARM_ANZEIGEN,
+					"Der ausgew&auml;lte Studienarm wird hier angezeigt. Sie können durch Anklicken des Buttons \"Studienarm beenden\" den Studienarm beenden." },
+			{
+					JspTitel.STUDIE_ANLEGEN,
+					"Hier k&ouml;nnen Sie eine neue Studie anlegen. Alle Felder m&uuml;ssen ausgef&uuml;lt werden! Durch \"Best&auml;tigen\" k&ouml;nnen Sie die neue Studie speichern." },
+			{
+					JspTitel.ZENTRUM_ANZEIGEN,
+					"Hier wird das ausgew&auml;lte Zentrum mit seinen Details dargestellt. <br><br> &Uuml;ber \"zur&uuml;ck zu allen Zentren\" gelangt man zur Ausgangsseite zur&uuml;ck. " },
+			{ JspTitel.ZENTRUM_STUDIE_ZUORDNEN,
+					"Hier kann ein Zentrum zu einer Studie hinzugef&uuml;gt werden!" },
+			{ JspTitel.SIMULATION, "Hier wird eine Simualtion dargestellt." },
+			{ JspTitel.STUDIENARM_ANZEIGEN,
+					"Hier wird der ausw&auml;lte Studienarm mit seinen Details angezeigt." },
+			{ JspTitel.STATISTIK_ANZEIGEN, "Hier wird die Statistik angezeigt." },
+			{ JspTitel.STUDIE_FORTSETZEN,
+					"Hier k&ouml;nnen Sie die ausgew&auml;lte Studie fortsetzen." },
+
+			{ JspTitel.SYSTEM_SPERREN,
+					"Um das System sperren zu k&ouml;nnen, m&uuml;ssen Sie einen Grund angeben!" },
+			{
+					JspTitel.ADMIN_ANLEGEN,
+					"Hier k&ouml;nnen Sie einen neuen Administrator anlegen. <br> Alle Felder, die mit * makiert sind, sind Pflichtfelder, d.h. diese m&uuml;ssen ausgef&uuml;lt werden!" },
+			{
+					JspTitel.,
+					"Hier werden alle im System eingetragenen Benutzer angezeigt. <br> <br> Mit der Filterfunktion <img src=\"images\\find.png\" k&ouml;nnen Sie je nach Vorname, Nachname, Loginname, E-Mail oder Institut einzeln oder nach Kombinationen daraus filtern. <br><br> Mit der Aktion \"anzeigen\" k&ouml;nnen Sie sich die Benutzerdetails der jeweiligen Benutzers anzeigen lassen. <br> Mit der Aktion \"sperren\" bzw. \"entsperren\" k&ouml;nnen Sie den jeweiligen Benutzer im System sperren bzw. entsperren.  " },
+					
+					{ 
+						JspTitel.BENUTZER_ANZEIGEN_ADMIN, "Hier werden alle Details des ausgew&auml;lten Benutzer dargestellt. Diese k&ouml;nnen nicht ver&auml;ndert werden!"
+					},
+					{ 
+						JspTitel.BENUTZER_SPERREN, "Um einen Benutzer sperren zu k&ouml;nnen, muss ein Grund hierf&uuml;r angegeben werden!"
+					},
+					{
+						JspTitel.ZENTRUM_AENDERN, "Hier k&ouml;nnen die Daten des ausgew&auml;lten Zentrums ge&auml;ndert werden. <br><br> Die mit '*' gekennzeichneten Felder sind Pflichtfelder, d.h. diese m&uuml;ssen ausgef&uuml;lt sein! "
+					}
 	};
 
 	/**
