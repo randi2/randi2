aa88e445a634f4580b65edc27a88dd969eb5fb97
jthoenes <jthoenes@ccfe945e-4522-0410-b90e-900a8cb55c58>
2008-04-14 13:54:46 +0000

    Some persistent Objects.
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/trunk@2116 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/model/Center.java
@@ -0,0 +1,13 @@
+package de.randi2.model;
+
+import java.util.HashMap;
+
+public class Center extends PersistentDomainObject {
+
+	@Override
+	public HashMap<String, String> getFilterMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/de/randi2/model/Trial.java
@@ -0,0 +1,111 @@
+package de.randi2.model;
+
+import java.io.File;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Vector;
+
+import javax.persistence.Entity;
+
+import de.randi2.model.exceptions.StudieException;
+import de.randi2.model.fachklassen.Studie.Status;
+import de.randi2.model.fachklassen.beans.BenutzerkontoBean;
+import de.randi2.model.fachklassen.beans.StrataBean;
+import de.randi2.model.fachklassen.beans.StudienarmBean;
+import de.randi2.model.fachklassen.beans.ZentrumBean;
+import de.randi2.randomisation.Randomisation;
+import de.randi2.utility.NullKonstanten;
+
+@Entity
+public class Trial extends PersistentDomainObject {
+
+	/**
+	 * Enumeration Status der Studie
+	 */
+	public static enum TrialStatus {
+
+		/**
+		 * Status aktiv
+		 */
+		AKTIV("aktiv"),
+		/**
+		 * Status in Vorbereitung
+		 */
+		INVORBEREITUNG("in Vorbereitung"),
+		/**
+		 * Status Studie beendet
+		 */
+		BEENDET("beendet"),
+		/**
+		 * Studie pausiert
+		 */
+		PAUSE("pausiert");
+
+		/**
+		 * Den Status als String.
+		 */
+		private String status = null;
+
+		/**
+		 * Weist den String dem tatsaechlichen Status zu.
+		 * 
+		 * @param status
+		 *            Der Parameter enthaelt den Status-String.
+		 */
+		private TrialStatus(String status) {
+			this.status = status;
+		}
+
+		/**
+		 * Gibt den Status als String zurueck.
+		 * 
+		 * @return Status der Studie
+		 */
+
+		public String toString() {
+			return this.status;
+		}
+
+		/**
+		 * Ueberfuehrt einen String in das entsprechende Status-Element
+		 * 
+		 * @param status
+		 *            Status der Studie
+		 * @return Status in Form eines Enumelementes
+		 * @throws StudieException
+		 *             StudieException
+		 */
+		public static TrialStatus parseStatus(String status) throws StudieException {
+
+			for (TrialStatus aStatus : TrialStatus.values()) {
+				if (status.equals(aStatus.toString())) {
+					return aStatus;
+				}
+			}
+			throw new StudieException(StudieException.STATUS_UNGUELTIG);
+		}
+	}
+	
+	
+	private String name;
+	private String description;
+	private GregorianCalendar startDate;
+	private GregorianCalendar endDate;
+	
+	private File studienprotokollPfad = null;
+	
+	private Person leader = null;
+	private Center leadingCenter = null;
+	private List<Center> centers = null;
+	
+	
+	private TrialStatus status = null;
+	
+	@Override
+	public HashMap<String, String> getFilterMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
