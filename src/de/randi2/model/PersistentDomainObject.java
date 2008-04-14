edb8c84be109e2321530749235ae194db0935d44
jthoenes <jthoenes@ccfe945e-4522-0410-b90e-900a8cb55c58>
2008-04-14 13:41:41 +0000

    Initial Spring Configuration, Base Persitent JPA/Hibernate Class.
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/trunk@2115 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/model/PersistentDomainObject.java
@@ -0,0 +1,55 @@
+package de.randi2.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public abstract class PersistentDomainObject implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Version
+	private Integer version;
+	
+	private boolean filter;
+	
+	protected PersistentDomainObject(){
+		this.filter = false;
+	}
+	
+	protected PersistentDomainObject(boolean _filter){
+		this.filter = filter;
+	}
+	
+	public abstract HashMap<String, String> getFilterMap();
+
+	public Long getId() {
+		return id;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public boolean isFilter() {
+		return filter;
+	}
+
+	private void setId(Long _id) {
+		this.id = _id;
+	}
+
+	private void setVersion(Integer _version) {
+		this.version = _version;
+	}	
+	
+	
+}
