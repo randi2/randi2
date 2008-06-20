c8dfc18d5b2fab52372d7ee507c9daea6f339039
dschrimpf <dschrimpf@ccfe945e-4522-0410-b90e-900a8cb55c58>
2008-06-20 14:22:53 +0000

    the name of the center is now unique
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/trunk@2267 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/model/Center.java
+++ src/de/randi2/model/Center.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
@@ -22,6 +23,7 @@ public class Center extends AbstractDomainObject{
 
 	public final static int MAX_LENGTH_POSTCODE = 10;
 	
+	@Column(unique=true)
 	private String name = "";
 	
 	private String street = "";
