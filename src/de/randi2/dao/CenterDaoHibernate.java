f74149242a44beed04a5aa3eb2497777c3cce3ac
dschrimpf <dschrimpf@ccfe945e-4522-0410-b90e-900a8cb55c58>
2008-06-20 13:44:16 +0000

    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/trunk@2263 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/dao/CenterDaoHibernate.java
+++ src/de/randi2/dao/CenterDaoHibernate.java
@@ -2,7 +2,7 @@ package de.randi2.dao;
 
 import de.randi2.model.Center;
 
-public class CenterDaoHibernate extends AbstractDaoHibernate<Center> {
+public class CenterDaoHibernate extends AbstractDaoHibernate<Center> implements CenterDao {
 
 	@Override
 	public Class<Center> getModelClass() {
