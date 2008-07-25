dbf8f8efcbf9465eadb98d27377d29d6903df848
lplotni <lplotni@ccfe945e-4522-0410-b90e-900a8cb55c58>
2008-07-25 11:21:53 +0000

    Totally forgotten password confirmation during registration process & some lookAndFeel improvements.
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/trunk@2278 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/de/randi2/jsf/pages/RegisterPage.java
+++ src/de/randi2/jsf/pages/RegisterPage.java
@@ -1,3 +1,17 @@
+ /* This file is part of RANDI2.
+ * 
+ * RANDI2 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * RANDI2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * RANDI2. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.randi2.jsf.pages;
 
 import java.io.IOException;
@@ -6,7 +20,6 @@ import java.util.Vector;
 
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
-import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
 
 import com.icesoft.faces.context.effects.Effect;
@@ -22,20 +35,6 @@ import de.randi2.model.enumerations.Gender;
  * </p>
  * 
  * @author Lukasz Plotnicki <lplotni@users.sourceforge.net>
- * 
- * This file is part of RANDI2.
- * 
- * RANDI2 is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- * 
- * RANDI2 is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * RANDI2. If not, see <http://www.gnu.org/licenses/>.
  */
 public class RegisterPage {
 
---
+++ src/de/randi2/jsf/validators/PasswordConfirmation.java
@@ -0,0 +1,63 @@
+/* This file is part of RANDI2.
+ * 
+ * RANDI2 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * RANDI2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * RANDI2. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.randi2.jsf.validators;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import de.randi2.model.AbstractDomainObject;
+import de.randi2.model.Login;
+import de.randi2.model.exceptions.ValidationException;
+
+/**
+ * <p>
+ * This class should be used for password confirmation - to check if the second
+ * password is just like firstone.
+ * </p>
+ * 
+ * @author Lukasz Plotnicki <lplotni@users.sourceforge.net>
+ * 
+ */
+public class PasswordConfirmation implements Validator, Serializable {
+
+	private static final long serialVersionUID = 3209827418681297534L;
+
+	@Override
+	public void validate(FacesContext arg0, UIComponent arg1, Object arg2)
+			throws ValidatorException {
+
+		// Application application = arg0.getApplication();
+
+		Login login = (Login) arg1.getAttributes().get("login");
+
+		// String messageBundle = application.getMessageBundle();
+		// Locale locale = arg0.getViewRoot().getLocale();
+		//		
+		// ResourceBundle rb = ResourceBundle.getBundle(messageBundle, locale);
+
+		if (!login.getPassword().equals(arg2)) {
+			String message = "The second passwort doesn't match the first one.";
+			throw new ValidatorException(new FacesMessage(
+					FacesMessage.SEVERITY_ERROR, message, null));
+		}
+
+	}
+
+}
