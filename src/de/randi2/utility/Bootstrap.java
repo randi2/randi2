a122acd3ae3cf4ace7401ecd4990f4d72ee178e8
dschrimpf <dschrimpf@ccfe945e-4522-0410-b90e-900a8cb55c58>
2008-11-27 18:23:04 +0000

    simple bootstrap (initialize database and insert test data)
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/trunk@2363 ccfe945e-4522-0410-b90e-900a8cb55c58

---
+++ src/de/randi2/utility/Bootstrap.java
@@ -0,0 +1,137 @@
+package de.randi2.utility;
+
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.security.providers.anonymous.AnonymousAuthenticationToken;
+
+import de.randi2.dao.LoginDaoHibernate;
+import de.randi2.dao.TrialSiteDaoHibernate;
+import de.randi2.model.Login;
+import de.randi2.model.Person;
+import de.randi2.model.Role2;
+import de.randi2.model.TrialSite;
+import de.randi2.model.enumerations.Gender;
+import de.randi2.utility.security.RolesAndRights;
+
+/**
+ * This class insert two user and two trial sites (name: DKFZ and NCT). 
+ * 
+ * Thirst user:
+ * <ul>
+ * 	<li>role: Administrator</li>
+ * 	<li>login: admin@test.de</li>
+ * 	<li>password: 1$heidelberg</li>
+ *</ul>
+ *
+ * Second user:
+ * <ul>
+ * 	<li>role: Investigator</li>
+ * 	<li>login: user@test.de</li>
+ * 	<li>password: 1$heidelberg</li>
+ *</ul>
+ *  
+ * 
+ * 
+ * @author Daniel Schrimpf <dschrimpf@users.sourceforge.net>
+ * 
+ */
+public class Bootstrap {
+
+	private HibernateTemplate template;
+	private RolesAndRights rolesAndRights;
+	private LoginDaoHibernate loginDao;
+	private TrialSiteDaoHibernate trialSiteDao;
+
+	public void init() {
+		Person adminP = new Person();
+		adminP.setFirstname("Max");
+		adminP.setSurname("Mustermann");
+		adminP.setEMail("admin@test.de");
+		adminP.setPhone("1234567");
+		adminP.setGender(Gender.MALE);
+
+		Login adminL = new Login();
+		adminL.setPassword("1$heidelberg");
+		adminL.setRegistrationDate(new GregorianCalendar());
+		adminL.setPerson(adminP);
+		adminL.setPrefLocale(Locale.GERMANY);
+		adminL.setUsername(adminP.getEMail());
+		adminL.addRole(Role2.ROLE_ADMIN);
+		template.saveOrUpdate(adminL);
+
+		TrialSite trialSite = new TrialSite();
+		trialSite.setCity("Heidelberg");
+		trialSite.setCountry("Germany");
+		trialSite.setName("DKFZ");
+		trialSite.setPostcode("69120");
+		trialSite.setStreet("INF");
+		trialSite.setPassword("1$heidelberg");
+		trialSite.setContactPerson(adminP);
+
+		rolesAndRights.registerPerson(adminL);
+		rolesAndRights.grantRigths(adminL, trialSite);
+
+		template.save(trialSite);
+		adminP.setTrialSite(trialSite);
+		template.saveOrUpdate(adminP);
+		rolesAndRights.grantRigths(trialSite, trialSite);
+
+		AnonymousAuthenticationToken authToken = new AnonymousAuthenticationToken(
+				"anonymousUser", adminL, adminL.getAuthorities());
+		// Perform authentication
+		SecurityContextHolder.getContext().setAuthentication(authToken);
+		SecurityContextHolder.getContext().getAuthentication()
+				.setAuthenticated(true);
+
+		Person userP = new Person();
+		userP.setFirstname("Maxi");
+		userP.setSurname("Musterfrau");
+		userP.setEMail("user@test.de");
+		userP.setPhone("1234567");
+		userP.setGender(Gender.FEMALE);
+		userP.setTrialSite(trialSite);
+
+		Login userL = new Login();
+		userL.setPassword("1$heidelberg");
+		userL.setRegistrationDate(new GregorianCalendar());
+		userL.setPerson(userP);
+		userL.setPrefLocale(Locale.GERMANY);
+		userL.setUsername(userP.getEMail());
+		userL.addRole(Role2.ROLE_INVESTIGATOR);
+
+		loginDao.save(userL);
+
+		TrialSite trialSite1 = new TrialSite();
+		trialSite1.setCity("Heidelberg");
+		trialSite1.setCountry("Germany");
+		trialSite1.setName("NCT");
+		trialSite1.setPostcode("69120");
+		trialSite1.setStreet("INF");
+		trialSite1.setPassword("1$heidelberg");
+		trialSite1.setContactPerson(adminP);
+
+		trialSiteDao.save(trialSite1);
+	}
+
+	public Bootstrap() {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"classpath:/META-INF/spring.xml");
+		template = (HibernateTemplate) ctx.getBean("hibernateTemplate");
+		loginDao = (LoginDaoHibernate) ctx.getBean("loginDAO");
+		rolesAndRights = (RolesAndRights) ctx.getBean("rolesAndRights");
+		trialSiteDao = (TrialSiteDaoHibernate) ctx.getBean("centerDAO");
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Bootstrap();
+	}
+
+}
