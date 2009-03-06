824de23d65b5e1da9fb45aa67c78036bc4ba43ca
jthoenes <jthoenes@ccfe945e-4522-0410-b90e-900a8cb55c58>
2009-03-06 16:01:18 +0000

    MINOR: Small Changes for unused imports
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/trunk@2464 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/test/java/de/randi2/utility/ReflectionUtilTest.java
+++ src/test/java/de/randi2/utility/ReflectionUtilTest.java
@@ -6,13 +6,10 @@ package de.randi2.utility;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
