0bc0cf015ee088adc351724ad536ded6e20c789a
dschrimpf <dschrimpf@ccfe945e-4522-0410-b90e-900a8cb55c58>
2009-03-07 10:57:23 +0000

    change template.saveOrUpdate to template.persist
    git-svn-id: https://randi2.svn.sourceforge.net/svnroot/randi2/trunk@2486 ccfe945e-4522-0410-b90e-900a8cb55c58

--- src/main/java/de/randi2/dao/AbstractDaoHibernate.java
+++ src/main/java/de/randi2/dao/AbstractDaoHibernate.java
@@ -29,7 +29,7 @@ public abstract class AbstractDaoHibernate<E extends AbstractDomainObject> imple
 	@Transactional(propagation=Propagation.REQUIRED, rollbackFor=RuntimeException.class)
 	public void save(E object){
 		if (((AbstractDomainObject)object).getId()==AbstractDomainObject.NOT_YET_SAVED_ID){
-			template.saveOrUpdate(object);
+			template.persist(object);
 		}else template.merge(object);
 	}
 	
