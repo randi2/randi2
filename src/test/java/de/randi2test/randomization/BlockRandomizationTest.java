80ea335e68130928869007c19a59e63547bc46e4
Johannes Thönes <johannes.thoenes@googlemail.com>
2009-03-20 10:35:40 +0100

    4 Probands Block-Randomization Fixing IntegerIterator Adding upto static method for IntegerInterator convienience
    Signed-off-by: Johannes Thönes <johannes.thoenes@googlemail.com>

--- src/test/java/de/randi2test/randomization/BlockRandomizationTest.java
+++ src/test/java/de/randi2test/randomization/BlockRandomizationTest.java
@@ -4,11 +4,14 @@
  */
 package de.randi2test.randomization;
 
+import de.randi2.model.TreatmentArm;
 import de.randi2.model.Trial;
 import de.randi2.model.TrialSubject;
 import de.randi2.model.randomization.BlockRandomizationConfig;
+import de.randi2.utility.IntegerIterator;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static de.randi2.utility.IntegerIterator.upto;
 
 /**
  *
@@ -30,10 +33,27 @@ public class BlockRandomizationTest {
 	@Test
 	public void testOneSubjectAllocation() {
 		RandomizationHelper.addArms(trial, 10, 10);
-		conf.setMinimum(2); conf.setMaximum(2);
+		conf.setMinimum(2);
+		conf.setMaximum(2);
 		s = new TrialSubject();
 		trial.randomize(s);
 		assertNotNull(s.getArm());
 		assertTrue(trial.getSubjects().contains(s));
 	}
+	
+	@Test
+	public void testFourSubjectAllocations() {
+		RandomizationHelper.addArms(trial, 40, 40);
+		conf.setMinimum(2);
+		conf.setMaximum(2);
+		for (int i : upto(4)) {
+			System.out.println(i);
+			s = new TrialSubject();
+			trial.randomize(s);
+		}
+		for(TreatmentArm arm : trial.getTreatmentArms()){
+			assertEquals(2, arm.getSubjects().size());
+		}
+	}
+
 }
