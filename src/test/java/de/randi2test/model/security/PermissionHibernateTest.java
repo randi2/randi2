e8d0adc182451dcdce1b4c8dd5da3d46cd36aa64
dschrimpf <daniel_schrimpf@web.de>
2009-03-31 17:24:57 +0200

    remove spring test runner

--- src/test/java/de/randi2test/model/security/PermissionHibernateTest.java
+++ src/test/java/de/randi2test/model/security/PermissionHibernateTest.java
@@ -1,16 +1,13 @@
 package de.randi2test.model.security;
 
+import static junit.framework.Assert.assertEquals;
+
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.security.acls.AclFormattingUtils;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import static junit.framework.Assert.*;
 
 import de.randi2.model.security.PermissionHibernate;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/META-INF/spring-test.xml" })
+
 public class PermissionHibernateTest {
 
 	@Test
