9a50042cd54765424a637352bc64fa2ad4373042
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-04-30 12:19:57 +0200

    added test class for TrialServiceTest changed spring test configuration for service tests

--- src/test/java/de/randi2/services/TrialServiceTest.java
+++ src/test/java/de/randi2/services/TrialServiceTest.java
@@ -1,22 +1,18 @@
 package de.randi2.services;
 
-import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/META-INF/spring-test.xml", "/META-INF/subconfig/security.xml"})
-public class UserServiceTest {
+public class TrialServiceTest {
 
-
-	@Autowired private UserService userService;
-	
 	@Test
 	public void test(){
-		assertNotNull(userService);
+		assertTrue(true);
 	}
 }
