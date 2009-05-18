295d6e18df3f29c6d91e69b95bc1bcd7588ea1c6
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-05-18 16:36:59 +0200

    added HttpSessionListener to flush the current hibernate session if a httpsession is expired

---
+++ src/main/java/de/randi2/utility/listener/SessionListener.java
@@ -0,0 +1,51 @@
+package de.randi2.utility.listener;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import org.apache.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.hibernate.impl.SessionImpl;
+
+import static de.randi2.utility.webfilter.HibernateWebFilter.*;
+
+public class SessionListener implements HttpSessionListener {
+
+	private Logger logger = Logger.getLogger(SessionListener.class);
+
+	@Override
+	public void sessionCreated(HttpSessionEvent se) {
+		System.out.println("create");
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent se) {
+		HttpSession httpSession = se.getSession();
+		Session hibernateSession = (Session) se.getSession().getAttribute(
+				HIBERNATE_SESSION_KEY);
+		if (hibernateSession != null) {
+			SessionFactory sf = ((SessionImpl) hibernateSession)
+					.getSessionFactory();
+			try {
+
+				sf.getCurrentSession().flush();
+				sf.getCurrentSession().close(); // Unbind is automatic here
+				httpSession.setAttribute(HIBERNATE_SESSION_KEY, null);
+				// SecurityContextHolder.getContext().setAuthentication(null);
+				if (httpSession.getAttribute(END_OF_CONVERSATION_FLAG) != null) {
+					httpSession.removeAttribute(END_OF_CONVERSATION_FLAG);
+				}
+				logger.debug("Hibernate session closed");
+			} catch (IllegalStateException e) {
+				sf.getCurrentSession().flush();
+				sf.getCurrentSession().close(); // Unbind is automatic here
+				// SecurityContextHolder.getContext().setAuthentication(null);
+				logger.trace("<<< End of conversation " + httpSession.getId());
+			}
+
+		}
+	}
+
+}
