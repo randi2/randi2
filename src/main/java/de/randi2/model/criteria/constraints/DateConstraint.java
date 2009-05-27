c22a1f3ba7bc6beb6f424da818a607a796992758
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-05-27 12:23:49 +0200

    added criterion for date ranges added constraint for date values

---
+++ src/main/java/de/randi2/model/criteria/constraints/DateConstraint.java
@@ -0,0 +1,32 @@
+package de.randi2.model.criteria.constraints;
+
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.persistence.Entity;
+
+import de.randi2.unsorted.ContraintViolatedException;
+
+@Entity
+public class DateConstraint extends AbstractConstraint<GregorianCalendar>{
+
+	
+	private GregorianCalendar exaptedValue;
+	
+	public GregorianCalendar getExaptedValue() {
+		return exaptedValue;
+	}
+	
+	public void setExaptedValue(GregorianCalendar exaptedValue) {
+		this.exaptedValue = exaptedValue;
+	}
+	
+	@Override
+	protected void configure(List<GregorianCalendar> args)
+			throws ContraintViolatedException {
+	}
+	
+	@Override
+	public void isValueCorrect(GregorianCalendar _value)
+			throws ContraintViolatedException {	}
+}
