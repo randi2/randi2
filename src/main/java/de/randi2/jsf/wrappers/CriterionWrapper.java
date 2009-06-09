ec6b9109efc2f28bcc90444cb4dae2116154809b
Lukasz Plotnicki <l.plotnicki@gmail.com>
2009-06-09 14:00:07 +0200

    Deactivated the constraints UI. Plus some generic fixes for the compiler

--- src/main/java/de/randi2/jsf/wrappers/CriterionWrapper.java
+++ src/main/java/de/randi2/jsf/wrappers/CriterionWrapper.java
@@ -1,5 +1,6 @@
 package de.randi2.jsf.wrappers;
 
+import java.io.Serializable;
 import java.util.ResourceBundle;
 
 import javax.faces.context.FacesContext;
@@ -11,7 +12,7 @@ import de.randi2.model.criteria.DateCriterion;
 import de.randi2.model.criteria.DichotomousCriterion;
 import de.randi2.model.criteria.FreeTextCriterion;
 import de.randi2.model.criteria.OrdinalCriterion;
-
+import de.randi2.model.criteria.constraints.AbstractConstraint;
 
 /**
  * UI Wrapper for the Criterion
@@ -21,64 +22,72 @@ import de.randi2.model.criteria.OrdinalCriterion;
  */
 public class CriterionWrapper {
 
-	private AbstractCriterion<?, ?> wrappedCriterion = null;
-	
-	private boolean isConstraint = true;
-	
+	private AbstractCriterion<? extends Serializable, ? extends AbstractConstraint<? extends Serializable>> wrappedCriterion = null;
+
+	private boolean isConstraint = false;
+
 	private String panelType = "criterionErrorPanel";
-	
-	public CriterionWrapper(AbstractCriterion<?, ?> _criterion){
+
+	public CriterionWrapper(AbstractCriterion<?, ?> _criterion) {
 		wrappedCriterion = _criterion;
 	}
-	
+
 	public AbstractCriterion<?, ?> getWrappedCriterion() {
 		return wrappedCriterion;
 	}
-	
+
 	public void setWrappedCriterion(AbstractCriterion<?, ?> wrappedCriterion) {
 		this.wrappedCriterion = wrappedCriterion;
 	}
-	
+
 	public boolean isConstraint() {
 		return isConstraint;
 	}
-	
+
 	public void setConstraint(boolean isConstraint) {
 		this.isConstraint = isConstraint;
 	}
-	
-	public String getL16edName(){
-		return ResourceBundle.getBundle( "de.randi2.jsf.i18n.criteria",((LoginHandler) FacesContext.getCurrentInstance()
-			    .getApplication().getELResolver().getValue(
-			    	      FacesContext.getCurrentInstance().getELContext(), null,
-			    	      "loginHandler")).getChosenLocale()).getString(wrappedCriterion.getUIName());
+
+	public String getL16edName() {
+		return ResourceBundle.getBundle(
+				"de.randi2.jsf.i18n.criteria",
+				((LoginHandler) FacesContext.getCurrentInstance()
+						.getApplication().getELResolver().getValue(
+								FacesContext.getCurrentInstance()
+										.getELContext(), null, "loginHandler"))
+						.getChosenLocale()).getString(
+				wrappedCriterion.getUIName());
 	}
 
 	public String getPanelType() {
-		if(DateCriterion.class.isInstance(wrappedCriterion))
+		if (DateCriterion.class.isInstance(wrappedCriterion))
 			panelType = "datePanel";
-		else if(DichotomousCriterion.class.isInstance(wrappedCriterion))
+		else if (DichotomousCriterion.class.isInstance(wrappedCriterion))
 			panelType = "dichotomousPanel";
-		else if(FreeTextCriterion.class.isInstance(wrappedCriterion))
+		else if (FreeTextCriterion.class.isInstance(wrappedCriterion))
 			panelType = "freeTextPanel";
-		else if(OrdinalCriterion.class.isInstance(wrappedCriterion))
+		else if (OrdinalCriterion.class.isInstance(wrappedCriterion))
 			panelType = "ordinalPanel";
 		return panelType;
 	}
-	
+
 	public void addElement(ActionEvent event) {
-		if(OrdinalCriterion.class.isInstance(wrappedCriterion))
-			((OrdinalCriterion)wrappedCriterion).getElements().add("");
+		if (OrdinalCriterion.class.isInstance(wrappedCriterion))
+			((OrdinalCriterion) wrappedCriterion).getElements().add("");
 	}
 
 	public void removeElement(ActionEvent event) {
-		if(OrdinalCriterion.class.isInstance(wrappedCriterion))
-			((OrdinalCriterion)wrappedCriterion).getElements().remove(((OrdinalCriterion)wrappedCriterion).getElements().size() -1);
+		if (OrdinalCriterion.class.isInstance(wrappedCriterion))
+			((OrdinalCriterion) wrappedCriterion).getElements()
+					.remove(
+							((OrdinalCriterion) wrappedCriterion).getElements()
+									.size() - 1);
 	}
-	
-	public boolean isElementsEmpty(){
-		if(OrdinalCriterion.class.isInstance(wrappedCriterion))
-			return ((OrdinalCriterion)wrappedCriterion).getElements().isEmpty();
+
+	public boolean isElementsEmpty() {
+		if (OrdinalCriterion.class.isInstance(wrappedCriterion))
+			return ((OrdinalCriterion) wrappedCriterion).getElements()
+					.isEmpty();
 		return true;
 	}
 }
