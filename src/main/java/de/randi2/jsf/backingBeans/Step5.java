21c974a4de00bd4d1f4f9253a390fd589d807bd0
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-07-06 16:46:09 +0200

    added view to configure block randomization

--- src/main/java/de/randi2/jsf/backingBeans/Step5.java
+++ src/main/java/de/randi2/jsf/backingBeans/Step5.java
@@ -17,9 +17,9 @@ import de.randi2.model.randomization.CompleteRandomizationConfig;
 public class Step5 {
 
 	public static enum AlgorithmPanelId {
-		COMPLETE_RANDOMIZATION("completeRandomization"),
-		BIASEDCOIN_RANDOMIZATION("biasedCoinRandomization"),
-		BLOCK_RANDOMIZATION("blockRandomization");
+		COMPLETE_RANDOMIZATION("completeRandomization"), BIASEDCOIN_RANDOMIZATION(
+				"biasedCoinRandomization"), BLOCK_RANDOMIZATION(
+				"blockRandomization");
 		private String id = null;
 
 		private AlgorithmPanelId(String id) {
@@ -30,25 +30,55 @@ public class Step5 {
 		public String toString() {
 			return this.id;
 		}
+
 	}
-	
+
+	public static enum BlockDesignTypeId {
+		VARIABLE_BLOCK("variableBlockSize"), CONSTANT_BOLCK("constantBlockSize");
+
+		private String id = null;
+
+		private BlockDesignTypeId(String id) {
+			this.id = id;
+		}
+
+		@Override
+		public String toString() {
+			return this.id;
+		}
+	}
+
 	private List<SelectItem> algorithms;
 
 	public List<SelectItem> getAlgorithms() {
-		if(algorithms==null){
-			ValueExpression ve = FacesContext.getCurrentInstance().getApplication().getExpressionFactory().createValueExpression(FacesContext.getCurrentInstance()
-					.getELContext(),
-					"#{loginHandler}", LoginHandler.class);
+		if (algorithms == null) {
+			ValueExpression ve = FacesContext.getCurrentInstance()
+					.getApplication().getExpressionFactory()
+					.createValueExpression(
+							FacesContext.getCurrentInstance().getELContext(),
+							"#{loginHandler}", LoginHandler.class);
 			LoginHandler currentLoginHandler = (LoginHandler) ve
-			.getValue(FacesContext.getCurrentInstance()
-					.getELContext());
-			ResourceBundle bundle = ResourceBundle.getBundle("de.randi2.jsf.i18n.algorithms",
-					currentLoginHandler.getChosenLocale());
+					.getValue(FacesContext.getCurrentInstance().getELContext());
+			ResourceBundle bundle = ResourceBundle.getBundle(
+					"de.randi2.jsf.i18n.algorithms", currentLoginHandler
+							.getChosenLocale());
 			algorithms = new ArrayList<SelectItem>();
-			algorithms.add(new SelectItem(AlgorithmPanelId.COMPLETE_RANDOMIZATION.toString(),bundle.getString(CompleteRandomizationConfig.class.getCanonicalName()+".short")));
-			algorithms.add(new SelectItem(AlgorithmPanelId.BIASEDCOIN_RANDOMIZATION.toString(), bundle.getString(BiasedCoinRandomizationConfig.class.getCanonicalName()+".short")));
-			algorithms.add(new SelectItem(AlgorithmPanelId.BLOCK_RANDOMIZATION.toString(), bundle.getString(BlockRandomizationConfig.class.getCanonicalName()+".short")));
-			
+			algorithms.add(new SelectItem(
+					AlgorithmPanelId.COMPLETE_RANDOMIZATION.toString(), bundle
+							.getString(CompleteRandomizationConfig.class
+									.getCanonicalName()
+									+ ".short")));
+			algorithms.add(new SelectItem(
+					AlgorithmPanelId.BIASEDCOIN_RANDOMIZATION.toString(),
+					bundle.getString(BiasedCoinRandomizationConfig.class
+							.getCanonicalName()
+							+ ".short")));
+			algorithms.add(new SelectItem(AlgorithmPanelId.BLOCK_RANDOMIZATION
+					.toString(), bundle
+					.getString(BlockRandomizationConfig.class
+							.getCanonicalName()
+							+ ".short")));
+
 		}
 		return algorithms;
 	}
@@ -57,19 +87,71 @@ public class Step5 {
 
 	public void setSelectedAlgorithmPanelId(String selectedAlgorithmPanelId) {
 		this.selectedAlgorithmPanelId = selectedAlgorithmPanelId;
-		ValueExpression ve = FacesContext.getCurrentInstance().getApplication().getExpressionFactory().createValueExpression(FacesContext.getCurrentInstance()
-				.getELContext(),
-				"#{trialHandler}", 	TrialHandler.class);
+		ValueExpression ve = FacesContext.getCurrentInstance().getApplication()
+				.getExpressionFactory().createValueExpression(
+						FacesContext.getCurrentInstance().getELContext(),
+						"#{trialHandler}", TrialHandler.class);
 		TrialHandler currentTrialHandler = (TrialHandler) ve
-		.getValue(FacesContext.getCurrentInstance()
-				.getELContext());
-		if(selectedAlgorithmPanelId.equals(AlgorithmPanelId.BLOCK_RANDOMIZATION.toString())){
-			currentTrialHandler.setRandomizationConfig(new BlockRandomizationConfig());
+				.getValue(FacesContext.getCurrentInstance().getELContext());
+		if (selectedAlgorithmPanelId
+				.equals(AlgorithmPanelId.BLOCK_RANDOMIZATION.toString())) {
+			currentTrialHandler
+					.setRandomizationConfig(new BlockRandomizationConfig());
 		}
 	}
 
 	public String getSelectedAlgorithmPanelId() {
 		return selectedAlgorithmPanelId;
+	}
+
+	private List<SelectItem> blockRandTypes;
+
+	public List<SelectItem> getBlockRandTypes() {
+		if (blockRandTypes == null) {
+			ValueExpression ve = FacesContext.getCurrentInstance()
+					.getApplication().getExpressionFactory()
+					.createValueExpression(
+							FacesContext.getCurrentInstance().getELContext(),
+							"#{loginHandler}", LoginHandler.class);
+			LoginHandler currentLoginHandler = (LoginHandler) ve
+					.getValue(FacesContext.getCurrentInstance().getELContext());
+			ResourceBundle bundle = ResourceBundle.getBundle(
+					"de.randi2.jsf.i18n.algorithms", currentLoginHandler
+							.getChosenLocale());
+			blockRandTypes = new ArrayList<SelectItem>();
+			blockRandTypes.add(new SelectItem(BlockDesignTypeId.CONSTANT_BOLCK
+					.toString(), bundle
+					.getString(BlockRandomizationConfig.class
+							.getCanonicalName()
+							+ ".constantBlockSize")));
+			blockRandTypes.add(new SelectItem(BlockDesignTypeId.VARIABLE_BLOCK
+					.toString(), bundle
+					.getString(BlockRandomizationConfig.class
+							.getCanonicalName()
+							+ ".variableBlockSize")));
+
+		}
+		return blockRandTypes;
+	}
+
+	private String selectedBlockRandTypes;
+	private boolean renderVariable = false;
+
+	public String getSelectedBlockRandTypes() {
+		return selectedBlockRandTypes;
+	}
+
+	public void setSelectedBlockRandTypes(String selectedBlockRandTypes) {
+		this.selectedBlockRandTypes = selectedBlockRandTypes;
+		if (selectedBlockRandTypes.equals(BlockDesignTypeId.VARIABLE_BLOCK
+				.toString())) {
+			renderVariable = true;
+		} else
+			renderVariable = false;
+	}
+
+	public boolean isRenderVariable() {
+		return renderVariable;
 	}
 
 }
--- src/main/java/de/randi2/jsf/supportBeans/Enums.java
+++ src/main/java/de/randi2/jsf/supportBeans/Enums.java
@@ -9,6 +9,7 @@ import javax.faces.model.SelectItem;
 
 import de.randi2.jsf.controllerBeans.LoginHandler;
 import de.randi2.model.enumerations.Gender;
+import de.randi2.model.randomization.BlockRandomizationConfig;
 
 public class Enums {
 	
@@ -18,6 +19,11 @@ public class Enums {
 		genderItems = new ArrayList<SelectItem>(Gender.values().length);
 		for (Gender g : Gender.values()) {
 			genderItems.add(new SelectItem(g, localizeGenderEntry(g)));
+		}
+		
+		blockSizeTypeItems = new ArrayList<SelectItem>(BlockRandomizationConfig.TYPE.values().length);
+		for (BlockRandomizationConfig.TYPE t : BlockRandomizationConfig.TYPE.values()) {
+			blockSizeTypeItems.add(new SelectItem(t, localizeBlockSizeTypeItemsEntry(t)));
 		}
 	}
 
@@ -30,6 +36,22 @@ public class Enums {
 			    .getApplication().getELResolver().getValue(
 			    	      FacesContext.getCurrentInstance().getELContext(), null,
 			    	      "loginHandler")).getChosenLocale()).getString(g.toString());
+	}
+	
+	
+private List<SelectItem> blockSizeTypeItems;
+	
+	
+
+	public List<SelectItem> getBlockSizeTypeItems() {
+		return blockSizeTypeItems;
+	}
+	
+	private String localizeBlockSizeTypeItemsEntry(BlockRandomizationConfig.TYPE t){
+		return ResourceBundle.getBundle( "de.randi2.jsf.i18n.algorithms",((LoginHandler) FacesContext.getCurrentInstance()
+			    .getApplication().getELResolver().getValue(
+			    	      FacesContext.getCurrentInstance().getELContext(), null,
+			    	      "loginHandler")).getChosenLocale()).getString(BlockRandomizationConfig.class.getCanonicalName()+"."+t.toString());
 	}
 	
 }
