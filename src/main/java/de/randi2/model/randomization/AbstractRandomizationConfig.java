fc6fb5dc9a672a56520fb20839a4f2a5d72ac016
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-08-21 14:51:01 +0200

    added @EqualsAndHashCode, @ToString and @Getter Annotation and removed methods

--- src/main/java/de/randi2/model/randomization/AbstractRandomizationConfig.java
+++ src/main/java/de/randi2/model/randomization/AbstractRandomizationConfig.java
@@ -3,22 +3,27 @@ package de.randi2.model.randomization;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
-
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 import de.randi2.model.AbstractDomainObject;
 import de.randi2.model.Trial;
 import de.randi2.randomization.RandomizationAlgorithm;
-import javax.persistence.OneToOne;
-import javax.persistence.Transient;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Table(name = "RandomizationConfig")
+@EqualsAndHashCode(callSuper=true)
+@ToString
 public abstract class AbstractRandomizationConfig extends AbstractDomainObject {
 
 	private static final long serialVersionUID = -942332706403245140L;
 	@OneToOne
+	@Getter 
 	private Trial trial;
 	@Transient
 	private RandomizationAlgorithm<? extends AbstractRandomizationConfig> algorithm;
@@ -32,10 +37,6 @@ public abstract class AbstractRandomizationConfig extends AbstractDomainObject {
 
 	public abstract RandomizationAlgorithm<? extends AbstractRandomizationConfig> createAlgorithm();
 
-	
-	public Trial getTrial() {
-		return trial;
-	}
 
 	public void setTrial(Trial _trial) {
 		this.trial = _trial;
