2d469ad586ffaaea74151acd40cae9b2f17cbeb9
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-09-01 11:37:33 +0200

    added hibernate and lombok annotations;

--- src/main/java/de/randi2/model/randomization/TruncatedBinomialDesignConfig.java
+++ src/main/java/de/randi2/model/randomization/TruncatedBinomialDesignConfig.java
@@ -1,8 +1,15 @@
 package de.randi2.model.randomization;
 
+import javax.persistence.Entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import de.randi2.randomization.RandomizationAlgorithm;
 import de.randi2.randomization.TruncatedBinomialDesign;
 
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=true)
 public class TruncatedBinomialDesignConfig extends AbstractRandomizationConfig {
 
 	private static final long serialVersionUID = -4994698552861007117L;
