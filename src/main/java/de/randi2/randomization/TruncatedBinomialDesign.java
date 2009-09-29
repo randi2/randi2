f93c0a600ae6f9087e00588258eb29e706721edd
dschrimpf <dschrimpf@leonardo6>
2009-09-29 15:53:41 +0200

    added truncated randomization for different treatment arm size

--- src/main/java/de/randi2/randomization/TruncatedBinomialDesign.java
+++ src/main/java/de/randi2/randomization/TruncatedBinomialDesign.java
@@ -1,5 +1,8 @@
 package de.randi2.randomization;
 
+import static de.randi2.utility.ArithmeticUtil.ggt;
+import static de.randi2.utility.IntegerIterator.upto;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -7,6 +10,7 @@ import java.util.Random;
 import de.randi2.model.TreatmentArm;
 import de.randi2.model.Trial;
 import de.randi2.model.TrialSubject;
+import de.randi2.model.randomization.Block;
 import de.randi2.model.randomization.TruncatedBinomialDesignConfig;
 
 public class TruncatedBinomialDesign extends
@@ -31,7 +35,28 @@ public class TruncatedBinomialDesign extends
 		if(possibleArms.isEmpty()){
 			return null;
 		}else{
-			return possibleArms.get(random.nextInt(possibleArms.size()));
+			Block block = new Block();
+		
+			int[] sizes = new int[possibleArms.size()];
+			int i = 0;
+			for (TreatmentArm arm : possibleArms) {
+				sizes[i] = arm.getPlannedSubjects();
+				i++;
+			}
+
+			int divide = sizes[0];
+			for (i = 1; i < sizes.length; i++) {
+				divide = ggt(divide, sizes[i]);
+			}
+
+			for (TreatmentArm arm : possibleArms) {
+				int size = arm.getPlannedSubjects() / divide;
+				for (int j : upto(size)) {
+					block.add(arm);
+				}
+			}
+			
+			return block.pullFromBlock(random);
 		}
 	}
 
