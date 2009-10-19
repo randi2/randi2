22898e060c21327f105e258ef7ba9072e324d98c
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-10-19 13:41:29 +0200

    added parameter p

--- src/main/java/de/randi2/model/randomization/MinimizationConfig.java
+++ src/main/java/de/randi2/model/randomization/MinimizationConfig.java
@@ -1,13 +1,18 @@
 package de.randi2.model.randomization;
 
+import lombok.Data;
+import de.randi2.randomization.Minimization;
 import de.randi2.randomization.RandomizationAlgorithm;
 
+@Data
 public class MinimizationConfig extends AbstractRandomizationConfig {
 
+	
+	private int p;
+	
 	@Override
 	public RandomizationAlgorithm<? extends AbstractRandomizationConfig> createAlgorithm() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Minimization(super.getTrial());
 	}
 
 }
