95c1a2544656a93538ee76f55ae5896560a14bfe
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-10-28 16:59:00 +0100

    added model classes for simulation results

---
+++ src/main/java/de/randi2/simulation/model/SimulationResult.java
@@ -0,0 +1,21 @@
+package de.randi2.simulation.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class SimulationResult {
+
+	private int amountRuns;
+	
+	private int min;
+	
+	private int max;
+	
+	private double mean;
+	
+	private int median;
+	
+	
+}
