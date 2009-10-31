3e29b67bba6cf1a877e9a7ebc9dc6c110d5320b6
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-10-31 16:21:34 +0100

    added java doc

--- src/main/java/de/randi2/dao/LoginDao.java
+++ src/main/java/de/randi2/dao/LoginDao.java
@@ -1,21 +1,45 @@
+/* 
+ * (c) 2008-2009 RANDI2 Core Development Team
+ * 
+ * This file is part of RANDI2.
+ * 
+ * RANDI2 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * RANDI2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * RANDI2. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.randi2.dao;
 
 
 import de.randi2.model.Login;
 
 
+/**
+ * The Interface LoginDao.
+ */
 public interface LoginDao extends AbstractDao<Login> {
 
 	/**
 	 * This method returns an login object for a given username.
 	 * 
-	 * @param username -
-	 *            unique username.
+	 * @param username
+	 *            - unique username.
+	 * 
 	 * @return - a complete login object or <code>null</code> if no such login
 	 *         is available.
 	 */
 	public Login get(String username);
 	
+	/* (non-Javadoc)
+	 * @see de.randi2.dao.AbstractDao#create(de.randi2.model.AbstractDomainObject)
+	 */
 	@Override
 	public void create(Login object);
 
--- src/main/java/de/randi2/dao/UserDetailsServiceImpl.java
+++ src/main/java/de/randi2/dao/UserDetailsServiceImpl.java
@@ -1,3 +1,20 @@
+/* 
+ * (c) 2008-2009 RANDI2 Core Development Team
+ * 
+ * This file is part of RANDI2.
+ * 
+ * RANDI2 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * RANDI2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * RANDI2. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.randi2.dao;
 
 import java.util.GregorianCalendar;
@@ -16,13 +33,21 @@ import org.springframework.transaction.annotation.Transactional;
 
 import de.randi2.model.Login;
 
+/**
+ * The Class UserDetailsServiceImpl.
+ */
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 
+	/** The logger. */
 	private Logger logger = Logger.getLogger(UserDetailsService.class);
 	
+	/** The session factory. */
 	@Autowired private SessionFactory sessionFactory;
 	
+	/* (non-Javadoc)
+	 * @see org.springframework.security.userdetails.UserDetailsService#loadUserByUsername(java.lang.String)
+	 */
 	@Override
 	@SuppressWarnings("unchecked")
 	@Transactional(propagation=Propagation.SUPPORTS, readOnly=true)
@@ -51,6 +76,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		
 	}
 	
+		/**
+		 * Sets the session factory.
+		 * 
+		 * @param sessionFactory
+		 *            the new session factory
+		 */
 		public void setSessionFactory(SessionFactory sessionFactory) {
 			this.sessionFactory = sessionFactory;
 		}
