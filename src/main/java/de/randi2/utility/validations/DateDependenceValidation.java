b38f6e929a3db7d712124598fdec97c60da5504a
hype <hype@TIGER.(none)>
2009-11-01 15:46:59 +0100

    Comments for copyright edited.

--- src/main/java/de/randi2/utility/validations/DateDependenceValidation.java
+++ src/main/java/de/randi2/utility/validations/DateDependenceValidation.java
@@ -1,3 +1,20 @@
+/* 
+ * (c) 2008- RANDI2 Core Development Team
+ * 
+ * This file is part of RANDI2.
+ * 
+ * RANDI2 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * RANDI2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * RANDI2. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.randi2.utility.validations;
 
 import java.lang.reflect.InvocationTargetException;
