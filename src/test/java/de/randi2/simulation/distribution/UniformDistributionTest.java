6f1c457082378fe5515be54c3bd7d61115ca0825
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-11-10 11:37:54 +0100

    changed and added test case

--- src/test/java/de/randi2/simulation/distribution/UniformDistributionTest.java
+++ src/test/java/de/randi2/simulation/distribution/UniformDistributionTest.java
@@ -7,17 +7,30 @@ import org.junit.Test;
 
 public class UniformDistributionTest {
 
-	@Test
-	public void testUniformDistribution(){
-		UniformDistribution<Integer> ud = new UniformDistribution<Integer>(Arrays.asList(0,1,2,3,4,5));
+	private int runsPerEntry = 1000;
+	private double limit = 0.05;
+	
+	private void testUniformDistribution(UniformDistribution<Integer> ud){
+		int runs = runsPerEntry*ud.elements.size();
 		int[] values = new int[ud.elements.size()];
-		for(int i =0; i<100000;i++){
+		for(int i =0; i<runs;i++){
 			values[ud.getNextValue()]++;
 		}
 		for(int i : values){
-			System.out.println(i/100000.0);
-			assertTrue((i/100000.0)>0.16);
-			assertTrue((i/100000.0)<0.17);
+			double mean = (1.0 * i) / runs;
+			assertTrue("error: " + mean +" > " +((1.0/ud.elements.size())-limit),(mean)>((1.0/ud.elements.size())-limit));
+			assertTrue("error: " + mean +" < " +((1.0/ud.elements.size())+limit),(mean)<((1.0/ud.elements.size())+limit));
+		}
+	}
+	
+	
+	
+	@Test
+	public void test1000Times(){
+		for(int i=0; i<1000;i++){
+			testUniformDistribution(new UniformDistribution<Integer>(Arrays.asList(0,1,2,3,4,5)));
+			testUniformDistribution(new UniformDistribution<Integer>(Arrays.asList(0,1)));
+			testUniformDistribution(new UniformDistribution<Integer>(Arrays.asList(0,1,2,3)));
 		}
 	}
 }
