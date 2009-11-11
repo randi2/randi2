59b75872ce30dd0474ab1b47b99e964ef67df52e
Daniel Schrimpf <daniel_schrimpf@web.de>
2009-11-11 17:50:07 +0100

    work on biased coin minimization

--- src/main/java/de/randi2/randomization/Minimization.java
+++ src/main/java/de/randi2/randomization/Minimization.java
@@ -1,28 +1,48 @@
 package de.randi2.randomization;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
+import de.randi2.model.SubjectProperty;
 import de.randi2.model.TreatmentArm;
 import de.randi2.model.Trial;
 import de.randi2.model.TrialSubject;
+import de.randi2.model.criteria.AbstractCriterion;
+import de.randi2.model.criteria.constraints.AbstractConstraint;
 import de.randi2.model.randomization.MinimizationConfig;
+import de.randi2.unsorted.ContraintViolatedException;
 
 public class Minimization extends RandomizationAlgorithm<MinimizationConfig>{
 
 
+	private Map<TreatmentArm, Map<TreatmentArm, Double>> probabilitiesPerPreferredTreatment;
+	
 	public Minimization(Trial _trial) {
 		super(_trial);
+		initProbabilitiesPerPreferredTreatment();
 	}
 
 	public Minimization(Trial _trial, long seed) {
 		super(_trial, seed);
+		initProbabilitiesPerPreferredTreatment();
 	}
 	
 	
 	@Override
 	protected TreatmentArm doRadomize(TrialSubject subject, Random random) {
+		if (configuration.isBiasedCoinMinimization()){
+			return doRandomizeBiasedCoinMinimization(subject, random);
+		}else{
+			return doRandomizeNaiveMinimization(subject, random);
+		}
+	}
+	
+	private TreatmentArm doRandomizeNaiveMinimization(TrialSubject subject, Random random){
 		//calculate the p-values for this allocation
 		List<TreatmentArm> arms = trial.getTreatmentArms();
 		ArrayList<Double> a = new ArrayList<Double>();
@@ -36,24 +56,12 @@ public class Minimization extends RandomizationAlgorithm<MinimizationConfig>{
 				}
 				totalPlannedSubjects +=arm1.getPlannedSubjects();
 			}
-			if(!configuration.isBiasedCoinMinimization()){
+			//Formula from: "Randomization by minimization for unbalanced treatment allocation" Baoguang Han, et al. 
 				double value = plannedSubjects*configuration.getP() + (1.0-configuration.getP())/(trial.getTreatmentArms().size() -1.0)*sum;
 				value = value / (configuration.isWithRandomizedSubjects() ? totalPlannedSubjects-trial.getTotalSubjectAmount(): totalPlannedSubjects);
 				a.add(value);
-			}else{
-				//TODO Problem with formula
-				double value = plannedSubjects*(1.0-sum/(totalPlannedSubjects-arms.get(0).getPlannedSubjects())*(1.0-configuration.getP()));
-				value = value + plannedSubjects/(totalPlannedSubjects-arms.get(0).getPlannedSubjects())*(1-configuration.getP())*sum;
-				value = value /totalPlannedSubjects;
-				a.add(value);
-			}
+		
 		}
-		double test = 0;
-		for(double t : a){
-			test +=t;
-			System.out.println("yyyy: "+ t);
-		}
-		System.out.println("xxxx: "  + test);
 		//get Treatment arm with calculated p-values
 		double randomNumber = random.nextDouble();
 		double sum = 0;
@@ -67,6 +75,131 @@ public class Minimization extends RandomizationAlgorithm<MinimizationConfig>{
 			i++;
 		}
 		return arm;
+	}
+	
+	
+	
+	@SuppressWarnings("unchecked")
+	private TreatmentArm doRandomizeBiasedCoinMinimization(TrialSubject subject, Random random){
+		List<TreatmentArm> arms = trial.getTreatmentArms();
+		ArrayList<Double> a = new ArrayList<Double>();
+		
+		HashMap<AbstractConstraint<?>,HashMap<TreatmentArm, Double>> relevantConstraints = new HashMap<AbstractConstraint<?>, HashMap<TreatmentArm, Double>>();
+		//Get relevant constraints
+		for(SubjectProperty prop : subject.getProperties()){
+				try {
+					HashMap<TreatmentArm, Double> countTreatment = new HashMap<TreatmentArm, Double>();
+					for(TreatmentArm arm : arms){
+						countTreatment.put(arm, 0.0);
+					}
+					relevantConstraints.put(prop.getCriterion().stratify(prop.getValue()), countTreatment);
+				} catch (ContraintViolatedException e) {	}
+		}
+		
+		//counting subjects with relevant constraints
+		for(TrialSubject sub : trial.getSubjects()){
+			for(AbstractConstraint constraint : relevantConstraints.keySet()){
+				for(SubjectProperty prop : sub.getProperties()){
+					if(constraint.checkValue(prop.getValue())){
+						relevantConstraints.get(constraint).put(sub.getArm(),(relevantConstraints.get(constraint).get(sub.getArm()) +1));
+					}
+				}
+			}
+		}
+		
+		//calculate imbalance scores
+		HashMap<TreatmentArm, Double> imbalacedScores = new HashMap<TreatmentArm, Double>();
+		//init scores
+		for(TreatmentArm arm :arms){
+			double imbalacedScore = 0.0;
+			for(HashMap<TreatmentArm, Double> map : relevantConstraints.values()){
+				double[] adjustetCountsPerArm = new double[arms.size()];
+				int i = 0;
+				for(TreatmentArm actArm : map.keySet()){
+					double adjustetCount = 0.0;
+					if(actArm.getId() == arm.getId()){
+						adjustetCount  =map.get(actArm)+1;
+					}else{
+						adjustetCount  =map.get(actArm);
+					}
+					//calculate adjusted counts
+					adjustetCount = adjustetCount / actArm.getPlannedSubjects();
+					adjustetCountsPerArm[i] = adjustetCount;
+					i++;
+				}
+				//calculate marginal balance
+				double marginalBalance = 0.0;
+				double numerator = 0.0;	
+				for(i=0;i<adjustetCountsPerArm.length-1;i++){
+					for(int j = i+1;j<adjustetCountsPerArm.length;j++){
+						marginalBalance += Math.abs(adjustetCountsPerArm[i]-adjustetCountsPerArm[j]);
+					}
+					numerator += adjustetCountsPerArm[i];
+				}
+				numerator+=adjustetCountsPerArm[adjustetCountsPerArm.length-1];
+				numerator =(adjustetCountsPerArm.length-1.0) * numerator;
+				marginalBalance = marginalBalance/numerator;
+				
+				imbalacedScore+= marginalBalance;
+			}
+			imbalacedScores.put(arm, imbalacedScore);
+		}
+	
+		//find preferred treatment
+		double tmpMinValue = Double.MAX_VALUE;
+		TreatmentArm preferredArm = null;
+		for(TreatmentArm arm : imbalacedScores.keySet()){
+			if(imbalacedScores.get(arm)<tmpMinValue){
+				tmpMinValue = imbalacedScores.get(arm);
+				preferredArm = arm;
+			}
+		}
+	
+		
+		//calculate p^H_pref
+		double p_pref = 0.0;
+		
+		for(TreatmentArm arm1 :arms){
+			if(!arm.equals(arm1)){
+				sum+=configuration.isWithRandomizedSubjects() ? (arm1.getPlannedSubjects()-arm1.getCurrentSubjectsAmount()) :arm1.getPlannedSubjects();
+			}
+			totalPlannedSubjects +=arm1.getPlannedSubjects();
+		}
+					
+		
+		
+		
+		//get Treatment arm with calculated p-values
+		double randomNumber = random.nextDouble();
+		double sum = 0;
+		int i = 0;
+		TreatmentArm arm = null;
+		while(i<a.size() && arm == null){
+			sum+=a.get(i);
+			if(randomNumber <sum){
+				arm = arms.get(i);
+			}
+			i++;
+		}
+		return arm;
+	}
+	
+	private void initProbabilitiesPerPreferredTreatment(){
+		probabilitiesPerPreferredTreatment = new HashMap<TreatmentArm, Map<TreatmentArm,Double>>();
+		for(TreatmentArm prefArm : trial.getTreatmentArms()){
+			HashMap<TreatmentArm,Double> probabilities = new HashMap<TreatmentArm, Double>();
+			double pH_pref = 0.0;
+			double denuminator = 0.0;
+			double numinator = 0.0;
+			for(TreatmentArm arm : trial.getTreatmentArms()){
+				if(!arm.equals(prefArm)){
+					denuminator+=arm.getPlannedSubjects();
+				}
+				if(!arm.equals(trial.getTreatmentArms().get(0))){
+					numinator+=arm.getPlannedSubjects();
+				}
+			}
+		}
 	}
 
 }
