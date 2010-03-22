9efb6417dcaac096e5a56472abe9a68d51e30fa2
Daniel Schrimpf <daniel_schrimpf@web.de>
2010-03-22 17:31:09 +0100

    added test classes

---
+++ src/test/java/de/randi2/simulation/model/SimulationResultTest.java
@@ -0,0 +1,25 @@
+package de.randi2.simulation.model;
+
+import org.junit.Test;
+
+public class SimulationResultTest {
+
+	
+	
+	@Test
+	public void testDuration(){
+		
+	}
+	
+	
+	@Test
+	public void testMarginalBalance(){
+		
+	}
+	
+	
+	@Test
+	public void testSubjectCounts(){
+		
+	}
+}
---
+++ src/test/java/de/randi2/simulation/model/SimulationRunTest.java
@@ -0,0 +1,12 @@
+package de.randi2.simulation.model;
+
+import org.junit.Test;
+
+public class SimulationRunTest {
+
+	
+	@Test
+	public void testMarginalBalance(){
+		
+	}
+}
