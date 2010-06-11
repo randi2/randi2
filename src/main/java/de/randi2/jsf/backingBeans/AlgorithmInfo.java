b8dcea9816e2b579fe0b5af6891ff4c83469c80b
Lukasz Plotnicki <luki@luki-desktop.(none)>
2010-06-11 08:50:31 +0200

    Fixed issue #52 (http://www.randi2.org/retro/tickets/52)

--- src/main/java/de/randi2/jsf/backingBeans/AlgorithmInfo.java
+++ src/main/java/de/randi2/jsf/backingBeans/AlgorithmInfo.java
@@ -32,118 +32,139 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
- * JSF backing bean which provieds some information about current trials randomization algorithm.
- * Created by IntelliJ IDEA.
- * User: Lukasz Plotnicki <l.plotnicki@dkfz.de>
- * Date: 02.10.2009
- * Time: 15:13:16
+ * JSF backing bean which provieds some information about current trials
+ * randomization algorithm. Created by IntelliJ IDEA. User: Lukasz Plotnicki
+ * <l.plotnicki@dkfz.de> Date: 02.10.2009 Time: 15:13:16
  */
 public class AlgorithmInfo {
 
-    @Setter
-    private Randi2Page randi2Page;
+	@Setter
+	private Randi2Page randi2Page;
 
-    @Setter
-    private LoginHandler loginHandler;
+	@Setter
+	private LoginHandler loginHandler;
 
-    public String getAlgName(){
-        ResourceBundle bundle = ResourceBundle.getBundle(
-					"de.randi2.jsf.i18n.algorithms", loginHandler
-							.getChosenLocale());
-        return bundle.getString(randi2Page.getCurrentTrial().getRandomizationConfiguration().getClass().getCanonicalName()+".name");
-    }
-
-    public String getFurtherDetails(){
-        StringBuffer furtherDetails = new StringBuffer();
-        ResourceBundle bundle = ResourceBundle.getBundle(
-				"de.randi2.jsf.i18n.labels", loginHandler
+	public String getAlgName() {
+		ResourceBundle bundle = ResourceBundle
+				.getBundle("de.randi2.jsf.i18n.algorithms", loginHandler
 						.getChosenLocale());
-    	if(BlockRandomizationConfig.class.isInstance(randi2Page.getCurrentTrial().getRandomizationConfiguration())){
-           BlockRandomizationConfig conf = BlockRandomizationConfig.class.cast(randi2Page.getCurrentTrial().getRandomizationConfiguration());
-           furtherDetails.append("<b>");
-           furtherDetails.append(bundle.getString("pages.blockR.variableBSize"));
-           furtherDetails.append("</b> ");
-           furtherDetails.append(conf.isVariableBlockSize());
-           furtherDetails.append("<br//>");
-           if(conf.isVariableBlockSize()){
-        	   furtherDetails.append("<b>");
-        	   furtherDetails.append(bundle.getString("pages.blockR.minBlockSize"));
-        	   furtherDetails.append("</b> ");
-               furtherDetails.append(conf.getMinimum());
-               furtherDetails.append("<br//>");
-               furtherDetails.append("<b>");
-               furtherDetails.append(bundle.getString("pages.blockR.maxBlockSize"));
-               furtherDetails.append("</b> ");
-               furtherDetails.append(conf.getMaximum());
-               furtherDetails.append("<br//>");  
-           }else{
-        	   furtherDetails.append("<b>");
-        	   furtherDetails.append(bundle.getString("pages.blockR.blockSize"));
-        	   furtherDetails.append("</b> ");
-               furtherDetails.append(conf.getMinimum());
-               furtherDetails.append("<br//>");
-           }
-        }else if(UrnDesignConfig.class.isInstance(randi2Page.getCurrentTrial().getRandomizationConfiguration())){
-        	UrnDesignConfig conf = UrnDesignConfig.class.cast(randi2Page.getCurrentTrial().getRandomizationConfiguration());
-        	furtherDetails.append("<b>");
-        	furtherDetails.append(bundle.getString("pages.urnR.initialCount"));
-        	furtherDetails.append("</b> ");
-        	furtherDetails.append(conf.getInitializeCountBalls());
-        	furtherDetails.append("<br//>");
-        	furtherDetails.append("<b>");
-        	furtherDetails.append(bundle.getString("pages.urnR.replacedBalls"));
-        	furtherDetails.append("</b> ");
-        	furtherDetails.append(conf.getCountReplacedBalls());
-        	furtherDetails.append("<br//>");
-    	}else if(MinimizationConfig.class.isInstance(randi2Page.getCurrentTrial().getRandomizationConfiguration())){
-    		MinimizationConfig conf = MinimizationConfig.class.cast(randi2Page.getCurrentTrial().getRandomizationConfiguration());
-        	furtherDetails.append("<b>");
-        	furtherDetails.append(bundle.getString("pages.minimization.pvalue"));
-        	furtherDetails.append("</b> ");
-        	furtherDetails.append(conf.getP());
-        	furtherDetails.append("<br//>");
-    	}else{
-        	furtherDetails.append("--");
-        }
-        return furtherDetails.toString();
-    }
+		return bundle.getString(randi2Page.getCurrentTrial()
+				.getRandomizationConfiguration().getClass().getCanonicalName()
+				+ ".name");
+	}
 
-    /**
-     * Specifies if the algorithm is stratified or not.
-     * @return
-     */
-    public boolean isStratified(){
-    	boolean t = isStrataFactorsDefined();
-        if(t)
-        	return t;
-        else
-        	return randi2Page.getCurrentTrial().isStratifyTrialSite();
-    }
-    
-    /**
-     * Checks if any strata factors are defined.
-     * @return
-     */
-    public boolean isStrataFactorsDefined(){
-    	 
-    	for(AbstractCriterion<?,?> c : randi2Page.getCurrentTrial().getCriteria()){
-    		if(c.getStrata()!=null){
-    			if(c.getStrata().size()>0)
-                    return true;
-    		}
-         }
-    	 return false;
-    }
+	public String getFurtherDetails() {
+		StringBuffer furtherDetails = new StringBuffer();
+		ResourceBundle bundle = ResourceBundle.getBundle(
+				"de.randi2.jsf.i18n.labels", loginHandler.getChosenLocale());
+		if (BlockRandomizationConfig.class.isInstance(randi2Page
+				.getCurrentTrial().getRandomizationConfiguration())) {
+			BlockRandomizationConfig conf = BlockRandomizationConfig.class
+					.cast(randi2Page.getCurrentTrial()
+							.getRandomizationConfiguration());
+			furtherDetails.append("<b>");
+			furtherDetails.append(bundle
+					.getString("pages.blockR.variableBSize"));
+			furtherDetails.append("</b> ");
+			furtherDetails.append(conf.isVariableBlockSize());
+			furtherDetails.append("<br//>");
+			if (conf.isVariableBlockSize()) {
+				furtherDetails.append("<b>");
+				furtherDetails.append(bundle
+						.getString("pages.blockR.minBlockSize"));
+				furtherDetails.append("</b> ");
+				furtherDetails.append(conf.getMinimum());
+				furtherDetails.append("<br//>");
+				furtherDetails.append("<b>");
+				furtherDetails.append(bundle
+						.getString("pages.blockR.maxBlockSize"));
+				furtherDetails.append("</b> ");
+				furtherDetails.append(conf.getMaximum());
+				furtherDetails.append("<br//>");
+			} else {
+				furtherDetails.append("<b>");
+				furtherDetails.append(bundle
+						.getString("pages.blockR.blockSize"));
+				furtherDetails.append("</b> ");
+				furtherDetails.append(conf.getMinimum());
+				furtherDetails.append("<br//>");
+			}
+		} else if (UrnDesignConfig.class.isInstance(randi2Page
+				.getCurrentTrial().getRandomizationConfiguration())) {
+			UrnDesignConfig conf = UrnDesignConfig.class.cast(randi2Page
+					.getCurrentTrial().getRandomizationConfiguration());
+			furtherDetails.append("<b>");
+			furtherDetails.append(bundle.getString("pages.urnR.initialCount"));
+			furtherDetails.append("</b> ");
+			furtherDetails.append(conf.getInitializeCountBalls());
+			furtherDetails.append("<br//>");
+			furtherDetails.append("<b>");
+			furtherDetails.append(bundle.getString("pages.urnR.replacedBalls"));
+			furtherDetails.append("</b> ");
+			furtherDetails.append(conf.getCountReplacedBalls());
+			furtherDetails.append("<br//>");
+		} else if (MinimizationConfig.class.isInstance(randi2Page
+				.getCurrentTrial().getRandomizationConfiguration())) {
+			MinimizationConfig conf = MinimizationConfig.class.cast(randi2Page
+					.getCurrentTrial().getRandomizationConfiguration());
+			furtherDetails.append("<b>");
+			furtherDetails
+					.append(bundle.getString("pages.minimization.pvalue"));
+			furtherDetails.append("</b> ");
+			furtherDetails.append(conf.getP());
+			furtherDetails.append("<br//>");
+		} else {
+			furtherDetails.append("--");
+		}
+		return furtherDetails.toString();
+	}
 
-    private List<CriterionWrapper<Serializable>> strata;
+	/**
+	 * Specifies if the algorithm is stratified or not.
+	 * 
+	 * @return
+	 */
+	public boolean isStratified() {
+		boolean t = isStrataFactorsDefined();
+		if (t)
+			return t;
+		else
+			return randi2Page.getCurrentTrial().isStratifyTrialSite();
+	}
 
-    @SuppressWarnings("unchecked")
-	public List<CriterionWrapper<Serializable>> getStrata(){
-        strata  = new ArrayList<CriterionWrapper<Serializable>>();
-        for(AbstractCriterion<? extends Serializable, ? extends AbstractConstraint<? extends Serializable>> c : randi2Page.getCurrentTrial().getCriteria()){
-            strata.add(new CriterionWrapper<Serializable>((AbstractCriterion<Serializable, ?>) c));
-        }
-        return strata;
-    }
+	/**
+	 * Checks if any strata factors are defined.
+	 * 
+	 * @return
+	 */
+	public boolean isStrataFactorsDefined() {
+
+		for (AbstractCriterion<?, ?> c : randi2Page.getCurrentTrial()
+				.getCriteria()) {
+			if (c.getStrata() != null) {
+				if (c.getStrata().size() > 0)
+					return true;
+			}
+		}
+		return false;
+	}
+
+	private List<CriterionWrapper<Serializable>> strata;
+
+	@SuppressWarnings("unchecked")
+	public List<CriterionWrapper<Serializable>> getStrata() {
+		strata = new ArrayList<CriterionWrapper<Serializable>>();
+		for (AbstractCriterion<? extends Serializable, ? extends AbstractConstraint<? extends Serializable>> c : randi2Page
+				.getCurrentTrial().getCriteria()) {
+			/*
+			 * Check if the criterion is a defined strata factor and if so wrapp
+			 * it and add to the collection
+			 */
+			if (c.getStrata() != null && !c.getStrata().isEmpty())
+				strata.add(new CriterionWrapper<Serializable>(
+						(AbstractCriterion<Serializable, ?>) c));
+		}
+		return strata;
+	}
 
 }
