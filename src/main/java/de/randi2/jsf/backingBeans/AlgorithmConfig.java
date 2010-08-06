3356695f27d744b7049cde6468304ac9bea4d6f4
lplotni <l.plotnicki@gmail.com>
2010-08-06 16:56:30 +0200

    Using the algorithm configuration UI for the editing

--- src/main/java/de/randi2/jsf/backingBeans/AlgorithmConfig.java
+++ src/main/java/de/randi2/jsf/backingBeans/AlgorithmConfig.java
@@ -22,12 +22,13 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
 
-import lombok.Getter;
 import lombok.Setter;
 import de.randi2.jsf.controllerBeans.LoginHandler;
 import de.randi2.jsf.controllerBeans.TrialHandler;
+import de.randi2.model.randomization.AbstractRandomizationConfig;
 import de.randi2.model.randomization.BiasedCoinRandomizationConfig;
 import de.randi2.model.randomization.BlockRandomizationConfig;
 import de.randi2.model.randomization.CompleteRandomizationConfig;
@@ -104,28 +105,48 @@ public class AlgorithmConfig {
 		return algorithms;
 	}
 
-	@Getter
 	private String selectedAlgorithmPanelId = "none";
+	
+	public String getSelectedAlgorithmPanelId() {
+		AbstractRandomizationConfig rand = trialHandler.getCurrentObject().getRandomizationConfiguration();
+		if(rand!=null){
+			if(rand instanceof CompleteRandomizationConfig)
+				return AlgorithmPanelId.COMPLETE_RANDOMIZATION.toString();
+			else if(rand instanceof BiasedCoinRandomizationConfig)
+				return AlgorithmPanelId.BIASEDCOIN_RANDOMIZATION.toString();
+			else if(rand instanceof TruncatedBinomialDesignConfig)
+				return AlgorithmPanelId.TRUNCATED_RANDOMIZATION.toString();
+			else if(rand instanceof BlockRandomizationConfig)
+				return AlgorithmPanelId.BLOCK_RANDOMIZATION.toString();
+			else if(rand instanceof UrnDesignConfig)
+				return AlgorithmPanelId.URN_MODEL.toString();
+			else if(rand instanceof MinimizationConfig)
+				return AlgorithmPanelId.MINIMIZATION.toString();
+		}
+		return selectedAlgorithmPanelId;
+	}
 
+	public void algorithmChanged(ValueChangeEvent event){
+		String newSelection = (String) event.getNewValue();
+		if(newSelection.equals(selectedAlgorithmPanelId))
+			return;
+		else
+			if(AlgorithmPanelId.COMPLETE_RANDOMIZATION.toString().equals(newSelection))
+				trialHandler.getCurrentObject().setRandomizationConfiguration(new CompleteRandomizationConfig());
+			else if(AlgorithmPanelId.BIASEDCOIN_RANDOMIZATION.toString().equals(newSelection))
+				trialHandler.getCurrentObject().setRandomizationConfiguration(new BiasedCoinRandomizationConfig());
+			else if(AlgorithmPanelId.TRUNCATED_RANDOMIZATION.toString().equals(newSelection))
+				trialHandler.getCurrentObject().setRandomizationConfiguration(new TruncatedBinomialDesignConfig());
+			else if(AlgorithmPanelId.BLOCK_RANDOMIZATION.toString().equals(newSelection))
+				trialHandler.getCurrentObject().setRandomizationConfiguration(new BlockRandomizationConfig());
+			else if(AlgorithmPanelId.URN_MODEL.toString().equals(newSelection))
+				trialHandler.getCurrentObject().setRandomizationConfiguration(new UrnDesignConfig());
+			else if(AlgorithmPanelId.MINIMIZATION.toString().equals(newSelection))
+				trialHandler.getCurrentObject().setRandomizationConfiguration(new MinimizationConfig());
+	}
+	
 	public void setSelectedAlgorithmPanelId(String selectedAlgorithmPanelId) {
 		this.selectedAlgorithmPanelId = selectedAlgorithmPanelId;
-		if (selectedAlgorithmPanelId
-				.equals(AlgorithmPanelId.BLOCK_RANDOMIZATION.toString())
-				&& !BlockRandomizationConfig.class.isInstance(trialHandler
-						.getRandomizationConfig())) {
-			trialHandler.setRandomizationConfig(new BlockRandomizationConfig());
-		}else if(selectedAlgorithmPanelId
-				.equals(AlgorithmPanelId.URN_MODEL.toString())
-				&& !UrnDesignConfig.class.isInstance(trialHandler
-						.getRandomizationConfig())) {
-			trialHandler.setRandomizationConfig(new UrnDesignConfig());
-		}else if(selectedAlgorithmPanelId
-				.equals(AlgorithmPanelId.MINIMIZATION.toString())
-				&& !MinimizationConfig.class.isInstance(trialHandler
-						.getRandomizationConfig())) {
-			trialHandler.setRandomizationConfig(new MinimizationConfig());
-		}
-				
 	}
 	
 	public void clean(){
