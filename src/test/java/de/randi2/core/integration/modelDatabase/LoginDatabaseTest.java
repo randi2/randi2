15ef447b66e27b7d73a4530d5bcc639c0afc5700
Daniel Schrimpf <daniel_schrimpf@web.de>
2010-10-27 18:04:57 +0200

    added test for named queries

--- src/test/java/de/randi2/core/integration/modelDatabase/LoginDatabaseTest.java
+++ src/test/java/de/randi2/core/integration/modelDatabase/LoginDatabaseTest.java
@@ -3,23 +3,29 @@ package de.randi2.core.integration.modelDatabase;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.Locale;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.transaction.annotation.Transactional;
 
 import de.randi2.model.AbstractDomainObject;
 import de.randi2.model.Login;
 import de.randi2.model.Person;
+import de.randi2.model.Role;
+import de.randi2.model.TrialSite;
 import de.randi2.testUtility.utility.AbstractDomainDatabaseTest;
 
 public class LoginDatabaseTest extends AbstractDomainDatabaseTest<Login> {
 
 	
-	Login validLogin;
+	private Login validLogin;
 	
 	
 	public LoginDatabaseTest() {
@@ -92,6 +98,178 @@ public class LoginDatabaseTest extends AbstractDomainDatabaseTest<Login> {
 //		FIXME in test with mysql it is a difference of nearly 1 second
 //		assertEquals(validLogin.getLockTime(), login.getLockTime());
 		assertEquals(validLogin.getPerson().getId(), login.getPerson().getId());
+	}
+	
+	@Test
+	@Ignore
+	@Transactional
+	public void namedQueryAllLoginsWithRolesAndNotTrialSiteScopeTest(){
+		fail();
+	}
+	
+	@Test
+	@Ignore
+	@Transactional
+	public void namedQueryLoginsWriteOtherUserTest(){
+		fail();
+	}
+	
+	@Test 
+	@Ignore
+	@Transactional
+	public void namedQueryLoginsWithPermissionTest(){
+		fail();
+	}
+	
+	@Test 
+	@Transactional
+	public void namedQueryAllLoginsWithSpecificRoleTest(){
+		/*
+		 * generate test data
+		 * 10 Logins with 2 roles
+		 */
+		List<Login> logins = new ArrayList<Login>();
+		for(int i =0; i<10;i++){
+			logins.add(factory.getLogin());
+			entityManager.persist(logins.get(i));
+		}
+		Role role1 = new Role("ROLE_Name1", false, false, false, false, false,
+				true, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, null);
+		entityManager.persist(role1);
+		Role role2 = new Role("ROLE_Name2", false, false, false, false, false,
+				true, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, null);
+		entityManager.persist(role2);
+		
+		//Logins 
+		logins.get(0).getRoles().add(role1);
+		logins.get(1).getRoles().add(role1);
+		logins.get(2).getRoles().add(role2);
+		logins.get(3).getRoles().add(role2);
+		logins.get(4).getRoles().add(role1);//login 4 has two roles
+		logins.get(4).getRoles().add(role2);
+		//Logins
+		logins.get(5).getRoles().add(role1);
+		logins.get(6).getRoles().add(role1);
+		logins.get(7).getRoles().add(role2);
+		logins.get(8).getRoles().add(role2);
+		logins.get(9).getRoles().add(role1);//login 9 has two roles
+		logins.get(9).getRoles().add(role2);
+		
+		for(Login l: logins){
+			l = entityManager.merge(l);
+		}
+		//Flush session and clear the entity manager
+		entityManager.flush();
+		entityManager.clear();
+		//Test named query
+		List<Login> result = entityManager.createNamedQuery("login.AllLoginsWithSpecificRole").setParameter(1, role1.getId()).getResultList();
+		assertEquals(6, result.size());
+		assertTrue(result.contains(logins.get(0)));
+		assertTrue(result.contains(logins.get(1)));
+		assertTrue(result.contains(logins.get(4)));
+		assertTrue(result.contains(logins.get(5)));
+		assertTrue(result.contains(logins.get(6)));
+		assertTrue(result.contains(logins.get(9)));
+		
+		result = entityManager.createNamedQuery("login.AllLoginsWithSpecificRole").setParameter(1, role2.getId()).getResultList();
+		assertEquals(6, result.size());
+		assertTrue(result.contains(logins.get(2)));
+		assertTrue(result.contains(logins.get(3)));
+		assertTrue(result.contains(logins.get(4)));
+		assertTrue(result.contains(logins.get(7)));
+		assertTrue(result.contains(logins.get(8)));
+		assertTrue(result.contains(logins.get(9)));
+
+
+	}
+	
+	@Test 
+	@Transactional
+	public void namedQueryAllLoginsWithSpecificRoleAndTrialSiteTest(){
+		/*
+		 * generate test data
+		 * 10 Logins in two trial sites with 2 roles
+		 * the first 5 logins in site1 the other in site2 
+		 */
+		List<Login> logins = new ArrayList<Login>();
+		for(int i =0; i<10;i++){
+			logins.add(factory.getLogin());
+			entityManager.persist(logins.get(i));
+		}
+		TrialSite site1 = factory.getTrialSite();
+		entityManager.persist(site1);
+		TrialSite site2 = factory.getTrialSite();
+		entityManager.persist(site2);
+		Role role1 = new Role("ROLE_Name1", false, false, false, false, false,
+				true, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, null);
+		entityManager.persist(role1);
+		Role role2 = new Role("ROLE_Name2", false, false, false, false, false,
+				true, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, null);
+		entityManager.persist(role2);
+		
+		for(int i=0;i< logins.size();i++){
+			if(i<5){
+				site1.getMembers().add(logins.get(i).getPerson());
+			}else{
+				site2.getMembers().add(logins.get(i).getPerson());
+			}
+		}
+		site1 = entityManager.merge(site1);
+		site2 = entityManager.merge(site2);
+		//Logins site1
+		logins.get(0).getRoles().add(role1);
+		logins.get(1).getRoles().add(role1);
+		logins.get(2).getRoles().add(role2);
+		logins.get(3).getRoles().add(role2);
+		logins.get(4).getRoles().add(role1);//login 4 has two roles
+		logins.get(4).getRoles().add(role2);
+		//Logins site2
+		logins.get(5).getRoles().add(role1);
+		logins.get(6).getRoles().add(role1);
+		logins.get(7).getRoles().add(role2);
+		logins.get(8).getRoles().add(role2);
+		logins.get(9).getRoles().add(role1);//login 9 has two roles
+		logins.get(9).getRoles().add(role2);
+		
+		for(Login l: logins){
+			l = entityManager.merge(l);
+		}
+		//Flush session and clear the entity manager
+		entityManager.flush();
+		entityManager.clear();
+		//Test named query
+		List<Login> result = entityManager.createNamedQuery("login.AllLoginsWithSpecificRoleAndTrialSite").setParameter(1, role1.getId()).setParameter(2, site1.getId()).getResultList();
+		assertEquals(3, result.size());
+		assertTrue(result.contains(logins.get(0)));
+		assertTrue(result.contains(logins.get(1)));
+		assertTrue(result.contains(logins.get(4)));
+		
+		result = entityManager.createNamedQuery("login.AllLoginsWithSpecificRoleAndTrialSite").setParameter(1, role2.getId()).setParameter(2, site1.getId()).getResultList();
+		assertEquals(3, result.size());
+		assertTrue(result.contains(logins.get(2)));
+		assertTrue(result.contains(logins.get(3)));
+		assertTrue(result.contains(logins.get(4)));
+
+		result = entityManager.createNamedQuery("login.AllLoginsWithSpecificRoleAndTrialSite").setParameter(1, role1.getId()).setParameter(2, site2.getId()).getResultList();
+		assertEquals(3, result.size());
+		assertTrue(result.contains(logins.get(5)));
+		assertTrue(result.contains(logins.get(6)));
+		assertTrue(result.contains(logins.get(9)));
+		
+		result = entityManager.createNamedQuery("login.AllLoginsWithSpecificRoleAndTrialSite").setParameter(1, role2.getId()).setParameter(2, site2.getId()).getResultList();
+		assertEquals(3, result.size());
+		assertTrue(result.contains(logins.get(7)));
+		assertTrue(result.contains(logins.get(8)));
+		assertTrue(result.contains(logins.get(9)));
+		
 	}
 	
 }
