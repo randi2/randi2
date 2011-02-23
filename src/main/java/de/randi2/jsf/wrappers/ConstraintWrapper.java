29c2ff17254d005e7684c616352315ead4c7db30
Daniel Schrimpf <ds@randi2.de>
2011-02-23 14:43:14 +0100

    refactored configured method

--- src/main/java/de/randi2/jsf/wrappers/ConstraintWrapper.java
+++ src/main/java/de/randi2/jsf/wrappers/ConstraintWrapper.java
@@ -18,85 +18,90 @@
 package de.randi2.jsf.wrappers;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.GregorianCalendar;
 import java.util.List;
-import java.text.SimpleDateFormat;
 
 import javax.faces.event.ValueChangeEvent;
 
 import lombok.Getter;
 import lombok.Setter;
-import de.randi2.model.criteria.OrdinalCriterion;
 import de.randi2.model.criteria.constraints.AbstractConstraint;
 import de.randi2.model.criteria.constraints.DateConstraint;
 import de.randi2.model.criteria.constraints.OrdinalConstraint;
-import de.randi2.utility.BoxedException;
 import de.randi2.unsorted.ContraintViolatedException;
+import de.randi2.utility.BoxedException;
 
 public class ConstraintWrapper<V extends Serializable> {
-	
-	@Getter @Setter
+
+	@Getter
+	@Setter
 	private AbstractConstraint<V> wrappedConstraint = null;
-	
-	@Getter @Setter
+
+	@Getter
+	@Setter
 	private int groupNr = 0;
-	
-	public ConstraintWrapper(int nr){
+
+	public ConstraintWrapper(int nr) {
 		groupNr = nr;
 	}
 
-    public ConstraintWrapper(int nr, AbstractConstraint<V> _constraint){
-        groupNr = nr;
-        wrappedConstraint = _constraint;
-        if(DateConstraint.class.isInstance(_constraint)){
-           date1 = DateConstraint.class.cast(wrappedConstraint).getFirstDate();
-           date2 = DateConstraint.class.cast(wrappedConstraint).getSecondDate();
-        }
-    }
-	
+	public ConstraintWrapper(int nr, AbstractConstraint<V> _constraint) {
+		groupNr = nr;
+		wrappedConstraint = _constraint;
+		if (DateConstraint.class.isInstance(_constraint)) {
+			date1 = DateConstraint.class.cast(wrappedConstraint).getFirstDate();
+			date2 = DateConstraint.class.cast(wrappedConstraint)
+					.getSecondDate();
+		}
+	}
+
 	@SuppressWarnings("unchecked")
-	public void update(ValueChangeEvent event){
+	public void update(ValueChangeEvent event) {
 		List<V> tValues;
-        try{
+		try {
 			tValues = Arrays.asList((V[]) event.getNewValue());
-            if(!tValues.isEmpty())
-                wrappedConstraint = (AbstractConstraint<V>) new OrdinalConstraint((List<String>)tValues);
-		}catch(ClassCastException ex1){
+			if (!tValues.isEmpty())
+				wrappedConstraint = (AbstractConstraint<V>) new OrdinalConstraint(
+						(List<String>) tValues);
+		} catch (ClassCastException ex1) {
 			BoxedException.throwBoxed(ex1);
 		} catch (ContraintViolatedException ex2) {
 			BoxedException.throwBoxed(ex2);
 		}
 	}
 
-	@Getter @Setter
+	@Getter
+	@Setter
 	private GregorianCalendar date1 = new GregorianCalendar();
-	
-	@Getter @Setter
+
+	@Getter
+	@Setter
 	private GregorianCalendar date2 = new GregorianCalendar();
-	
-	
+
 	@SuppressWarnings("unchecked")
-	public AbstractConstraint<V> configure(){
-		if(wrappedConstraint!=null && OrdinalConstraint.class.isInstance(wrappedConstraint))
-			return wrappedConstraint;
-		else{
+	public AbstractConstraint<V> configure() {
+		if (wrappedConstraint != null
+				&& DateConstraint.class.isInstance(wrappedConstraint)) {
 			try {
-				wrappedConstraint = (AbstractConstraint<V>) new DateConstraint(Arrays.asList(new GregorianCalendar[]{date1, date2}));
+				wrappedConstraint = (AbstractConstraint<V>) new DateConstraint(
+						Arrays.asList(new GregorianCalendar[] { date1, date2 }));
 			} catch (ContraintViolatedException e) {
 				e.printStackTrace();
 			}
+
 		}
 		return wrappedConstraint;
 	}
 
-    private SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
 
-    public String getFormatedDate1(){
-        return sdf.format(date1.getTime());
-    }
+	public String getFormatedDate1() {
+		return sdf.format(date1.getTime());
+	}
 
-    public String getFormatedDate2(){
-        return sdf.format(date2.getTime());
-    }
+	public String getFormatedDate2() {
+		return sdf.format(date2.getTime());
+	}
 }
